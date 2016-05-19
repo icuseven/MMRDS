f2362510eae364947cd200e224fe78e12b4fa3ce
jhaines <jhaines@thetagrid.com>
2016-05-19 16:52:28 -0400

    Added WebAPI Route.

--- source-code/scratch/owin/owin/Program.cs
+++ source-code/scratch/owin/owin/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.Owin;
 using Owin;
 
+using System.Web.Http;
+
 namespace owin
 {
 	class MainClass
@@ -26,6 +28,18 @@ namespace owin
 			app.UseErrorPage();
 			#endif
 			app.UseWelcomePage("/");
+
+
+			// Configure Web API for self-host. 
+			HttpConfiguration config = new HttpConfiguration(); 
+			config.Routes.MapHttpRoute( 
+				name: "DefaultApi", 
+				routeTemplate: "api/{controller}/{id}", 
+				defaults: new { id = RouteParameter.Optional } 
+			); 
+
+			app.UseWebApi(config); 
+
 		}
 	}
 }
