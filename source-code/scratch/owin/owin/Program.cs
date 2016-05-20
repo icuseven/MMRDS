58c14d06af5fbdfb985453d50488398ea16ab539
jhaines <jhaines@thetagrid.com>
2016-05-19 23:39:40 -0400

    Missing changes added.

--- source-code/scratch/owin/owin/Program.cs
+++ source-code/scratch/owin/owin/Program.cs
@@ -9,10 +9,25 @@ namespace owin
 	class MainClass
 	{
 
+		// http://www.asp.net/aspnet/samples/owin-katana
+
+		//http://localhost:12345
+		//http://localhost:12345/api/values
+
 		static void Main(string[] args)
 		{
-			using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:12345"))
+			var url = "http://localhost:12345";
+
+			using (Microsoft.Owin.Hosting.WebApp.Start<Startup>(url))
 			{
+				WebApp.Start(url, builder => builder.UseFileServer(enableDirectoryBrowsing:true));            
+				Console.WriteLine("Listening at " + url);
+
+				//http://odetocode.com/blogs/scott/archive/2014/02/10/building-a-simple-file-server-with-owin-and-katana.aspx
+
+
+
+
 				Console.ReadLine();
 			}
 		}
