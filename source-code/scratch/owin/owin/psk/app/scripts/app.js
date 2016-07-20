64371dcd71e404aba81cbc14d394d1c7f26ac19a
icuseven <jhaines@thetagrid.net>
2016-07-20 15:07:44 -0400

    Updated data binding for listboxes on death-certificate.

--- source-code/scratch/owin/owin/psk/app/scripts/app.js
+++ source-code/scratch/owin/owin/psk/app/scripts/app.js
@@ -15,6 +15,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
+
+  
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
