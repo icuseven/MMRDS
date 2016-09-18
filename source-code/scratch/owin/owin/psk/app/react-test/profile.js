209bee370639edc7c43556130b2dff3d5029fa8e
jhaines <jhaines@thetagrid.com>
2016-09-17 21:12:54 -0400

    Setup proper cookie authsession and profile login/logout.

--- source-code/scratch/owin/owin/psk/app/react-test/profile.js
+++ source-code/scratch/owin/owin/psk/app/react-test/profile.js
@@ -7,7 +7,28 @@ var ProfileComponent = React.createClass({
 	},
 	componentWillMount:function()
 	{
-		this.setState({ email: "user1", password: "password" });
+		
+		var current_auth_session = null;
+		var cookie_string = new String(document.cookie);
+		if(cookie_string.length > 0)
+		{
+			var cookie_array = cookie_string.split("=");
+			if(cookie_array.length > 1 && cookie_array[0] == "AuthSession")
+			{
+				current_auth_session=cookie_array[1];
+			}
+		}
+		
+		if(current_auth_session)
+		{
+			var url =  location.protocol + '//' + location.host + "/api/session";
+			var AJAX = new AJAX_();
+			var meta_data = AJAX.GetResponse(url, this.handle_get_response);
+		}
+		else
+		{
+			this.setState({ user_name: "user1", password: "password" });
+		}
 	},
 	handleLogin : function (e)
 	{
@@ -23,19 +44,26 @@ var ProfileComponent = React.createClass({
 	{
 		console.log('on no change');
 	},
+	onLogin : function()
+	{
+		this.state.is_logged_in = true;
+	},
+	onLogout : function()
+	{
+		document.cookie = "AuthSession=" + this.state.auth_session + "; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";		
+		this.setState({ is_logged_in: false	});
+		
+	},
 	render: function render() 
 	{
 		if(this.state.is_logged_in)
 		{
 			return React.createElement('form', {},
 				React.createElement('fieldset',{},
-					React.createElement('legend',{},'Login:'),
-					'Email:', 
-					React.createElement('input', { type:'text', ref:'email', onChange:this.onNoChange, onBlur: this.onEmailChange, defaultValue: this.state.email}),
+					React.createElement('legend',{},'profile:'),
+					'user: ' + this.state.user_name,
 					React.createElement('br'),
-					'Password:', 
-					React.createElement('input', { type:'password', ref:'password', onChange:this.onPasswordChange, defaultValue:this.state.password}),
-					React.createElement('input', {  type:'button', onClick:this.handleLogin, value:'login'})
+					React.createElement('input', {  type:'button', onClick:this.onLogout, value:'logout'})
 				)
 			);
 		}
@@ -45,7 +73,7 @@ var ProfileComponent = React.createClass({
 					React.createElement('fieldset',{},
 						React.createElement('legend',{},'Login:'),
 						'Email:', 
-						React.createElement('input', { type:'text', ref:'email', onChange:this.onNoChange, onBlur: this.onEmailChange, defaultValue: this.state.email}),
+						React.createElement('input', { type:'text', ref:'email', onChange:this.onNoChange, onBlur: this.onEmailChange, defaultValue: this.state.user_name}),
 						React.createElement('br'),
 						'Password:', 
 						React.createElement('input', { type:'password', ref:'password', onChange:this.onPasswordChange, defaultValue:this.state.password}),
@@ -66,37 +94,49 @@ var ProfileComponent = React.createClass({
 			var url =  location.protocol + '//' + location.host + "/api/session";
 			var AJAX = new AJAX_();
 			var post_data = "userid=" + this.refs['email'].value + "&password=" + this.refs['password'].value;
-			var meta_data = AJAX.GetResponse(url + "?" + post_data, login_response);
+			var meta_data = AJAX.GetResponse(url + "?" + post_data, this.login_response);
 		
 		}
 		
 
 	},
-	login_response: function(request) 
+	login_response: function(response) 
 	{
-				//ready_this.CreateFromMetaData(document, ready_this, metadata, parent);
-				console.log("response\n", response);
-				var valid_login = false;
-		
-				var json_response = response[0];
+		//ready_this.CreateFromMetaData(document, ready_this, metadata, parent);
+		console.log("response\n", response);
+		var valid_login = false;
 
-				console.log(response);
-				
-				//document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
-				//console.log(this.$.ajax.lastResponse);
-				//{"ok":true,"userCtx":{"name":null,"roles":[]},"info":{"authentication_db":"_users","authentication_handlers":["oauth","cookie","default"]}}
-				//http://stackoverflow.com/questions/34042408/polymerjs-iron-ajax-how-to-bind-token-to-headers-property
-				
-				valid_login = json_response.name != null;
-				if(valid_login)
-				{
-					var blank = { 
-						is_logged_in: true, 
-						user_name: json_response.name,
-						user_roles: json_response.roles,
-						auth_session: json_response.auth_session,
-					};
-				}
+		var json_response = response[0];
+
+		console.log(response);
+		
+		//{"ok":true,"userCtx":{"name":null,"roles":[]},"info":{"authentication_db":"_users","authentication_handlers":["oauth","cookie","default"]}}
+		valid_login = json_response.name != null;
+		if(valid_login)
+		{
+			var blank = { 
+				is_logged_in: true, 
+				user_name: json_response.name,
+				user_roles: json_response.roles,
+				auth_session: json_response.auth_session,
+			};
+			
+			this.setState({ is_logged_in: true,
+			user_name: json_response.name,
+			user_roles: json_response.roles,
+			auth_session: json_response.auth_session
+			});
+			var minutes_14 = 14;
+			var current_date_time = new Date();
+			var new_date_time = new Date(current_date_time.getTime() + minutes_14 * 60000);
+			
+			document.cookie = "AuthSession=" + this.state.auth_session + "; expires=" + new_date_time.toGMTString() + "; path=/";
+		}
+		else
+		{
+			this.setState({ is_logged_in: false	});
+			document.cookie = "AuthSession=" + this.state.auth_session + "; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";		
+		}
 				
 	},
 	ready:function()
@@ -119,15 +159,14 @@ var ProfileComponent = React.createClass({
 			this.$.ajax.generateRequest();
 		}
 	},
-	handle_response: function(request) 
+	handle_get_response: function(response) 
 	{
+		//ready_this.CreateFromMetaData(document, ready_this, metadata, parent);
+		console.log("response\n", response);
 		var valid_login = false;
-		
-	
-		var json_response = request.detail.response[0];
-		this.user_name = json_response.userCTX.name;
-		this.user_roles = json_response.userCTX.roles
-		
+
+		var json_response = response[0];
+
 		var current_auth_session = null;
 		var cookie_string = new String(document.cookie);
 		if(cookie_string.length > 0)
@@ -139,28 +178,31 @@ var ProfileComponent = React.createClass({
 			}
 		}
 		
-		this.auth_session = current_auth_session;
-
-		console.log(request.detail.response);
-		/*
-		var minutes_14 = 14;
-		var current_date_time = new Date();
-		var new_date_time = new Date(current_date_time.getTime() + minutes_14 * 60000);
-		
-		document.cookie = "AuthSession=" + json_response.auth_session + "; expires=" + new_date_time.toISOString() + ";";
 		
 		
-		*/
-		valid_login = this.user_name != null;
+		//{"ok":true,"userCtx":{"name":null,"roles":[]},"info":{"authentication_db":"_users","authentication_handlers":["oauth","cookie","default"]}}
+		valid_login = json_response.userCTX.name != null;
 		if(valid_login)
 		{
-			this.fire('profile_login_changed', { 
-				is_logged_in: true, 
-				user_name: this.user_name,
-				user_roles: this.user_roles,
-				auth_session: this.auth_session,
+			this.setState({
+				is_logged_in: true,
+				user_name: json_response.userCTX.name,
+				user_roles: json_response.userCTX.roles,
+				auth_session: current_auth_session
 			});
+			
+			var minutes_14 = 14;
+			var current_date_time = new Date();
+			var new_date_time = new Date(current_date_time.getTime() + minutes_14 * 60000);
+			
+			document.cookie = "AuthSession=" + this.state.auth_session + "; expires=" + new_date_time.toGMTString() + "; path=/";
 		}
+		else
+		{
+			this.setState({ is_logged_in: false	});
+			document.cookie = "AuthSession=" + this.state.auth_session + "; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";		
+		}
+
 		
 	}
 });
