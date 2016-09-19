b254dfef5cf3e65a5ca64ea40c808e02e2c33e20
jhaines <jhaines@thetagrid.com>
2016-09-18 21:20:51 -0400

    Added data_acces.js Wire up summary to pouchDB.

--- source-code/scratch/owin/owin/psk/app/react-test/app.js
+++ source-code/scratch/owin/owin/psk/app/react-test/app.js
@@ -1,5 +1,6 @@
 var AppComponent = React.createClass({
 	displayName: "AppComponent",
+	
 	getInitialState: function() 
 	{
 		
@@ -21,16 +22,36 @@ var AppComponent = React.createClass({
 	},
 	render: function render() 
 	{
-		return React.createElement('div', {},
-			React.createElement('img',{ src:"../images/mmria-secondary.svg", height:75, width:100}),
-			React.createElement('h1',{},'App Element: MMRIA'),
-			React.createElement('div',{ id:'profile_content_id'},'App Element: MMRIA'),
-			React.createElement('div',{ id:'page_content_id'},
-			React.createElement('p',{},'The Maternal Mortality Review Information App (MMRIA) is a public health software tool created to collect, store, analyze and summarize information relevant to maternal deaths. The MMRIA serves 2 purposes: first to provide complete, detailed, and organized medical and social information that can be used by medical review committees to investigate individual maternal deaths; and second to provide a standardized cumulative database for future research and analysis on maternal mortality.'),
-			React.createElement('div',{ id:'navigation_id'}),
-			React.createElement('div',{ id:'form_content_id'},React.createElement(SummaryComponent,{}))
-			)
-		);
+		var result = null;
+		if(this.state.isLoggedIn)
+		{
+			 result = React.createElement('div', {},
+				React.createElement('img',{ src:"../images/mmria-secondary.svg", height:75, width:100}),
+				React.createElement('h1',{},'App Element: MMRIA'),
+				React.createElement('div',{ id:'profile_content_id'},'App Element: MMRIA'),
+				React.createElement('div',{ id:'page_content_id'},
+				React.createElement('p',{},'The Maternal Mortality Review Information App (MMRIA) is a public health software tool created to collect, store, analyze and summarize information relevant to maternal deaths. The MMRIA serves 2 purposes: first to provide complete, detailed, and organized medical and social information that can be used by medical review committees to investigate individual maternal deaths; and second to provide a standardized cumulative database for future research and analysis on maternal mortality.'),
+				React.createElement('div',{ id:'navigation_id'}),
+				React.createElement('div',{ id:'form_content_id'},React.createElement(SummaryComponent,{}))
+				)
+			);
+		}
+		else
+		{
+			 result = React.createElement('div', {},
+				React.createElement('img',{ src:"../images/mmria-secondary.svg", height:75, width:100}),
+				React.createElement('h1',{},'App Element: MMRIA'),
+				React.createElement('div',{ id:'profile_content_id'},'App Element: MMRIA'),
+				React.createElement('div',{ id:'page_content_id'},
+				React.createElement('p',{},'The Maternal Mortality Review Information App (MMRIA) is a public health software tool created to collect, store, analyze and summarize information relevant to maternal deaths. The MMRIA serves 2 purposes: first to provide complete, detailed, and organized medical and social information that can be used by medical review committees to investigate individual maternal deaths; and second to provide a standardized cumulative database for future research and analysis on maternal mortality.'),
+				React.createElement('div',{ id:'navigation_id'}),
+				React.createElement('div',{ id:'form_content_id'}))
+				)
+			
+		}
+		
+		
+		return result;
 	},
 	profile_login_changed:function( info ) 
 	{
--- source-code/scratch/owin/owin/psk/app/react-test/summary.js
+++ source-code/scratch/owin/owin/psk/app/react-test/summary.js
@@ -2,7 +2,32 @@ var SummaryComponent = React.createClass({
 	displayName: "SummaryComponent",
 	getInitialState: function() 
 	{
+		var ctx = this;
+		var new_array = [];
+		var data_access = new Data_Access("http://localhost:5984");
+		
+		data_access.db.allDocs({ include_docs: true }).then(function (info) 
+		{
+			for(var i in info.rows)
+			{
+				//new_array.push(info.rows[i].doc);
+				var item = info.rows[i].doc;
+				new_array.push({
+					'_id': item._id,
+					'last_name':item.last_name,
+					'first_name':item.first_name,
+					'middle_name':item.middle_name,
+					'record_id':item.record_id
+				});
+				//console.log(info.rows[i].doc._id);
+				console.log(info.rows[i].doc);
 
+				//{{item.last_name}}, {{item.first_name}} {{item.middle_name}} {{item.record_id}}				
+				
+			}
+			this.setState( { bound_data: new_array});
+		}.bind(ctx));
+		
 		return { };
 	},
 	componentWillMount:function()
@@ -44,7 +69,29 @@ var SummaryComponent = React.createClass({
 	render: function render() 
 	{
 
-			return React.createElement('form', {},
+			var child_array = [];
+			if(this.state.bound_data)
+			{
+				for(var i = 0; i < this.state.bound_data.length; i++)
+				{
+					var item = this.state.bound_data[i];
+					var url = "#/home-record/" + item._id;
+					var li = React.createElement('li',{ key: item._id, index:i },
+						'[ ',
+						React.createElement('a',{ 'data-route': 'master', href:url}, 'select'),
+						' ] ',
+						item.last_name + ', ' + item.first_name + ' ' + item.middle_name + ' (' + item.record_id + ')'
+						);
+					
+
+					child_array.push(li)
+
+				}
+			}
+			
+			var summary_rows = React.createElement('ul',{ id:'summary_row_id'}, child_array);
+			
+			var result = React.createElement('form', {},
 					React.createElement('fieldset',{},
 						React.createElement('legend',{},'select action:'),
 						React.createElement('input', {  type:'button', onClick:this.addNewCase, value:'add new mortality case'}), 
@@ -54,12 +101,10 @@ var SummaryComponent = React.createClass({
 						React.createElement('input', {  type:'text' }),
 						' ',
 						React.createElement('input', {  type:'button', onClick:this.addNewCase, value:'search'}),
-						React.createElement('ul',{},
-							React.createElement('li',{},'[ select ] name last (record number)'),
-							React.createElement('li',{},'[ select ] name last (record number)'),
-							React.createElement('li',{},'[ select ] name last (record number)')
-						)
+						summary_rows
 					)
 			);		
+			
+			return result;
 	}
 });
