b6dbd535fb220215091cb52d38e5d341e47fa131
icuseven <jhaines@thetagrid.net>
2016-10-07 15:08:19 -0400

    Update to CatchFiles Need to test File watcher on mono runtime.

---
+++ source-code/scratch/owin/owin/WatchFiles.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace owin
+{
+	static class WatchFiles
+	{
+		//static string source_folder = System.Configuration.ConfigurationManager.AppSettings["file_root_folder"];
+		static string source_folder = System.Configuration.ConfigurationManager.AppSettings["file_root_folder"] + "/editor";
+		static System.IO.DirectoryInfo sourceDirectoryInfo;
+		public static string WatchedFolder;
+		public static string ProcessedFolder;
+		static System.Collections.Generic.HashSet<string> WatchedFiles;
+
+
+		static FileSystemWatcher fs;
+
+		internal static void StartWatch()
+		{
+			/*
+			try
+			{
+				if (string.IsNullOrWhiteSpace(sourceFolderUserName) || string.IsNullOrWhiteSpace(sourceFolderPassword))
+				{
+					sourceFolderPassword = null;
+					sourceFolderUserName = null;
+				}
+
+
+				// *** Connect Networked Folder Connections
+				if (string.IsNullOrWhiteSpace(targetUserName) || string.IsNullOrWhiteSpace(targetPassword))
+				{
+					targetPassword = null;
+					targetUserName = null;
+				}
+
+				if (sourceFolderIsNetworkedConnection)
+				{
+					//DriveSettings.MapNetworkDrive(sourceDrive, sourceFolder, sourceFolderUserName, sourceFolderPassword);
+
+				}
+
+				if (targetFolderIsNetworkedConnection)
+				{
+					//DriveSettings.MapNetworkDrive(targetDrive, targetFolder, targetUserName, targetPassword);
+				}
+
+			}
+			catch (Exception ex)
+			{
+				System.Console.WriteLine("Exception Encountered: {0}", ex);
+
+			}
+			*/
+
+			sourceDirectoryInfo = new DirectoryInfo(source_folder);
+
+			WatchedFolder = sourceDirectoryInfo.FullName;// + "\\InputFileFolder";
+			ProcessedFolder = sourceDirectoryInfo.FullName + "\\ProcessedFileFolder\\";
+			WatchedFiles = new System.Collections.Generic.HashSet<string>(System.StringComparer.OrdinalIgnoreCase);
+
+			WatchedFiles.Add ("editor.js");
+			WatchedFiles.Add ("tree_node.js");
+			WatchedFiles.Add ("app.js");
+			WatchedFiles.Add ("navigation_renderer.js");
+			WatchedFiles.Add ("page_renderer.js");
+
+
+			// convert and move any existing files at start up
+			//ImageConverter.convertDirectory(WatchedFolder, System.Configuration.ConfigurationManager.AppSettings["TargetFolder"], "tif");
+
+
+			fs = new FileSystemWatcher
+			{
+				Path = WatchedFolder,
+				NotifyFilter = NotifyFilters.FileName,
+				Filter = "*.js"
+
+			};
+
+			fs.Created += fs_Changed;
+			fs.Changed += fs_Changed;
+			fs.EnableRaisingEvents = true;
+
+		}
+		static void fs_Changed(object sender, FileSystemEventArgs e)
+		{
+			FileInfo f = new FileInfo(e.FullPath);
+
+			if (f.Extension.Equals (".js") && WatchedFiles.Contains(f.Name))
+			{
+
+				var worker = new MyTaskWorkerDelegate (MyTaskWorker);
+				worker.BeginInvoke (e.FullPath, null, null);
+			}
+		}
+
+		private delegate void MyTaskWorkerDelegate(string SourceFilePath);
+
+		private static void MyTaskWorker(string SourceFilePath)
+		{
+			try
+			{
+				//ImageConverter.convertFile(SourceFilePath, System.Configuration.ConfigurationManager.AppSettings["TargetFolder"], "tif");
+			}
+			catch (Exception ex)
+			{
+				//if (bool.Parse(System.Configuration.ConfigurationManager.AppSettings["WriteErrorsToEventLog"]))
+				//{
+				System.Console.WriteLine("WatchFiles.MyTaskWorker SourceFilePath: {0}\n{1}", SourceFilePath, ex);
+				//}
+
+			}
+
+		}
+		/*
+		internal static void DisconnectDrives()
+		{
+			// *** Disconnect Networked Folder Connections
+			if (sourceFolderIsNetworkedConnection)
+			{
+				//DriveSettings.DisconnectNetworkDrive(sourceDrive, true);
+			}
+
+			if (targetFolderIsNetworkedConnection)
+			{
+				//DriveSettings.DisconnectNetworkDrive(targetDrive, true);
+			}
+		}*/
+
+	}
+}
+
+/*
+		static string sourceDrive = System.Configuration.ConfigurationManager.AppSettings["SourceDriveName"];
+		static bool sourceFolderIsNetworkedConnection = bool.Parse(System.Configuration.ConfigurationManager.AppSettings["SourceFolderIsNetworkConnection"]);
+		static string sourceFolderUserName = System.Configuration.ConfigurationManager.AppSettings["SourceFolderUserName"];
+		static string sourceFolderPassword = System.Configuration.ConfigurationManager.AppSettings["SourceFolderPassword"];
+
+
+		static string targetFolder = System.Configuration.ConfigurationManager.AppSettings["TargetFolder"];
+		static string targetDrive = System.Configuration.ConfigurationManager.AppSettings["TargetDriveName"];
+		static string targetUserName = System.Configuration.ConfigurationManager.AppSettings["TargetFolderUserName"];
+		static string targetPassword = System.Configuration.ConfigurationManager.AppSettings["TargetFolderPassword"];
+		static bool targetFolderIsNetworkedConnection = bool.Parse(System.Configuration.ConfigurationManager.AppSettings["TargetFolderIsNetworkConnection"]);
+		static bool testNetworkConnectionOnly = bool.Parse(System.Configuration.ConfigurationManager.AppSettings["TestNetworkConnectionOnly"]);
+		static bool DeleteUnUsedData = bool.Parse(System.Configuration.ConfigurationManager.AppSettings["DeleteUnUsedData"]);
+		//static System.IO.DirectoryInfo targetDirectoryInfo;
+		*/
---
+++ source-code/scratch/owin/owin/psk/app/editor/page_renderer.js
@@ -0,0 +1,278 @@
+function Meta_Data_Renderer_(){};
+
+
+
+
+
+
+
+Meta_Data_Renderer_.prototype.CreateFromMetaData = function(metadata, data)
+{
+	if(data){}else{ data = {};}
+	var a = null;
+	var element = null;
+	var h1 = null;
+	var element_2 = null;
+	var element_3 = null;
+	
+	var result = null;
+	
+	
+	
+	if(Array.isArray(metadata))
+	{
+		result = [];
+		for(var i = 0; i < metadata.length; i++)
+		{
+			var child = metadata[i];
+			var child_data = data[child.name];
+			var temp = null;
+			if(child_data || child_data== "")
+			{
+				temp = this.CreateFromMetaData(child, child_data);
+
+			}
+			else
+			{
+				if(child.type)
+				{
+					switch(child.type.toLowerCase())
+					{
+						case "yes_no":
+						case "list":
+							data[child.name] = "";
+							var value_list = [];
+							for(var i = 0; i < child.values.length; i++)
+							{
+								value_list.push(React.createElement('option', { key:child.values[i]}, child.values[i]));
+							}
+							var key_value = child.name + Math.random();
+							temp = React.createElement('fieldset',{ key:key_value },
+									React.createElement('legend',{},child.prompt),
+									React.createElement('select',{},
+										value_list
+										)
+									);
+							break;
+						case 'race':
+							data[child.name] = "";
+							var value_list = [];
+							for(var i = 0; i < child.values.length; i++)
+							{
+								value_list.push(React.createElement('option', { key:child.values[i]}, child.values[i]));
+							}
+							var key_value = child.name + Math.random();
+							temp = React.createElement('fieldset',{ key:key_value },
+									React.createElement('legend',{},child.prompt),
+									React.createElement('select',{ multiple:true, size:7 },
+										value_list
+										)
+									);
+							break;							
+						case "string":
+							data[child.name] = "";
+							break;
+						case "number":
+							data[child.name] = new Number();
+							break;
+						case "address":
+						case "group":
+							data[child.name] = {};
+							break;
+						case "date":
+							data[child.name] = new Date().toISOString();
+							break;
+						case "list":
+							data[child.name] = "";
+							break;
+						case "grid":
+							data[child.name] = [];
+							break;
+						default:
+							data[child.name] = "";
+							break;
+					}
+					
+					switch(child.type.toLowerCase())
+					{
+						case "yes_no":
+						case "list":
+							child_data = data[child.name];
+							temp = React.createElement('option', { key:i}, child_data);
+							break;
+						default:
+							child_data = data[child.name];
+							temp = this.CreateFromMetaData(child, child_data);
+							break;
+					}
+					
+				}
+				else
+				{
+					data[child.name] = "";
+					child_data = data[child.name];
+					temp = this.CreateFromMetaData(child, child_data);
+				}
+			}
+			if(temp)
+			{
+				result.push(temp);
+			}
+		}
+				
+		return result;
+	}
+	else if(metadata.type)
+	{
+		if(metadata.type.toLowerCase().endsWith(".json"))
+		{
+			/*
+			var url = location.protocol + '//' + location.host + '/meta-data/00/' + metadata.type;
+			var AJAX = new AJAX_();
+			
+			var meta_data = AJAX.GetSynchronousResponse(url);
+			console.log("synchronous", meta_data);
+			*/
+			
+			console.log("meta_data_render called .json: ", metadata.type);
+		}
+		else
+		{
+			switch(metadata.type.toLowerCase())
+			{
+				case 'string':
+					//<p key=''>prompt text <input /><p>
+					/*
+					element = React.createElement
+					(
+						"p",{ key: metadata.name},
+						metadata.prompt,
+						' ',
+						React.createElement("input", 
+						{ 
+							onChange:eval ('function(value){ this.bound_data.' + metadata.name + '=value' }' 
+						}, type:"text", "defaultValue": metadata.name'})
+					);*/
+					
+					element = React.createElement(StringComponent, { key:metadata.name, "metadata":metadata, defaultValue: data });
+					return element;
+					break;			
+				case 'number':
+					element = React.createElement(NumberComponent, { key:metadata.name, "metadata":metadata, defaultValue: data });
+					return element;
+					break;							
+				case 'boolean':
+					element = React.createElement(BooleanComponent, { key:metadata.name, "metadata":metadata, defaultValue: data });
+					return element;
+					break;
+				case 'date':
+					element = React.createElement(DateComponent, { key:metadata.name, "metadata":metadata, defaultValue: data });
+					//element = React.createElement(AReactDatepicker, { key:metadata.name, "metadata":metadata, defaultValue: data });
+					return element;
+					break;				
+				case 'form':
+					var section = React.createElement
+					(
+						"section", { "data-route": metadata.data_route, "tabindex": "-1", key: metadata.name},
+						React.createElement("h1", null, metadata.prompt),
+						this.CreateFromMetaData(metadata.children, data)
+					);
+					return section;
+					break;
+					
+				
+				case 'address':
+				case 'group':
+					element = React.createElement('fieldset',{ key:metadata.name },
+							React.createElement('legend',{},metadata.prompt),
+							this.CreateFromMetaData(metadata.children, data)
+							);
+					return element;
+					break;
+				case 'yes_no':
+				case 'list':
+					var value_list = [];
+					for(var i = 0; i < metadata.values.length; i++)
+					{
+						var key_value = metadata.name + i;
+						value_list.push(React.createElement('option', { key: key_value}, metadata.values[i]));
+					}
+					
+					element = React.createElement('fieldset',{ key:metadata.name },
+							React.createElement('legend',{},metadata.prompt),
+							React.createElement('select',{ },
+								value_list
+								)
+							);
+					return element;
+					break;	
+				case 'race':
+				
+					var value_list = [];
+					for(var i = 0; i < metadata.values.length; i++)
+					{
+						var key_value = metadata.name + i;
+						value_list.push(React.createElement('option', { key: key_value}, metadata.values[i]));
+					}
+					
+					element = React.createElement('fieldset',{ key:metadata.name },
+							React.createElement('legend',{},metadata.prompt),
+							React.createElement('select',{ multiple:true, size:7},
+								value_list
+								)
+							);
+					return element;
+					break;						
+				case 'radio':
+					//<paper-input label="In prior 3 months (# of cigarettes/ packs)" value="{{bound_data.cigarette_smoking.prior_3_months}}"></paper-input>
+					//<paper-radio-group selected="{{bound_data.cigarette_smoking.prior_3_months_type}}">
+					
+					h1 = document.createElement("h2");
+					h1.innerHTML = metadata.prompt;
+					Polymer.dom(parent).appendChild(h1);
+					
+					element = document.createElement("paper-radio-group");
+					a  = document.createAttribute("selected");
+					a.value = "{{bound_data." + metadata.name + "}}";
+					element.setAttributeNode(a);
+					
+					
+					
+					//<paper-radio-button name="">not specified</paper-radio-button>
+					element_2 = document.createElement("paper-radio-button");
+					element_2.innerHTML = "not specified";
+					
+					a = document.createAttribute("name");
+					a.value = "";
+					element_2.setAttributeNode(a);
+					
+					Polymer.dom(element).appendChild(element_2);
+					
+					
+					for(var i = 0; i < metadata.values.length; i++)
+					{
+						var radio_value = metadata.values[i];
+						
+
+						element_2 = document.createElement("paper-radio-button");
+						a = document.createAttribute("name");
+						a.value = radio_value;
+						element_2.setAttributeNode(a);
+						element_2.innerHTML = radio_value;
+						Polymer.dom(element).appendChild(element_2);
+					}
+					
+					Polymer.dom(parent).appendChild(element);
+					
+					break;
+				case 'app':
+					return this.CreateFromMetaData(metadata.children, data);
+					break;					
+				default:
+					console.log(metadata.type);
+					break;
+				
+			}
+		}
+	}
+}
