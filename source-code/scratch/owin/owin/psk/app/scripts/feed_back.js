db602345fd3347f5584ba89c98223fb7c8674a96
jhaines <jhaines@thetagrid.com>
2016-10-13 09:57:51 -0400

    Added functions to dynamically load css.

--- source-code/scratch/owin/owin/psk/app/scripts/feed_back.js
+++ source-code/scratch/owin/owin/psk/app/scripts/feed_back.js
@@ -13,7 +13,7 @@ function remove_js_file(script_name)
       )
       {
         script_list[i].parentNode.removeChild(script_list[i]);
-        //break;
+        break;
       }
     }
 }
@@ -32,6 +32,44 @@ function reload_js(script_name, callback)
   }
   document_head.appendChild(dynamic_script);
 }
+
+
+function remove_css_file(sheet_name)
+{
+    var script_list = document.getElementsByTagName("link")
+    var url_host =  location.protocol + '//' + location.host;
+    var search_text = sheet_name.replace(url_host, "");
+    for (var i = script_list.length; i >= 0; i--)
+    {
+      if
+      (
+        script_list[i] &&
+        script_list[i].getAttribute("href")!=null &&
+        script_list[i].getAttribute("href").indexOf(search_text)!=-1
+      )
+      {
+        script_list[i].parentNode.removeChild(script_list[i]);
+        break;
+      }
+    }
+}
+
+function reload_css(sheet_name, callback)
+{
+  remove_css_file(sheet_name);
+
+  var document_head = document.getElementsByTagName("head")[0];
+  var dynamic_link = document.createElement("link");
+  dynamic_link.rel = "stylesheet";
+  dynamic_link.type = "text/css";
+  dynamic_link.src = sheet_name;
+  if(callback)
+  {
+    dynamic_script.addEventListener("load", callback);
+  }
+  document_head.appendChild(dynamic_link);
+}
+
 
 var change_detection_map = {};
 var interval_ids = null;
