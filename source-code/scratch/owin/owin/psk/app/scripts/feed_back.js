986dc3489690f0c2e29f7c353c7d3f62f05258b1
jhaines <jhaines@thetagrid.com>
2016-10-31 00:24:55 -0400

    Stubbed out print-version to handle print version and core summary report.

--- source-code/scratch/owin/owin/psk/app/scripts/feed_back.js
+++ source-code/scratch/owin/owin/psk/app/scripts/feed_back.js
@@ -1,75 +1,3 @@
-function remove_js_file(script_name)
-{
-    var script_list = document.getElementsByTagName("script")
-    var url_host =  location.protocol + '//' + location.host;
-    var search_text = script_name.replace(url_host, "");
-    for (var i = script_list.length; i >= 0; i--)
-    {
-      if
-      (
-        script_list[i] &&
-        script_list[i].getAttribute("src")!=null &&
-        script_list[i].getAttribute("src").indexOf(search_text)!=-1
-      )
-      {
-        script_list[i].parentNode.removeChild(script_list[i]);
-        break;
-      }
-    }
-}
-
-function reload_js(script_name, callback)
-{
-  remove_js_file(script_name);
-
-  var document_head = document.getElementsByTagName("head")[0];
-  var dynamic_script = document.createElement("script");
-  dynamic_script.type = "text/javascript";
-  dynamic_script.src = script_name;
-  if(callback)
-  {
-    dynamic_script.addEventListener("load", callback);
-  }
-  document_head.appendChild(dynamic_script);
-}
-
-
-function remove_css_file(sheet_name)
-{
-    var script_list = document.getElementsByTagName("link")
-    var url_host =  location.protocol + '//' + location.host;
-    var search_text = sheet_name.replace(url_host, "");
-    for (var i = script_list.length; i >= 0; i--)
-    {
-      if
-      (
-        script_list[i] &&
-        script_list[i].getAttribute("href")!=null &&
-        script_list[i].getAttribute("href").indexOf(search_text)!=-1
-      )
-      {
-        script_list[i].parentNode.removeChild(script_list[i]);
-        break;
-      }
-    }
-}
-
-function reload_css(sheet_name, callback)
-{
-  remove_css_file(sheet_name);
-
-  var document_head = document.getElementsByTagName("head")[0];
-  var dynamic_link = document.createElement("LINK");
-  dynamic_link.rel = "stylesheet";
-  dynamic_link.type = "text/css";
-  dynamic_link.href = sheet_name;
-  if(callback)
-  {
-    dynamic_link.addEventListener("load", callback);
-  }
-  document_head.appendChild(dynamic_link);
-}
-
 
 var change_detection_map = {};
 var interval_ids = null;
@@ -187,21 +115,6 @@ function monitor_changes()
       }
     }
 
-/*
-
-[
-{"id":"","edit_type":"json","metadata":null},
-{"id":"691a8194e2dcb8d0be248381e8a51593","edit_type":"javascript","metadata":"scripts/app.js"},
-{"id":"9bfe081c511cd92b2df43f6308f9edff","edit_type":"javascript","metadata":"scripts/data_access.js"},
-{"id":"e071abda8fe61194711cfc2ab99fe104","edit_type":"javascript","metadata":"scripts/jquery-3.1.1.min.js"},
-{"id":"b5af69f54ad79831b58f541b0525fefa","edit_type":"javascript","metadata":"scripts/profile.js"},
-{"id":"1d4375d962ccde7364cef5f9855b85e8","edit_type":"javascript","metadata":"scripts/editor/editor.js"},
-{"id":"371139b07850f44fa07b6f3fb2c1378c","edit_type":"javascript","metadata":"scripts/editor/page_renderer.js"},
-{"id":"009f6d333c375e15e57bd679b368f639","edit_type":"javascript","metadata":"scripts/editor/teeview.js"},
-{"id":"51df4194c5fce9d68ad8a46554d2aab3","edit_type":"javascript","metadata":"scripts/editor/tree_node.js"}
-]
-
-*/
     if(initial_setup)
     {
       console.log('initial setup');
---
+++ source-code/scratch/owin/owin/psk/app/scripts/print-version/print_version_renderer.js
@@ -0,0 +1,50 @@
+function print_version_render(p_metadata, p_path, p_ui)
+{
+	var result = [];
+	//switch(p_metadata.type.toLowerCase())
+	//{
+	//	case 'app':
+	//	default:
+				result.push('<table border=1>');
+				result.push('<tr><th colspan=2>');
+				result.push(p_path);
+				result.push('</th></tr>');
+				result.push('<tr><th>attribue</th><th>value</th></tr>');
+				result.push('<tr><td>name</td><td>');
+				result.push(p_metadata.name);
+				result.push('</td></tr>');
+				result.push('<tr><td>prompt</td><td>');
+				result.push(p_metadata.prompt);
+				result.push('</td></tr>');
+				result.push('<tr><td>type</td><td>');
+				result.push(p_metadata.type);
+				result.push('</td></tr>');
+				if(p_metadata.values)
+				{
+					result.push('<tr><td valign="center">values</td><td>');
+					for(var i = 0; i < p_metadata.values.length; i++)
+					{
+						var child = p_metadata.values[i];
+						result.push(child);
+						result.push('<br/>');
+					}
+					result.push('</td></tr>');
+				}
+				result.push("</table><br/>");
+
+
+				if(p_metadata.children)
+				{
+					for(var i = 0; i < p_metadata.children.length; i++)
+					{
+						var child = p_metadata.children[i];
+						Array.prototype.push.apply(result, print_version_render(child, p_path + "." + child.name, p_ui));
+					}
+				}
+				//break;
+			//console.log("editor_render not processed", p_metadata);
+	//}
+
+	return result;
+
+}
