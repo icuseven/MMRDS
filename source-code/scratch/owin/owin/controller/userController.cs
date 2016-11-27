5d452fafc908cbf537252ec6c72e9f7288d71db5
jhaines <jhaines@thetagrid.com>
2016-11-27 15:06:37 -0500

    Metadata backup. interim check-in on setting user manager.

---
+++ source-code/scratch/owin/owin/controller/userController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace owin
+{
+	public class userController: ApiController 
+	{ 
+		// GET api/values 
+		public IEnumerable<owin.model.couchdb.alldocs_response> Get() 
+		{ 
+			try
+			{
+				string request_string = this.get_couch_db_url() + "/_users";
+				System.Net.WebRequest request = System.Net.WebRequest.Create(new Uri(request_string));
+
+				request.PreAuthenticate = false;
+
+
+				if(this.Request.Headers.Contains("Cookie") && this.Request.Headers.GetValues("Cookie").Count() > 0)
+				{
+					string[] auth_session_token = this.Request.Headers.GetValues("Cookie").First().Split('=');
+					request.Headers.Add("Cookie", "AuthSession=" + auth_session_token[1]);
+					//request.Headers.Add(this.Request.Headers.GetValues("Cookie").First(), "");
+					request.Headers.Add("X-CouchDB-WWW-Authenticate", auth_session_token[1]);
+				}
+
+				System.Net.WebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+				System.IO.Stream dataStream = response.GetResponseStream ();
+				System.IO.StreamReader reader = new System.IO.StreamReader (dataStream);
+				string responseFromServer = reader.ReadToEnd ();
+				session_response json_result = Newtonsoft.Json.JsonConvert.DeserializeObject<session_response>(responseFromServer);
+			
+				session_response[] result =  new session_response[] 
+				{ 
+					json_result
+				}; 
+
+				return result;
+
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine (ex);
+
+			} 
+
+			return null;
+			return new owin.model.couchdb.user[] { default(owin.model.couchdb.user), default(owin.model.couchdb.user) }; 
+		} 
+
+		// GET api/values/5 
+		public owin.model.couchdb.user Get(string id) 
+		{ 
+			return default(owin.model.couchdb.user); 
+		} 
+
+		// POST api/values 
+		public void Post([FromBody]owin.model.couchdb.user value) 
+		{ 
+		} 
+
+		// PUT api/values/5 
+		public void Put(string id, [FromBody]owin.model.couchdb.user value) 
+		{ 
+		} 
+
+		// DELETE api/values/5 
+		public void Delete(string id) 
+		{ 
+		} 
+
+		private string get_couch_db_url()
+		{
+			string result = null;
+
+			if (bool.Parse (System.Configuration.ConfigurationManager.AppSettings ["is_container_based"])) 
+			{
+				result = System.Environment.GetEnvironmentVariable ("couchdb_url");
+			} 
+			else
+			{
+				result = System.Configuration.ConfigurationManager.AppSettings ["couchdb_url"];
+			}
+
+			return result;
+		}
+
+	} 
+}
+
---
+++ source-code/scratch/owin/owin/model/couchdb/alldocs_response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace owin.model.couchdb
+{
+	public class alldoc_item
+	{
+		public alldoc_item(){}
+
+		public string id { get; set; } //": "16e458537602f5ef2a710089dffd9453",
+		public string key { get; set; } //": "16e458537602f5ef2a710089dffd9453",
+		public struct value {  public string rev;};
+	
+	}
+
+	public class alldocs_response
+	{
+		public alldocs_response (){}
+
+		public int offset { get; set; } //": 0,
+		public alldoc[]_item rows { get; set; }
+		public int total_rows { get; set; } 
+	}
+}
+
