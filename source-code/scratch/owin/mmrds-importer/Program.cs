89003c72c4ac126ec12ebb4582987018a38c0e75
icuseven <jhaines@thetagrid.net>
2016-12-19 16:10:54 -0500

    interim check-in working on importer.

--- source-code/scratch/owin/mmrds-importer/Program.cs
+++ source-code/scratch/owin/mmrds-importer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+
 
 namespace importer
 {
@@ -6,6 +8,17 @@ namespace importer
 	{
 		public static void Main (string[] args)
 		{
+
+			var data = new cData ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Maternal_Mortlity.mdb;User ID=;Password=;");
+
+
+			var rs = data.GetDataTable ("Select * from AutopsyReport");
+
+			foreach (System.Data.DataRow row in rs.Rows) 
+			{
+				Console.WriteLine (row[1].ToString());
+			}
+
 			Console.WriteLine ("Hello World!");
 		}
 	}
