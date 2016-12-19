340cdab33690377da03861c79c33fb25ae27b51c
icuseven <jhaines@thetagrid.net>
2016-12-19 10:03:16 -0500

    Fixed core-summary to display grid and properly print values.

--- source-code/scratch/owin/owin/psk/app/scripts/core-summary/core_summary_renderer.js
+++ source-code/scratch/owin/owin/psk/app/scripts/core-summary/core_summary_renderer.js
@@ -12,17 +12,17 @@ function core_summary_render(p_metadata, p_data,  p_path, p_ui, p_is_core_summar
 	switch(p_metadata.type.toLowerCase())
 	{
 		case 'group':
-				if(p_metadata.is_core_summary || p_metadata.is_core_summary == true)
-				{
-									
+
+				/*									
 				result.push('<fieldset>');
-				//result.push(p_path)
+
 				result.push('<legend>')
 				result.push(p_metadata.prompt);
 				result.push('</legend> ');
-				//result.push(p_data[p_metadata.name]);
+				*/
 				
-
+				if(p_metadata.is_core_summary || p_metadata.is_core_summary == true)
+				{
 					is_core_summary = true;
 				}
 
@@ -35,7 +35,7 @@ function core_summary_render(p_metadata, p_data,  p_path, p_ui, p_is_core_summar
 						Array.prototype.push.apply(result, core_summary_render(child, p_data[child.name], p_path + "." + child.name, p_ui, is_core_summary));
 					}
 				}
-				result.push('</fieldset>');
+				//result.push('</fieldset>');
 				break;	
 		case 'form':
 				result.push('<section id="');
@@ -62,28 +62,46 @@ function core_summary_render(p_metadata, p_data,  p_path, p_ui, p_is_core_summar
 					is_core_summary == true
 				)
 				{
-					result.push('<p>');
-					//result.push(p_path)
-					result.push(' <strong>')
+					result.push('<table border=1>');
+					result.push('<tr><th colspan=')
+					result.push(p_metadata.children.length);
+					result.push('>');
 					result.push(p_metadata.prompt);
-					result.push('</strong>: ');
-					result.push(p_data[p_metadata.name]);
-					result.push('</p>');
-				}
+					result.push('</th></tr>');
 
-				if(p_metadata.is_core_summary || p_metadata.is_core_summary == true)
-				{
-					is_core_summary = true;
-				}
-
-				if(p_metadata.children)
-				{
-					for(var i = 0; i < p_metadata.children.length; i++)
+					result.push('<tr>');
+					for(var j = 0; j < p_metadata.children.length; j++)
 					{
-						var child = p_metadata.children[i];
-						if(p_data[child.name] != null)
-						Array.prototype.push.apply(result, core_summary_render(child, p_data[child.name], p_path + "." + child.name, p_ui, is_core_summary));
+						var child = p_metadata.children[j];
+						result.push('<td>');
+						result.push(child.name);
+						result.push('</td>');
 					}
+					result.push('</tr>');
+
+					for(var i = 0; i < p_data.length; i++)
+					{
+						if(p_data[i] != null)
+						{
+							result.push('<tr>');
+							for(var j = 0; j < p_metadata.children.length; j++)
+							{
+								var child = p_metadata.children[j];
+								result.push('<td>');
+								if(p_data[i][child.name] != null)
+								{
+									result.push(p_data[i][child.name]);
+								}
+								else
+								{
+									result.push("&nbsp;");
+								}
+								result.push('</td>');
+							}
+							result.push('</tr>');
+						}
+					}
+					result.push('</table>');
 				}
 				break;				
 		case 'app':	
@@ -110,7 +128,23 @@ function core_summary_render(p_metadata, p_data,  p_path, p_ui, p_is_core_summar
 						Array.prototype.push.apply(result, core_summary_render(child, p_data[child.name], p_path + "." + child.name, p_ui, is_core_summary));
 					}
 				}
-				break;				
+				break;		
+		case 'list':
+				if(
+					(p_metadata.is_core_summary || p_metadata.is_core_summary == true) ||
+					is_core_summary == true
+				)
+				{
+
+					result.push('<p>');
+					//result.push(p_path)
+					result.push(' <strong>')
+					result.push(p_metadata.prompt);
+					result.push('</strong>: ');
+					result.push(p_data);
+					result.push('</p>');
+				}
+			break;
 		default:
 				if(
 					(p_metadata.is_core_summary || p_metadata.is_core_summary == true) ||
@@ -123,7 +157,7 @@ function core_summary_render(p_metadata, p_data,  p_path, p_ui, p_is_core_summar
 					result.push(' <strong>')
 					result.push(p_metadata.prompt);
 					result.push('</strong>: ');
-					result.push(p_data[p_metadata.name]);
+					result.push(p_data);
 					result.push('</p>');
 				}
 
