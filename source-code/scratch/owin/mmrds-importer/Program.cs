641a33996062055baba38dc78ce9f9ca9eb3a39c
icuseven <jhaines@thetagrid.net>
2016-12-21 14:08:57 -0500

    Updated test db with test data. added csv read example.

--- source-code/scratch/owin/mmrds-importer/Program.cs
+++ source-code/scratch/owin/mmrds-importer/Program.cs
@@ -9,15 +9,44 @@ namespace mmria.console
 		public static void Main (string[] args)
 		{
 
-			var data = new cData ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Maternal_Mortlity.mdb;User ID=;Password=;");
+			var data = new cData ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=mapping-file-set/Maternal_Mortality.mdb;User ID=;Password=;");
 
 
 			var rs = data.GetDataTable ("Select * from AutopsyReport");
 
 			foreach (System.Data.DataRow row in rs.Rows) 
 			{
-				Console.WriteLine (row[1].ToString());
+				Console.WriteLine (row[2].ToString());
 			}
+
+			var filename = @"mapping-file-set/MMRDS-Mapping-NO-GRIDS-test.csv";
+			var connString = string.Format(
+				@"Provider=Microsoft.Jet.OleDb.4.0; Data Source={0};Extended Properties=""Text;HDR=YES;FMT=Delimited""",
+				System.IO.Path.GetDirectoryName(filename)
+			);
+
+			var data2 = new cData(connString);
+			var rs2 = data2.GetDataTable("SELECT * FROM [" + System.IO.Path.GetFileName(filename) + "]");
+
+			//Path,BaseTable,DataTablePath,f.Name,prompttext,ft.Name,DataType,MMRIA Path,MMRIA Group Name,Comments,
+
+
+			foreach (System.Data.DataRow row in rs2.Rows)
+			{
+				Console.WriteLine(row["path"].ToString());
+			}
+			/*
+			using (var conn = new System.Data.OleDb.OleDbConnection(connString))
+			{
+				conn.Open();
+				var query = "SELECT * FROM [" + System.IO.Path.GetFileName(filename) + "]";
+				using (var adapter = new System.Data.OleDb.OleDbDataAdapter(query, conn))
+				{
+					var ds = new DataSet("CSV File");
+					adapter.Fill(ds);
+				}
+			}*/
+
 
 			Console.WriteLine ("Hello World!");
 		}
