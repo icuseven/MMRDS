e5228f25aeb5a116ffe23c70cfe04e51cf381525
jhaines <jhaines@thetagrid.com>
2017-01-12 16:58:30 -0500

    interim check-in wix deployment.

--- source-code/scratch/owin/install.setup/Program.cs
+++ source-code/scratch/owin/install.setup/Program.cs
@@ -25,12 +25,18 @@ namespace install.setup
 
 
 			XElement ProductElement = wix_doc.XPathSelectElement("prefix:Wix/prefix:Product", namespaceManager);
+
+			ProductElement.Attribute("Id").SetValue(Guid.NewGuid().ToString());
+			ProductElement.Attribute("UpgradeCode").SetValue(Guid.NewGuid().ToString());
+
 			Console.WriteLine("Product");
 			Console.WriteLine(ProductElement.Attribute("Name"));
 			Console.WriteLine(ProductElement.Attribute("Id"));
 			Console.WriteLine(ProductElement.Attribute("UpgradeCode"));
 			Console.WriteLine(ProductElement.Attribute("Manufacturer"));
 			Console.WriteLine(ProductElement.Attribute("Version"));
+
+
 
 			XElement PackageElement = ProductElement.XPathSelectElement("./prefix:Package", namespaceManager);
 			Console.WriteLine("Package");
@@ -67,12 +73,20 @@ namespace install.setup
 			Console.WriteLine("Components");
 			foreach (XElement ComponentElement in ProductElement.XPathSelectElements(".//prefix:Component", namespaceManager))
 			{
+				ComponentElement.Attribute("Guid").SetValue(Guid.NewGuid().ToString());
+
 				print_xattribute(ComponentElement.Attribute("Id"), ComponentElement.Attribute("Guid"));
 				XElement FileElement = ComponentElement.XPathSelectElement("./prefix:File", namespaceManager);
 				if (FileElement != null)
 				{
 					print_xattribute(FileElement.Attribute("Id"), FileElement.Attribute("Name"));
 				}
+
+				/*
+				XElement new_node = new_file_node();
+				new_node.Add(new_short_cut_node());
+				ComponentElement.Add(new_node);
+				*/
 			}
 			Console.WriteLine("ComponentRefs");
 			foreach (XElement ComponentRefElement in ProductElement.XPathSelectElements(".//prefix:ComponentRef", namespaceManager))
@@ -80,6 +94,8 @@ namespace install.setup
 				print_xattribute(ComponentRefElement.Attribute("Id"));
 			}
 
+
+			wix_doc.Save("output.xml");
 
 			//var FieldsTypeIDs = from _FieldTypeID in wix_doc.Descendants("Field") select _FieldTypeID;
 
@@ -143,6 +159,62 @@ namespace install.setup
 
 			}
 
+		}
+
+
+		static public XElement new_file_node()
+		{
+			/*
+			  <File 
+				Id = 'FoobarEXE'
+				Name = 'FoobarAppl10.exe' 
+				DiskId = '1' 
+				Source = 'FoobarAppl10.exe' 
+				KeyPath = 'yes' >	
+			*/
+			//XNamespace ns = "http://schemas.microsoft.com/wix/2006/wi";
+			XElement result = new XElement
+				(
+				"File",
+				new XAttribute("Id", "FoobarEXE1"),
+				new XAttribute("Name", "FoobarAppl10"),
+				new XAttribute("DiskId", "1"),
+				new XAttribute("Source", "FoobarAppl10.exe"),
+				new XAttribute("KeyPath", "yes")
+				);
+
+
+				return result;
+		}
+
+
+		static public XElement new_short_cut_node()
+		{
+			/*
+			  <Shortcut
+			  	Id="desktopFoobar10"
+			  	Directory="DesktopFolder"
+			  	Name="Foobar 1.0"
+			  	WorkingDirectory="INSTALLDIR"
+			  	Icon="Foobar10.exe"
+			  	IconIndex="0"
+			  	Advertise="yes"
+			  	/>	
+			*/
+
+			XElement result = new XElement
+				(
+				"File",
+				new XAttribute("Id", "desktopFoobar10"),
+				new XAttribute("Directory", "DesktopFolder"),
+				new XAttribute("Name", "Foobar 1.0"),
+				new XAttribute("WorkingDirectory", "INSTALLDIR"),
+				new XAttribute("Icon", "Foobar10.exe"),
+				new XAttribute("IconIndex", "0"),
+				new XAttribute("Advertise", "yes")
+				);
+
+			return result;
 		}
 
 		static public string get_xml_template()
