ca205221c1f3f676630b7412f5dbf987349a9138
jhaines <jhaines@thetagrid.com>
2017-01-12 12:15:11 -0500

    Interim check-in stetting up wix install deployment.

--- source-code/scratch/owin/install.setup/Program.cs
+++ source-code/scratch/owin/install.setup/Program.cs
@@ -13,7 +13,7 @@ namespace install.setup
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Console.WriteLine("Hello World!");
 
 			string xml = get_xml_template();
 
@@ -24,25 +24,78 @@ namespace install.setup
 			namespaceManager.AddNamespace("prefix", "http://schemas.microsoft.com/wix/2006/wi");
 
 
-			XElement ViewElement = wix_doc.XPathSelectElement("prefix:Wix/prefix:Product", namespaceManager);
+			XElement ProductElement = wix_doc.XPathSelectElement("prefix:Wix/prefix:Product", namespaceManager);
+			Console.WriteLine("Product");
+			Console.WriteLine(ProductElement.Attribute("Name"));
+			Console.WriteLine(ProductElement.Attribute("Id"));
+			Console.WriteLine(ProductElement.Attribute("UpgradeCode"));
+			Console.WriteLine(ProductElement.Attribute("Manufacturer"));
+			Console.WriteLine(ProductElement.Attribute("Version"));
+
+			XElement PackageElement = ProductElement.XPathSelectElement("./prefix:Package", namespaceManager);
+			Console.WriteLine("Package");
+			Console.WriteLine(PackageElement.Attribute("Description"));
+			Console.WriteLine(PackageElement.Attribute("Comments"));
+			Console.WriteLine(PackageElement.Attribute("Manufacturer"));
+			//Console.WriteLine(PackageElement.Attribute("Manufacturer"));
+			//Console.WriteLine(PackageElement.Attribute("Version"));
+
+			XElement MediaElement = ProductElement.XPathSelectElement("./prefix:Media", namespaceManager);
+			Console.WriteLine("Media");
+			Console.WriteLine(MediaElement.Attribute("Cabinet"));
 
 
-			Console.WriteLine(ViewElement.Attribute("Name"));
-			Console.WriteLine(ViewElement.Attribute("Id"));
+			XElement PropertyElement = ProductElement.XPathSelectElement("./prefix:Property", namespaceManager);
+			Console.WriteLine("Property");
+			Console.WriteLine(PropertyElement.Attribute("Id"));
+			Console.WriteLine(PropertyElement.Attribute("Value"));
 
-			var FieldsTypeIDs = from _FieldTypeID in wix_doc.Descendants("Field") select _FieldTypeID;
+			XElement IconElement = ProductElement.XPathSelectElement("./prefix:Icon", namespaceManager);
+			Console.WriteLine("Icon");
+			print_xattribute(IconElement.Attribute("Id"), IconElement.Attribute("SourceFile"));
 
-			double width, height;
+
+			XElement DirectoryElement = ProductElement.XPathSelectElement("./prefix:Directory", namespaceManager);
+			Console.WriteLine("Directory");
+			print_xattribute(DirectoryElement.Attribute("Id"), DirectoryElement.Attribute("Name"));
+
+
+			XElement FeatureElement = ProductElement.XPathSelectElement("./prefix:Feature", namespaceManager);
+			Console.WriteLine("Feature");
+			print_xattribute(FeatureElement.Attribute("Id"), FeatureElement.Attribute("Level"));
+
+			Console.WriteLine("Components");
+			foreach (XElement ComponentElement in ProductElement.XPathSelectElements(".//prefix:Component", namespaceManager))
+			{
+				print_xattribute(ComponentElement.Attribute("Id"), ComponentElement.Attribute("Guid"));
+				XElement FileElement = ComponentElement.XPathSelectElement("./prefix:File", namespaceManager);
+				if (FileElement != null)
+				{
+					print_xattribute(FileElement.Attribute("Id"), FileElement.Attribute("Name"));
+				}
+			}
+			Console.WriteLine("ComponentRefs");
+			foreach (XElement ComponentRefElement in ProductElement.XPathSelectElements(".//prefix:ComponentRef", namespaceManager))
+			{
+				print_xattribute(ComponentRefElement.Attribute("Id"));
+			}
+
+
+			//var FieldsTypeIDs = from _FieldTypeID in wix_doc.Descendants("Field") select _FieldTypeID;
+
+			//double width, height;
 
 
 
-			string checkcode = ViewElement.Attribute("CheckCode").Value.ToString();
+			//string checkcode = ViewElement.Attribute("CheckCode").Value.ToString();
+
+			/*
+			
 			StringBuilder JavaScript = new StringBuilder();
 			StringBuilder VariableDefinitions = new StringBuilder();
 
 			XDocument xdocResponse = XDocument.Parse("");
 
-			/*
 			XDocMetadata.RequiredFieldsList = xdocResponse.Root.Attribute("RequiredFieldsList").Value;
 			XDocMetadata.HiddenFieldsList = xdocResponse.Root.Attribute("HiddenFieldsList").Value;
 			XDocMetadata.HighlightedFieldsList = xdocResponse.Root.Attribute("HighlightedFieldsList").Value;
@@ -50,6 +103,16 @@ namespace install.setup
 
 			*/
 
+		}
+
+		static private void print_xattribute(XAttribute value)
+		{
+			Console.WriteLine(string.Format("\t{0}", value));
+		}
+
+		static private void print_xattribute(XAttribute value_1, XAttribute value_2)
+		{
+			Console.WriteLine(string.Format("\t{0} {1}", value_1, value_2));
 		}
 
 		private void DeleteNonMaxRes(string ImageName, System.IO.DirectoryInfo directoryInfo)
