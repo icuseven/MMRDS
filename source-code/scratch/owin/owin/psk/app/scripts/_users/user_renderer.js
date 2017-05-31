50a83da1e8a53e922fb8936c3fc0695c08ed244d
BryanWilliams561 <bryan1williams@yahoo.com>
2017-05-31 10:36:42 -0400

    MMRDS-530 User Admin Page-Font correction and underscore removal on Admin page.

--- source-code/scratch/owin/owin/psk/app/scripts/_users/user_renderer.js
+++ source-code/scratch/owin/owin/psk/app/scripts/_users/user_renderer.js
@@ -30,7 +30,7 @@ function user_entry_render(p_user, p_i)
 	var result = [];
 
 	if(p_i % 2)
-	{
+	{l
 		result.push("<tr id='" +  convert_to_jquery_id(p_user._id) + "' style='background:#DDDDDD;' valign=top><td>");
 	}
 	else
@@ -41,12 +41,12 @@ function user_entry_render(p_user, p_i)
 	result.push(p_user.name);
 	result.push("</td><td>");
 	result.push("<table>");
-	result.push("<tr><th>Role name</th><th>&nbsp;</th><tr>");
+	result.push("<tr><th>Role Name</th><th>&nbsp;</th><tr>");
 	for(var j = 0; j < p_user.roles.length; j++)
 	{
 		result.push("<tr><td>");
 		result.push(p_user.roles[j]);
-		result.push("</td><td><input type='button' value='remove role' onclick='remove_role(\"" + p_user._id + "\",\"" + p_user.roles[j] + "\")'/></td></tr>");
+		result.push("</td><td><input type='button' value='Remove Role' onclick='remove_role(\"" + p_user._id + "\",\"" + p_user.roles[j] + "\")'/></td></tr>");
 	}
 	result.push("<tr><td colspan=2 align=right>");
 	Array.prototype.push.apply(result, user_role_render(p_user));
@@ -63,12 +63,12 @@ function user_entry_render(p_user, p_i)
 	result.push("<input type='button' value='change user name'/>");
 	result.push("<br/><br/>");
 	*/	
-	result.push("New password <input type='password' value='' role='confirm_1' path='" + p_user._id + "' />");
-	result.push("<br/>Verify password<input type='password' value='' role='confirm_2' path='" + p_user._id + "' />");
+	result.push("New Password <input type='password' value='' role='confirm_1' path='" + p_user._id + "' />");
+	result.push("<br/>Verify Password<input type='password' value='' role='confirm_2' path='" + p_user._id + "' />");
 	result.push("</td>");
 	result.push("<td>&nbsp;")
 	//result.push("<input type='button' value='disable user'/>");
-	result.push("<input type='button' value='Save user " + p_user.name + " changes' onclick='change_password_user_click(\"" + p_user._id + "\")'/>");
+	result.push("<input type='button' value='Save User " + p_user.name + " changes' onclick='change_password_user_click(\"" + p_user._id + "\")'/>");
 	
 	result.push("<span id='");
 	result.push(convert_to_jquery_id(p_user._id));
@@ -84,7 +84,7 @@ function user_entry_render(p_user, p_i)
 function user_role_render(p_user)
 {
 	var result = [];
-	var role_set = [ '', 'abstractor','committee_member','form_designer'];
+	var r_set = [ '', 'Abstractor','Committee Reviewer','Form Designer'];
 
 	result.push("<select size='1' path='" + p_user._id + "'>")
 	for(var i = 0; i < role_set.length; i++)
@@ -107,6 +107,6 @@ function user_role_render(p_user)
 		}
 	}
 	result.push("</select>");
-	result.push("<input type='button' value='add role' onclick='add_role(\"" + p_user._id + "\")' />");
+	result.push("<input type='button' value='Add Role' onclick='add_role(\"" + p_user._id + "\")' />");
 	return result;
 }
