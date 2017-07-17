b12cc529ec442fef9668e628daa81c251bb71a8a
icuseven <jhaines@thetagrid.net>
2017-07-17 14:58:28 -0400

    MMRDS-584 MMRDS-585 Added export_queueController and export_queue_item

---
+++ source-code/scratch/owin/owin/model/export_queue_item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mmria
+{
+	public class export_queue_item
+	{
+		public export_queue_item ()
+		{
+		}
+
+		DateTime date_created { get; set;}
+		string created_by { get; set;}
+		DateTime date_last_updated { get; set;}
+		string last_updated_by { get; set;}
+		string file_name { get; set;}
+		string export_type { get; set;}
+		string status { get; set;}
+	}
+}
+
