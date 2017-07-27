d4159c454e8ffeb89b3fc9734822620ef16855f3
icuseven <jhaines@thetagrid.com>
2017-07-27 09:52:54 -0400

    MMRDS-584 Fixed get error in export_queueController

--- source-code/scratch/owin/owin/controller/export_queueController.cs
+++ source-code/scratch/owin/owin/controller/export_queueController.cs
@@ -56,19 +56,26 @@ namespace mmria.server
 			
 
 					export_queue_item item = new export_queue_item();
+					try
+					{
+						item._id = doc_item ["_id"].ToString ();
+						item._rev = doc_item ["_rev"].ToString ();
+						item._deleted = doc_item .ContainsKey("_deleted") ? doc_item["_deleted"] as bool?: null;
+						item.date_created = doc_item["date_created"] as DateTime?;
+						item.created_by = doc_item.ContainsKey("created_by") ? doc_item["created_by"] as string: null;
+						item.date_last_updated = doc_item["date_last_updated"] as DateTime?;
+						item.last_updated_by = doc_item.ContainsKey("last_updated_by")? doc_item["last_updated_by"] as string : null;
+						item.file_name = doc_item["file_name"] != null? doc_item["file_name"].ToString() : null;
+						item.export_type = doc_item["export_type"] != null? doc_item["export_type"].ToString() : null;
+						item.status = doc_item["status"] != null? doc_item["status"].ToString() : null;
+					
 
-					item._id = doc_item ["_id"].ToString ();
-					item._rev = doc_item ["_rev"].ToString ();
-					item._deleted = doc_item .ContainsKey("_deleted") ? doc_item["_deleted"] as bool?: null;
-					item.date_created = doc_item["date_created"] as DateTime?;
-					item.created_by = doc_item["created_by"] != null ? doc_item["created_by"].ToString() : null;
-					item.date_last_updated = doc_item["date_last_updated"] as DateTime?;
-					item.last_updated_by = doc_item["last_updated_by"] != null? doc_item["last_updated_by"].ToString() : null;
-					item.file_name = doc_item["file_name"] != null? doc_item["file_name"].ToString() : null;
-					item.export_type = doc_item["export_type"] != null? doc_item["export_type"].ToString() : null;
-					item.status = doc_item["status"] != null? doc_item["status"].ToString() : null;
-
-					result.Add(item);
+						result.Add(item);
+					}
+					catch(Exception ex)
+					{
+						// do nothing for now
+					}
 				
 				}
 
