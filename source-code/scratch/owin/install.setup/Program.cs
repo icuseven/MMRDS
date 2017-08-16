c0d39ef87a17fc8eb5eafb7702bfa9fba3d5966c
icuseven <jhaines@thetagrid.com>
2017-08-16 13:16:31 -0400

    MMRDS-619 - updated download.html and upgrade.html with info for MMRIA-17.08.16-7cf3f64-Install.msi

--- source-code/scratch/owin/install.setup/Program.cs
+++ source-code/scratch/owin/install.setup/Program.cs
@@ -356,8 +356,8 @@ namespace install.setup
 					new XAttribute("Id", "MainExecutable"),
 					new XAttribute("Guid", get_id(p_file_info.FullName)),
 					new_file_node(p_file_info),
-					get_shortcut("startmenummria17.08.15_v_9c5e9ea_", "ProgramMenuDir", "MMRIA 17.08.15 v(9c5e9ea)", "mmria_server.exe"),
-					get_shortcut("desktopmmria17.08.15_v_9c5e9ea_", "DesktopFolder", "MMRIA 17.08.15 v(9c5e9ea)", "mmria_server.exe")
+					get_shortcut("startmenummria17.08.16_v_7cf3f64_", "ProgramMenuDir", "MMRIA 17.08.16 v(7cf3f64)", "mmria_server.exe"),
+					get_shortcut("desktopmmria17.08.16_v_7cf3f64_", "DesktopFolder", "MMRIA 17.08.16 v(7cf3f64)", "mmria_server.exe")
 				);
 			/*
 		            <Component Id = 'MainExecutable' Guid='YOURGUID-83F1-4F22-985B-FDB3C8ABD471'>
