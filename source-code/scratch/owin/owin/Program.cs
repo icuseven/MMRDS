0c85eb95f28af05b92d64c04cf623aef0436f28e
icuseven <jhaines@thetagrid.com>
2017-09-18 12:55:47 -0400

    MMRDS-647 Improving start up to handle missing database tables.

--- source-code/scratch/owin/owin/Program.cs
+++ source-code/scratch/owin/owin/Program.cs
@@ -228,66 +228,108 @@ namespace mmria.server
 			DateTimeOffset? ft = sched.ScheduleJob (data_job, trigger);
 
 
-			if (
-				database_exists (Program.config_couchdb_url + "/mmrds", Program.config_timer_user_name, Program.config_timer_password) &&
-				database_exists (Program.config_couchdb_url + "/metadata", Program.config_timer_user_name, Program.config_timer_password))
-			{
+            /*if (!url_exists (Program.config_couchdb_url, Program.config_timer_user_name, Program.config_timer_password, "GET")) 
+            {
+                
+                //curl -X PUT $HOST/_config/admins/anna -d '"secret"'
+                new cURL ("PUT", null, Program.config_couchdb_url + "/_config/", null).execute ();
+                //new cURL ("PUT", null, Program.config_couchdb_url + "/_config/admins", null).execute ();
+                new cURL ("PUT", null, Program.config_couchdb_url + "/_config/admins/" + Program.config_timer_user_name, Program.config_timer_password).execute ();
 
-				if (!database_exists (Program.config_couchdb_url + "/export_queue", Program.config_timer_user_name, Program.config_timer_password))
-				{
-					System.Console.WriteLine("Creating export_queue db.");
-					var export_queue_curl = new cURL ("PUT", null, Program.config_couchdb_url + "/export_queue", null, Program.config_timer_user_name, Program.config_timer_password);
-					System.Console.WriteLine(export_queue_curl.execute ());
-					new cURL ("PUT", null, Program.config_couchdb_url + "/export_queue/_security", "{\"admins\":{\"names\":[],\"roles\":[\"abstractor\"]},\"members\":{\"names\":[],\"roles\":[\"abstractor\"]}}", Program.config_timer_user_name, Program.config_timer_password).execute();
-				}
+                if (!url_exists (Program.config_couchdb_url + "/_config", Program.config_timer_user_name, Program.config_timer_password)) {
+                    try {
 
-				var sync_curl = new cURL ("GET", null, Program.config_couchdb_url + "/mmrds/_changes", null, Program.config_timer_user_name, Program.config_timer_password);
-				string res = sync_curl.execute ();
-				mmria.server.model.couchdb.c_change_result latest_change_set = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.server.model.couchdb.c_change_result> (res);
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/couch_httpd_auth/allow_persistent_cookies/true", null, Program.config_timer_user_name, "\"" + Program.config_timer_password + "\"").execute ();
 
-				Program.Last_Change_Sequence = latest_change_set.last_seq;
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/httpd/enable_cors/true", null, Program.config_timer_user_name, Program.config_timer_password).execute ();
 
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/cors/credentials/true", null, Program.config_timer_user_name, Program.config_timer_password).execute ();
 
-				System.Threading.Tasks.Task.Run
-				(
-					new Action (() =>
-					{
-						mmria.server.util.c_document_sync_all sync_all = new mmria.server.util.c_document_sync_all (
-																			 Program.config_couchdb_url,
-																			 Program.config_timer_user_name,
-																			 Program.config_timer_password
-																		 );
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/cors/headers/accept,authorization,content-type,origin,referer,cache-control, x-requested-with", null, Program.config_timer_user_name, Program.config_timer_password).execute ();
 
-						sync_all.execute ();
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/cors/methods/GET,PUT,POST,HEAD,DELETE", null, Program.config_timer_user_name, Program.config_timer_password).execute ();
 
+                        new cURL ("PUT", null, Program.config_couchdb_url + "/_config/cors/origins/*", null, Program.config_timer_user_name, Program.config_timer_password).execute ();
+                    } catch (Exception ex) {
+                        System.Console.WriteLine ("config error", ex.ToString ());
+                    }
+                }
+            }*/
 
-/*
-{"total_rows":11,"offset":0,"rows":[
-{"id":"02279162-6be3-49e4-930f-42eed7cd4706","key":"02279162-6be3-49e4-930f-42eed7cd4706","value":{"rev":"1-1e8c9c42f75d1582c7d2261230268f0a"}},
-{"id":"140836d7-abed-07ff-5b84-72a9ca30b9c4","key":"140836d7-abed-07ff-5b84-72a9ca30b9c4","value":{"rev":"1-7d713a250c1dd52843724df2e909841f"}},
-{"id":"2243372a-9801-155c-4098-9540daabe76c","key":"2243372a-9801-155c-4098-9540daabe76c","value":{"rev":"1-d7b3cb2bbddfa7dab44161b745ba3f2c"}},
-{"id":"244da20f-41cc-4300-ad94-618004a51917","key":"244da20f-41cc-4300-ad94-618004a51917","value":{"rev":"1-3930c68b758258af365bda35aee22731"}},
-{"id":"999907aa-8b73-3cfa-f13b-657beb325428","key":"999907aa-8b73-3cfa-f13b-657beb325428","value":{"rev":"1-1e3bac81a24f00755613f0f7d2604fcb"}},
-{"id":"acbf75d5-9c7a-57bc-9bef-59624bac7847","key":"acbf75d5-9c7a-57bc-9bef-59624bac7847","value":{"rev":"1-6f758041b4fb6954ec5ff4a52cc57eda"}},
-{"id":"b5003bc5-1ab3-4ba2-8aea-9f3717c9682a","key":"b5003bc5-1ab3-4ba2-8aea-9f3717c9682a","value":{"rev":"1-ab8dc8c5852d0e053683d64ee7c5e9ba"}},
-{"id":"d0e08da8-d306-4a9a-a5ff-9f1d54702091","key":"d0e08da8-d306-4a9a-a5ff-9f1d54702091","value":{"rev":"1-bbddad634887348768fa8badc4db5ded"}},
-{"id":"e28af3a7-b512-d1b4-d257-19f2fabeb14d","key":"e28af3a7-b512-d1b4-d257-19f2fabeb14d","value":{"rev":"1-a9ce1f6a0be2416e2ff06ef9adb1bd0e"}},
-{"id":"e98ce2be-4446-439a-bb63-d9b4e690e3c3","key":"e98ce2be-4446-439a-bb63-d9b4e690e3c3","value":{"rev":"1-297a418df441f52109714fdc3b21bd07"}},
-{"id":"f6660468-ec54-a569-9903-a6682c5881d6","key":"f6660468-ec54-a569-9903-a6682c5881d6","value":{"rev":"1-113fa14b491002aa951616627cb35562"}}
-]}
-*/
+            if (url_exists (Program.config_couchdb_url, Program.config_timer_user_name, Program.config_timer_password, "GET")) 
+            {
+
+                if (!url_exists (Program.config_couchdb_url + "/metadata", Program.config_timer_user_name, Program.config_timer_password)) 
+                {
+                    var metadata_curl = new cURL ("PUT", null, Program.config_couchdb_url + "/metadata", null, Program.config_timer_user_name, Program.config_timer_password);
+                    System.Console.WriteLine ("metadata_curl", metadata_curl.execute ());
+                    new cURL ("PUT", null, Program.config_couchdb_url + "/metadata/_security", "{\"admins\":{\"names\":[],\"roles\":[\"form_designer\"]},\"members\":{\"names\":[],\"roles\":[]}}", Program.config_timer_user_name, Program.config_timer_password).execute ();
+                    System.Console.WriteLine ("metadata/_security", "completed successfully");
+                } 
 
 
+                if (!url_exists (Program.config_couchdb_url + "/mmrds", Program.config_timer_user_name, Program.config_timer_password))
+                {
+                    var mmrds_curl = new cURL ("PUT", null, Program.config_couchdb_url + "/mmrds", null, Program.config_timer_user_name, Program.config_timer_password);
+                    System.Console.WriteLine ("mmrds_curl", mmrds_curl.execute ());
+                    new cURL ("PUT", null, Program.config_couchdb_url + "/mmrds/_security", "{\"admins\":{\"names\":[],\"roles\":[\"form_designer\"]},\"members\":{\"names\":[],\"roles\":[\"abstractor\",\"data_analyst\",\"timer\"]}}", Program.config_timer_user_name, Program.config_timer_password).execute ();
+                    System.Console.WriteLine ("mmrds/_security", "completed successfully");
+                }
+
+                if (!url_exists (Program.config_couchdb_url + "/export_queue", Program.config_timer_user_name, Program.config_timer_password)) 
+                {
+                    System.Console.WriteLine ("Creating export_queue db.");
+                    var export_queue_curl = new cURL ("PUT", null, Program.config_couchdb_url + "/export_queue", null, Program.config_timer_user_name, Program.config_timer_password);
+                    System.Console.WriteLine (export_queue_curl.execute ());
+                    new cURL ("PUT", null, Program.config_couchdb_url + "/export_queue/_security", "{\"admins\":{\"names\":[],\"roles\":[\"abstractor\"]},\"members\":{\"names\":[],\"roles\":[\"abstractor\"]}}", Program.config_timer_user_name, Program.config_timer_password).execute ();
+                }
+
+                var sync_curl = new cURL ("GET", null, Program.config_couchdb_url + "/mmrds/_changes", null, Program.config_timer_user_name, Program.config_timer_password);
+                string res = sync_curl.execute ();
+                mmria.server.model.couchdb.c_change_result latest_change_set = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.server.model.couchdb.c_change_result> (res);
+
+                Program.Last_Change_Sequence = latest_change_set.last_seq;
+
+
+                System.Threading.Tasks.Task.Run
+                (
+                    new Action (() => {
+                        mmria.server.util.c_document_sync_all sync_all = new mmria.server.util.c_document_sync_all (
+                                                                             Program.config_couchdb_url,
+                                                                             Program.config_timer_user_name,
+                                                                             Program.config_timer_password
+                                                                         );
+
+                        sync_all.execute ();
+
+
+                    /*
+                    {"total_rows":11,"offset":0,"rows":[
+                    {"id":"02279162-6be3-49e4-930f-42eed7cd4706","key":"02279162-6be3-49e4-930f-42eed7cd4706","value":{"rev":"1-1e8c9c42f75d1582c7d2261230268f0a"}},
+                    {"id":"140836d7-abed-07ff-5b84-72a9ca30b9c4","key":"140836d7-abed-07ff-5b84-72a9ca30b9c4","value":{"rev":"1-7d713a250c1dd52843724df2e909841f"}},
+                    {"id":"2243372a-9801-155c-4098-9540daabe76c","key":"2243372a-9801-155c-4098-9540daabe76c","value":{"rev":"1-d7b3cb2bbddfa7dab44161b745ba3f2c"}},
+                    {"id":"244da20f-41cc-4300-ad94-618004a51917","key":"244da20f-41cc-4300-ad94-618004a51917","value":{"rev":"1-3930c68b758258af365bda35aee22731"}},
+                    {"id":"999907aa-8b73-3cfa-f13b-657beb325428","key":"999907aa-8b73-3cfa-f13b-657beb325428","value":{"rev":"1-1e3bac81a24f00755613f0f7d2604fcb"}},
+                    {"id":"acbf75d5-9c7a-57bc-9bef-59624bac7847","key":"acbf75d5-9c7a-57bc-9bef-59624bac7847","value":{"rev":"1-6f758041b4fb6954ec5ff4a52cc57eda"}},
+                    {"id":"b5003bc5-1ab3-4ba2-8aea-9f3717c9682a","key":"b5003bc5-1ab3-4ba2-8aea-9f3717c9682a","value":{"rev":"1-ab8dc8c5852d0e053683d64ee7c5e9ba"}},
+                    {"id":"d0e08da8-d306-4a9a-a5ff-9f1d54702091","key":"d0e08da8-d306-4a9a-a5ff-9f1d54702091","value":{"rev":"1-bbddad634887348768fa8badc4db5ded"}},
+                    {"id":"e28af3a7-b512-d1b4-d257-19f2fabeb14d","key":"e28af3a7-b512-d1b4-d257-19f2fabeb14d","value":{"rev":"1-a9ce1f6a0be2416e2ff06ef9adb1bd0e"}},
+                    {"id":"e98ce2be-4446-439a-bb63-d9b4e690e3c3","key":"e98ce2be-4446-439a-bb63-d9b4e690e3c3","value":{"rev":"1-297a418df441f52109714fdc3b21bd07"}},
+                    {"id":"f6660468-ec54-a569-9903-a6682c5881d6","key":"f6660468-ec54-a569-9903-a6682c5881d6","value":{"rev":"1-113fa14b491002aa951616627cb35562"}}
+                    ]}
+                    */
 
 
 
 
-						Program.sched.Start ();
-					}
-					)
-				);
 
-			}
+
+                        Program.sched.Start ();
+                    }
+                    )
+                );
+                    
+            }
 
 			// ****   Quartz Timer - End
 
@@ -331,11 +373,12 @@ namespace mmria.server
             }
             System.Console.WriteLine ("Quit command recieved shutting down.");
         }
-		private static bool database_exists(string p_target_server, string p_user_name, string p_password)
+
+		private static bool url_exists(string p_target_server, string p_user_name, string p_password, string p_method="HEAD")
 		{
 			bool result = false;
 
-			var curl = new cURL ("HEAD", null, p_target_server, null, p_user_name, p_password);	 
+            var curl = new cURL (p_method, null, p_target_server, null, p_user_name, p_password);	 
 			try
 			{
 				curl.execute();
@@ -355,6 +398,9 @@ namespace mmria.server
 
 			return result;
 		}
+
+
+
 
 
 		public static void StartSchedule ()
