d858d1eae81d2dd47081ad769e47110ba9d0eaba
icuseven <jhaines@thetagrid.com>
2017-10-25 11:31:43 -0400

    UI Tweaks - Added "Page # of #" and fixed Records option bug.

--- source-code/scratch/owin/owin/psk/app/scripts/editor/page_renderer.js
+++ source-code/scratch/owin/owin/psk/app/scripts/editor/page_renderer.js
@@ -694,7 +694,7 @@ function page_render(p_metadata, p_data, p_ui, p_metadata_path, p_object_path, p
 			}
 			else
 			{
-				result.push("<option>400</option>");
+				result.push("<option>500</option>");
 				
 			}
 			result.push("</select>");
@@ -852,6 +852,11 @@ function page_render(p_metadata, p_data, p_ui, p_metadata_path, p_object_path, p
 			result.push('		</table>');
 
 			result.push('<hr/>')
+			result.push("Page: ");
+			result.push(p_ui.case_view_request.page);
+			result.push(" of ")
+			result.push(current_page - 1);
+			result.push("<br/>");
 			result.push("Records per page: <select id='search_records_per_page' onchange='g_ui.case_view_request.take = this.value;' >");
 			if(p_ui.case_view_request.take==25)
 			{
