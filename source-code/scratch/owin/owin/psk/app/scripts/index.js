9edb0e69129a680d311c459b195c5f4d2a093067
icuseven <jhaines@thetagrid.com>
2018-01-12 15:30:16 -0500

    MMRDS-739 Interim Check-in added idle-timer.js.

--- source-code/scratch/owin/owin/psk/app/scripts/index.js
+++ source-code/scratch/owin/owin/psk/app/scripts/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//@ts-check
 //http://www.w3schools.com/css/css3_flexbox.asp
 
 var g_metadata = null;
@@ -405,13 +405,89 @@ $(function ()
 
 	});
 
+  // https://pure-essence.net/2010/02/14/jquery-session-timeout-countdown/
+  // create the warning window and set autoOpen to false
+  var sessionTimeoutWarningDialog = $("#sessionTimeoutWarning");
+  $(sessionTimeoutWarningDialog).html(initialSessionTimeoutMessage);
+  $(sessionTimeoutWarningDialog).dialog({
+      title: 'Session Expiration Warning',
+      autoOpen: false,    // set this to false so we can manually open it
+      closeOnEscape: false,
+      draggable: false,
+      width: 460,
+      minHeight: 50,
+      modal: true,
+      beforeclose: function() { // bind to beforeclose so if the user clicks on the "X" or escape to close the dialog, it will work too
+          // stop the timer
+          clearInterval(timer);
+
+          // stop countdown
+          running = false;
+
+          // ajax call to keep the server-side session alive
+          $.ajax({
+            url: keepAliveURL,
+            async: false
+          });
+      },
+      buttons: {
+          OK: function() {
+              // close dialog
+              $(this).dialog('close');
+          }
+      },
+      resizable: false,
+      open: function() {
+          // scrollbar fix for IE
+          $('body').css('overflow','hidden');
+      },
+      close: function() {
+          // reset overflow
+          $('body').css('overflow','auto');
+      }
+  });
+  // end of dialog
+
+
+  // start the idle timer
+  //$.idleTimer(idleTime);
+
+  // bind to idleTimer's idle.idleTimer event
+  $(document).bind("idle.idleTimer", function()
+  {
+      // if the user is idle and a countdown isn't already running
+      if($.data(document,'idleTimer') === 'idle' && !running){
+          var counter = redirectAfter;
+          running = true;
+
+          // intialisze timer
+          $('#'+sessionTimeoutCountdownId).html(redirectAfter);
+          // open dialog
+          $(sessionTimeoutWarningDialog).dialog('open');
+
+          // create a timer that runs every second
+          timer = setInterval(function(){
+              counter -= 1;
+
+              // if the counter is 0, redirect the user
+              if(counter === 0) {
+                  $(sessionTimeoutWarningDialog).html(expiredMessage);
+                  $(sessionTimeoutWarningDialog).dialog('disable');
+                  window.location = redirectTo;
+              } else {
+                  $('#'+sessionTimeoutCountdownId).html(counter);
+              };
+          }, 1000);
+      };
+  });
+
+
+
+
+
   $.datetimepicker.setLocale('en');
 
   load_values();
-
-
-
-
 });
 
 function load_values()
@@ -1257,3 +1333,12 @@ function undo_click()
 
   g_render();
 }
+
+
+var idleTime = 2000; // number of miliseconds until the user is considered idle
+var initialSessionTimeoutMessage = 'Your session will expire in <span id="sessionTimeoutCountdown"></span> seconds.<br /><br />Click on <b>OK</b> to continue your session.';
+var sessionTimeoutCountdownId = 'sessionTimeoutCountdown';
+var redirectAfter = 10; // number of seconds to wait before redirecting the user
+var redirectTo = 'http://regretless.com/2010/02/14/jquery-session-timeout-countdown/'; // URL to relocate the user to once they have timed out
+var keepAliveURL = 'keepAlive.php'; // URL to call to keep the session alive
+var expiredMessage = 'Your session has expired.  You are being logged out for security reasons.'; // message to show user when the countdown reaches 0
