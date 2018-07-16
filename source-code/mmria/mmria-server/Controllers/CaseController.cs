1ed6b07ce155aa6acb9d29bddffa44a2b77abdaa
jhaines <jhaines@thetagrid.com>
2018-07-16 07:51:06 -0400

    MMRDS-750 Added CaseController and View

---
+++ source-code/mmria/mmria-server/Controllers/CaseController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers
+{
+    //[Authorize(Policy = "EmployeeId")]
+    //[Authorize(Policy = "Over21Only")]
+    //[Authorize(Policy = "BuildingEntry")]
+    
+    public class CaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
