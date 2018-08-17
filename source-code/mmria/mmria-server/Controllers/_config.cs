e296ea3fd20567e253f2d2e3b73d6ff7939f8ce1
jhaines <jhaines@thetagrid.com>
2018-08-16 21:21:09 -0400

    MMRDS-821 setting up _config UI

--- source-code/mmria/mmria-server/Controllers/_config.cs
+++ source-code/mmria/mmria-server/Controllers/_config.cs
@@ -41,7 +41,37 @@ namespace mmria.server.Controllers
             app_config.EMAIL_FROM = configuration["mmria_settings:EMAIL_FROM"];
             app_config.EMAIL_PASSWORD = configuration["mmria_settings:EMAIL_PASSWORD"];
 
-            return View();
+            return View(app_config);
+        }
+
+
+[HttpPost]
+        public IActionResult Index(mmria.server.model.app_config app_config)
+        {
+            
+            //var app_config = new mmria.server.model.app_config();
+
+            configuration["mmria_settings:is_environment_based"] = app_config.is_environment_based.Value.ToString();
+            configuration["mmria_settings:web_site_url"] = app_config.web_site_url;
+            configuration["mmria_settings:log_directory"] = app_config.log_directory;
+            configuration["mmria_settings:export_directory"] = app_config.export_directory;
+            configuration["mmria_settings:couchdb_url"] = app_config.couchdb_url;
+            configuration["mmria_settings:timer_user_name"] = app_config.timer_user_name;
+            configuration["mmria_settings:timer_password"] = app_config.timer_password;
+            configuration["mmria_settings:cron_schedule"] = app_config.cron_schedule;
+            configuration["mmria_settings:password_minimum_length"] = app_config.password_minimum_length.Value.ToString();
+            configuration["mmria_settings:password_days_before_expires"] = app_config.password_days_before_expires.Value.ToString();
+            configuration["mmria_settings:password_days_before_user_is_notified_of_expiration"] = app_config.password_days_before_user_is_notified_of_expiration.Value.ToString();
+            configuration["mmria_settings:EMAIL_USE_AUTHENTICATION"] = app_config.EMAIL_USE_AUTHENTICATION.Value.ToString();
+            configuration["mmria_settings:EMAIL_USE_SSL"] = app_config.EMAIL_USE_SSL.Value.ToString();
+            configuration["mmria_settings:SMTP_HOST"] = app_config.SMTP_HOST;
+            configuration["mmria_settings:SMTP_PORT"] = app_config.SMTP_PORT.ToString();
+            configuration["mmria_settings:EMAIL_FROM"] = app_config.EMAIL_FROM;
+            configuration["mmria_settings:EMAIL_PASSWORD"] = app_config.EMAIL_PASSWORD;
+            
+            
+
+            return View(app_config);
         }
 
     }
