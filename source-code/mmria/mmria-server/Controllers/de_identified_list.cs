3de413d17ddecd8cfbe73dd0318cb222a69e4947
jhaines <jhaines@thetagrid.com>
2018-08-29 13:10:16 -0400

    MMRDS-859 added recover case and de-identified list UI and navigation

---
+++ source-code/mmria/mmria-server/Controllers/de_identified_list.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers
+{
+    [Authorize(Roles  = "committee_member")]
+    [Route("de-identified-list")]
+    //[Authorize(Policy = "Over21Only")]
+    //[Authorize(Policy = "BuildingEntry")]
+    //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/resourcebased?view=aspnetcore-2.1&tabs=aspnetcore2x
+    public class de_identified_listController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+        //private readonly IDocumentRepository _documentRepository;
+
+        public de_identified_listController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+            //_documentRepository = documentRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ source-code/mmria/mmria-server/Controllers/recover_case.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers
+{
+    [Authorize(Roles  = "committee_member")]
+    [Route("recover-case")]
+    //[Authorize(Policy = "Over21Only")]
+    //[Authorize(Policy = "BuildingEntry")]
+    //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/resourcebased?view=aspnetcore-2.1&tabs=aspnetcore2x
+    public class recover_caseController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+        //private readonly IDocumentRepository _documentRepository;
+
+        public recover_caseController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+            //_documentRepository = documentRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
