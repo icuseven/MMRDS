e3ba06b295063026dbba4d3045bb71b06ee27285
jhaines <jhaines@thetagrid.com>
2018-10-03 23:48:55 -0400

    MMRDS-832 Implemented Lockout based on Configuration Settings.

--- source-code/mmria/mmria-server/Controllers/AccountController.cs
+++ source-code/mmria/mmria-server/Controllers/AccountController.cs
@@ -118,34 +118,46 @@ namespace mmria.server.Controllers
 
                 try
                 {
-                    var session_event_request_url = $"{Program.config_couchdb_url}/session/_design/session_event_sortable/_view/by_date_created_user_id?startkey=[" + "{}" + $",\"{user.UserName}\"]&decending=true&limit={unsuccessful_login_attempts_number_before_lockout}";
+                    //var session_event_request_url = $"{Program.config_couchdb_url}/session/_design/session_event_sortable/_view/by_date_created_user_id?startkey=[" + "{}" + $",\"{user.UserName}\"]&decending=true&limit={unsuccessful_login_attempts_number_before_lockout}";
+                    var session_event_request_url = $"{Program.config_couchdb_url}/session/_design/session_event_sortable/_view/by_user_id?startkey=\"{user.UserName}\"&endkey=\"{user.UserName}\"";
 
                     var session_event_curl = new cURL("GET", null, session_event_request_url, null, Program.config_timer_user_name, Program.config_timer_password);
                     string response_from_server = await session_event_curl.executeAsync ();
 
-                    var session_event_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_sortable_view_reponse_object_key_header<mmria.common.model.couchdb.session_event>>(response_from_server);
+                    //var session_event_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_sortable_view_reponse_object_key_header<mmria.common.model.couchdb.session_event>>(response_from_server);
+                    var session_event_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_sortable_view_reponse_header<mmria.common.model.couchdb.session_event>>(response_from_server);
 
-                    DateTime first_item_date = DateTime.MinValue;
+                    DateTime first_item_date = DateTime.Now;
+                    DateTime last_item_date = DateTime.Now;
 
-                    var Consecutive_Fail_Count = 0;
+                    var MaxRange = DateTime.Now.AddMinutes(-unsuccessful_login_attempts_within_number_of_minutes);
+                    session_event_response.rows.Sort(new mmria.common.model.couchdb.Compare_Session_Event_By_DateCreated<mmria.common.model.couchdb.session_event>());
 
-                    for(int i = 0; i < session_event_response.rows.Count; i++)
+                    foreach(var session_event in session_event_response.rows.Where(row=> row.value.date_created >= MaxRange))
                     {
-                        var session_event = session_event_response.rows[i];
-
-                        if(i == 0)
-                        {
-                            first_item_date = session_event.value.date_created;
-                        }
-
                         if(session_event.value.action_result == mmria.common.model.couchdb.session_event.session_event_action_enum.failed_login)
                         {
-                            Consecutive_Fail_Count++;
+                            
                             failed_login_count++;
+                            if(failed_login_count==1)
+                            {
+                                first_item_date = session_event.value.date_created;
+                            }
+                            
+                            if(failed_login_count >= unsuccessful_login_attempts_number_before_lockout)
+                            {
+                                last_item_date= session_event.value.date_created;
+                                var grace_period_date = first_item_date.AddMinutes(unsuccessful_login_attempts_lockout_number_of_minutes);
+                                if(DateTime.Now < grace_period_date)
+                                {
+                                    is_locked_out = true;
+                                    break;
+                                }
+                            }
                         }
                         else
                         {
-
+                            break;
                         }
                     }
                 }
@@ -154,155 +166,103 @@ namespace mmria.server.Controllers
                     System.Console.WriteLine ($"{ex}");
                 }
 
-                if(failed_login_count >=5)
-                {
-                    is_locked_out = true;
-                }
-
-
-
-                string post_data = string.Format ("name={0}&password={1}", user.UserName, user.Password);
-				byte[] post_byte_array = System.Text.Encoding.ASCII.GetBytes(post_data);
-
-				string request_string = Program.config_couchdb_url + "/_session";
-				System.Net.WebRequest request = System.Net.WebRequest.Create(new Uri(request_string));
-				//request.UseDefaultCredentials = true;
-
-				request.PreAuthenticate = false;
-				//request.Credentials = new System.Net.NetworkCredential("mmrds", "mmrds");
-				request.Method = "POST";
-				request.ContentType = "application/x-www-form-urlencoded";
-				request.ContentLength = post_byte_array.Length;
-
-				using (System.IO.Stream stream = request.GetRequestStream())
-				{
-					stream.Write(post_byte_array, 0, post_byte_array.Length);
-				}
-
-				System.Net.WebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
-				System.IO.Stream dataStream = response.GetResponseStream ();
-				System.IO.StreamReader reader = new System.IO.StreamReader (dataStream);
-				string responseFromServer = await reader.ReadToEndAsync ();
-
-				mmria.common.model.couchdb.login_response json_result = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.login_response>(responseFromServer);
-
-				mmria.common.model.couchdb.login_response[] result =  new mmria.common.model.couchdb.login_response[] 
-				{ 
-					json_result
-				}; 
-
-
-				this.Response.Headers.Add("Set-Cookie", response.Headers["Set-Cookie"]);
-
-				string[] set_cookie = response.Headers["Set-Cookie"].Split(';');
-				string[] auth_array = set_cookie[0].Split('=');
-				if(auth_array.Length > 1)
-				{
-					string auth_session_token = auth_array[1];
-					result[0].auth_session = auth_session_token;
-				}
-				else
-				{
-					result[0].auth_session = "";
-				}
-
-				//{"ok":true,"userCtx":{"name":null,"roles":[]},"info":{"authentication_db":"_users","authentication_handlers":["oauth","cookie","default"]}}
-				if (json_result.ok && !string.IsNullOrWhiteSpace(json_result.name)) 
-				{
-/*
-                    //string jurisdiction_url = $"{Program.config_couchdb_url}/jurisdiction/_design/sortable/_view/by_user_id?" + $"/jurisdiction/" + json_result.name;
-                    string jurisdiction_url = $"{Program.config_couchdb_url}/jurisdiction/_design/sortable/_view/by_user_idfilter?key={json_result.name}";
-                    //_design/filter/_view/filter?key=%22fun%22
-                    var jurisdiction_curl = new cURL("GET", null, jurisdiction_url, null, Program.config_timer_user_name, Program.config_timer_password);
-                    responseFromServer = await jurisdiction_curl.executeAsync();
-
-                    var user_role_list = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_response_header<mmria.common.model.couchdb.user_role_jurisdiction>> (responseFromServer);
- */
-                    const string Issuer = "https://contoso.com";
-                    var claims = new List<Claim>();
-                    claims.Add(new Claim(ClaimTypes.Name, json_result.name, ClaimValueTypes.String, Issuer));
-
- 
-                    foreach(var role in json_result.roles)
-                    {
-                        if(role == "_admin")
-                        {
-                            claims.Add(new Claim(ClaimTypes.Role, "installation_admin", ClaimValueTypes.String, Issuer));
-                        }
-                    }
- 
-
-                    foreach(var role in mmria.server.util.authorization.get_current_user_role_jurisdiction_set_for(json_result.name).Select( jr => jr.role_name).Distinct())
-                    {
-
-                        claims.Add(new Claim(ClaimTypes.Role, role, ClaimValueTypes.String, Issuer));
-                    }
-
-
-                    Response.Cookies.Append("uid", json_result.name);
-                    Response.Cookies.Append("roles", string.Join(",",json_result.roles));
-					
-					//claims.Add(new Claim("EmployeeId", string.Empty, ClaimValueTypes.String, Issuer));
-					//claims.Add(new Claim("EmployeeId", "123", ClaimValueTypes.String, Issuer));
-					//claims.Add(new Claim(ClaimTypes.DateOfBirth, "1970-06-08", ClaimValueTypes.Date));
-
-					//var userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    var userIdentity = new ClaimsIdentity("SuperSecureLogin");
-                    userIdentity.AddClaims(claims);
-					var userPrincipal = new ClaimsPrincipal(userIdentity);
-
-/*
-                    var jurisdiction_hashset = mmria.server.util.case_authorization.get_current_jurisdiction_id_set_for(userPrincipal);
-                    var jurisdiction_list = string.Join(",",jurisdiction_hashset.ToList());
-
-                    //Response.Cookies.Append("jurisdiction_list", jurisdiction_list);
-                    claims.Add
-                    (
-                        new Claim
-                        (   "jurisdiction_list",
-                            jurisdiction_list,
-                            json_result.name,
-                            ClaimValueTypes.String,
-                            Issuer
-                        )
-                    ); 
-*/
-
-					await HttpContext.SignInAsync(
-						CookieAuthenticationDefaults.AuthenticationScheme,
-						userPrincipal,
-						new AuthenticationProperties
-						{
-							ExpiresUtc = DateTime.UtcNow.AddMinutes(30),
-							IsPersistent = false,
-							AllowRefresh = false,
-						});
-				}
-
-				/*
-				{
-					"ok":true,
-					"userCtx":
-					{
-						"name":"mmrds",
-						"roles":["_admin"]
-					},
-					"info":
-					{
-						"authentication_db":"_users",
-						"authentication_handlers":
-						[
-							"oauth",
-							"cookie",
-							"default"
-						],
-						"authenticated":"cookie"
-					}
-				}
-				*/
 
                 if(!is_locked_out)
                 {
+
+                    string post_data = string.Format ("name={0}&password={1}", user.UserName, user.Password);
+                    byte[] post_byte_array = System.Text.Encoding.ASCII.GetBytes(post_data);
+
+                    string request_string = Program.config_couchdb_url + "/_session";
+                    System.Net.WebRequest request = System.Net.WebRequest.Create(new Uri(request_string));
+                    //request.UseDefaultCredentials = true;
+
+                    request.PreAuthenticate = false;
+                    //request.Credentials = new System.Net.NetworkCredential("mmrds", "mmrds");
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    request.ContentLength = post_byte_array.Length;
+
+                    using (System.IO.Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(post_byte_array, 0, post_byte_array.Length);
+                    }
+
+                    System.Net.WebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
+                    System.IO.Stream dataStream = response.GetResponseStream ();
+                    System.IO.StreamReader reader = new System.IO.StreamReader (dataStream);
+                    string responseFromServer = await reader.ReadToEndAsync ();
+
+                    mmria.common.model.couchdb.login_response json_result = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.login_response>(responseFromServer);
+
+                    mmria.common.model.couchdb.login_response[] result =  new mmria.common.model.couchdb.login_response[] 
+                    { 
+                        json_result
+                    }; 
+
+
+                    this.Response.Headers.Add("Set-Cookie", response.Headers["Set-Cookie"]);
+
+                    string[] set_cookie = response.Headers["Set-Cookie"].Split(';');
+                    string[] auth_array = set_cookie[0].Split('=');
+                    if(auth_array.Length > 1)
+                    {
+                        string auth_session_token = auth_array[1];
+                        result[0].auth_session = auth_session_token;
+                    }
+                    else
+                    {
+                        result[0].auth_session = "";
+                    }
+
+                    
+                    if (json_result.ok && !string.IsNullOrWhiteSpace(json_result.name)) 
+                    {
+
+                        const string Issuer = "https://contoso.com";
+                        var claims = new List<Claim>();
+                        claims.Add(new Claim(ClaimTypes.Name, json_result.name, ClaimValueTypes.String, Issuer));
+
+    
+                        foreach(var role in json_result.roles)
+                        {
+                            if(role == "_admin")
+                            {
+                                claims.Add(new Claim(ClaimTypes.Role, "installation_admin", ClaimValueTypes.String, Issuer));
+                            }
+                        }
+    
+
+                        foreach(var role in mmria.server.util.authorization.get_current_user_role_jurisdiction_set_for(json_result.name).Select( jr => jr.role_name).Distinct())
+                        {
+
+                            claims.Add(new Claim(ClaimTypes.Role, role, ClaimValueTypes.String, Issuer));
+                        }
+
+
+                        Response.Cookies.Append("uid", json_result.name);
+                        Response.Cookies.Append("roles", string.Join(",",json_result.roles));
+                        
+                        //claims.Add(new Claim("EmployeeId", string.Empty, ClaimValueTypes.String, Issuer));
+                        //claims.Add(new Claim("EmployeeId", "123", ClaimValueTypes.String, Issuer));
+                        //claims.Add(new Claim(ClaimTypes.DateOfBirth, "1970-06-08", ClaimValueTypes.Date));
+                        //var userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                        var userIdentity = new ClaimsIdentity("SuperSecureLogin");
+                        userIdentity.AddClaims(claims);
+                        var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+                        await HttpContext.SignInAsync(
+                            CookieAuthenticationDefaults.AuthenticationScheme,
+                            userPrincipal,
+                            new AuthenticationProperties
+                            {
+                                ExpiresUtc = DateTime.UtcNow.AddMinutes(30),
+                                IsPersistent = false,
+                                AllowRefresh = false,
+                            });
+                    }
+
                     var Session_Event_Message = new mmria.server.model.actor.Session_Event_Message
                     (
                         DateTime.Now,
@@ -324,6 +284,16 @@ namespace mmria.server.Controllers
 			catch(Exception ex)
 			{
 				Console.WriteLine (ex);
+
+                var Session_Event_Message = new mmria.server.model.actor.Session_Event_Message
+                (
+                    DateTime.Now,
+                    user.UserName,
+                    _accessor.HttpContext.Connection.RemoteIpAddress.ToString(),
+                    mmria.server.model.actor.Session_Event_Message.Session_Event_Message_Action_Enum.failed_login
+                );
+
+                _actorSystem.ActorOf(Props.Create<mmria.server.model.actor.Record_Session_Event>()).Tell(Session_Event_Message);
 
 			} 
 /*
--- source-code/mmria/mmria.common/couchdb/get_sortable_view_response_object_key_header.cs
+++ source-code/mmria/mmria.common/couchdb/get_sortable_view_response_object_key_header.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace mmria.common.model.couchdb
 {
@@ -33,12 +34,62 @@ namespace mmria.common.model.couchdb
         public get_sortable_view_response_object_key_item(){}
 
 		public string id { get; set; } //": "16e458537602f5ef2a710089dffd9453",
-		public System.Collections.Generic.List<string> key { get; set; } //": "16e458537602f5ef2a710089dffd9453",
+		public object[] key { get; set; } //": "16e458537602f5ef2a710089dffd9453",
 
         //public T value {  get; set; }
         public T value {  get; set; }
 
 	
 	}
+
+
+    public class Compare_Session_Event_By_DateCreated<T> : IComparer<get_sortable_view_response_item<session_event>>
+    {
+        public int Compare(get_sortable_view_response_item<session_event> a, get_sortable_view_response_item<session_event> b)
+        {
+            if (a.value.date_created > b.value.date_created)
+            {
+                return -1;
+            }
+                
+            if (a.value.date_created < b.value.date_created)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+                
+        }
+    }
+
+/*
+    public class Compare_Session_Event_By_DateCreated<T> : IComparer<get_sortable_view_response_object_key_item<T>>
+    {
+        public int Compare(get_sortable_view_response_object_key_item<T> a, get_sortable_view_response_object_key_item<T> b)
+        {
+            var a_date = a.key[0] as DateTime?;
+            var b_date = b.key[0] as DateTime?;
+
+
+            if (a_date.Value > b_date.Value)
+            {
+                return -1;
+            }
+                
+            if (a_date.Value < b_date.Value)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+                
+        }
+    }
+
+     */
 
 }
