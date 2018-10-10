a32c118858a34c6803fb62f302826206e7c6658b
jhaines <jhaines@thetagrid.com>
2018-10-10 01:28:49 -0400

    General update to SeleniumTest

--- source-code/mmria/test/SeleniumTest.cs
+++ source-code/mmria/test/SeleniumTest.cs
@@ -13,8 +13,15 @@ namespace test
     // docker run -d -p 4444:4444 --shm-size=2g selenium/standalone-chrome:3.14.0-europium
     public class SeleniumTest
     {
-    public class Class1
-    {
+        string app_url = "http://10.0.0.18:5000";
+        string Debugger_Host_And_Port  = "localhost:4444";
+
+        string BinaryLocation = "/media/jhaines/tera27/file-set/workspace/CoffeeBreak/src/test/bin/Debug/netcoreapp2.1/";
+
+        IWebDriver driver;
+        OpenQA.Selenium.Internal.IFindsByXPath findByXPath;
+        OpenQA.Selenium.ITakesScreenshot ITakesScreenshot;
+
         [Fact]
         public void PassingTest()
         {
@@ -54,28 +61,33 @@ driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
 
             // Initialize the Chrome Driver
             //using (var driver = new ChromeDriver("/media/jhaines/tera27/file-set/workspace/coffeebreak-test/bin/Debug/netcoreapp2.1/", options))
-            using (var driver = new RemoteWebDriver(new System.Uri("http://localhost:4444/wd/hub"),new ChromeOptions()))
+            using (var driver = get_remote_chrome_driver())
             {            
+                findByXPath = driver as OpenQA.Selenium.Internal.IFindsByXPath;
+                ITakesScreenshot = driver as OpenQA.Selenium.ITakesScreenshot;
                 // Go to the home page
-                driver.Navigate().GoToUrl("http://test.mmria.org");
+                driver.Navigate().GoToUrl(app_url);
 
                 System.Threading.Thread.Sleep(1 * milliseconds_in_second);
 
+/*
                 // Get the page elements
-
-                //var login_toggle = driver.FindElementByXPath("//li[@id='profile_content_id']/a[@class='dropdown-toggle']");
                 var login_toggle = driver.FindElementByXPath("//li[@id='profile_content_id']/a");
                 login_toggle.Click();
                 var userNameField = driver.FindElementByXPath("//*[@id='profile_content_id2']/div/input[1]");
                 var userPasswordField = driver.FindElementByXPath("//*[@id='profile_content_id2']/div/input[2]");
                 var loginButton = driver.FindElementByXPath("//*[@id='profile_content_id2']/div/input[3]");
-
                 // Type user name and password
                 userNameField.SendKeys("user1");
                 userPasswordField.SendKeys("password");
-
                 // and click the login button
                 loginButton.Click();
+ */
+                set_value("/html/body/form/input[1]", "user1", "user_name");
+                set_value("/html/body/form/input[2]", "password", "password");
+                click("/html/body/form/input[3]", "login_click");
+
+
 /*
                 // Extract the text and save it into result.txt
                 var result = driver.FindElementById("form_content_id").Text;
@@ -92,11 +104,63 @@ driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
  */
                 System.Threading.Thread.Sleep(5 * milliseconds_in_second);
                 // Take a screenshot and save it into screen.png
-                driver.GetScreenshot().SaveAsFile(@"add_new_case.png", ScreenshotImageFormat.Png);
+                TakeScreenshot(@"add_new_case.png");
 
                  Assert.Equal(1, 1);
             }
         }
-    }
+
+        private IWebDriver get_local_chrome_driver()
+        {
+            var options = new OpenQA.Selenium.Chrome.ChromeOptions();
+            //options.BinaryLocation = "/usr/share/iron/chrome-wrapper";
+            options.BinaryLocation = "/usr/share/iron/chrome";
+            options.AddArgument("--disable-extensions");
+           // options.BinaryLocation = "/wd/hub";
+            options.DebuggerAddress  = Debugger_Host_And_Port;
+            //new ChromeDriver(chrome_driver_binary, chrome_options=options);
+
+            return new ChromeDriver(BinaryLocation, options);
+            //return new ChromeDriver("/media/jhaines/tera27/file-set/workspace/CoffeeBreak/src/test/bin/Debug/netcoreapp2.1/");
+            /*
+            System.setProperty("webdriver.chrome.driver","");
+
+                        
+                        using (var driver = new ChromeDriver("/media/jhaines/tera27/file-set/workspace/coffeebreak-test/bin/Debug/netcoreapp2.1/", options))
+
+ */
+
+            // Initialize the Chrome Driver
+            //using (var driver = new ChromeDriver("/media/jhaines/tera27/file-set/workspace/coffeebreak-test/bin/Debug/netcoreapp2.1/", options))
+        }
+
+        private IWebDriver get_remote_chrome_driver()
+        {
+            return new RemoteWebDriver(new System.Uri($"http://{Debugger_Host_And_Port}/wd/hub"),new ChromeOptions());
+        }
+
+        private void click(string p_xpath, string p_comment = null)
+        {
+            var element = findByXPath.FindElementByXPath(p_xpath);
+            element.Click();
+        }
+
+        private void set_value(string p_xpath, string p_value, string p_comment = null)
+        {
+                var element = findByXPath.FindElementByXPath(p_xpath);
+                element.SendKeys(p_value);
+        }
+
+
+        private IWebElement get_element(string p_xpath, string p_comment = null)
+        {
+                var element = findByXPath.FindElementByXPath(p_xpath);
+                return element;
+        }
+        private void TakeScreenshot(string p_path, string p_comment = null)
+        {
+            ITakesScreenshot.GetScreenshot().SaveAsFile(p_path, ScreenshotImageFormat.Png);
+        }
+    
     }
 }
