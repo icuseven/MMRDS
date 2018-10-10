114b2177f4aee1e4121062f6bb1bb3c0bab5c868
icuseven <jhaines@thetagrid.com>
2018-10-10 14:32:05 -0400

    MMRDS-925 Added form_designer/index.js

---
+++ source-code/mmria/mmria-server/wwwroot/scripts/form-designer/index.js
@@ -0,0 +1,19 @@
+function get_new_ui_specification(p_name)
+{
+    return {
+        "name": p_name,
+        "_id": $mmria.get_new_guid(),
+        "data_type": "ui-specification",
+        "date_created": new Date().toISOString(),
+        "created_by": $mmria.getCookie("uid"),
+        "date_last_updated": new Date().toISOString(),
+        "last_updated_by": $mmria.getCookie("uid"),
+        "dimension": { 
+            "width": 8.5,
+            "height": null
+        },
+        "form_design": {
+        }
+        
+    };
+}
