efd04b19fbeb85c74a20e1b7bf1e3945012a5329
jhaines <jhaines@thetagrid.com>
2018-11-20 09:18:43 -0500

    MMRDS-832

---
+++ source-code/mmria/mmria-server/Controllers/export_queueController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers
+{
+    [Authorize(Roles  = "abstractor")]
+    [Route("export-queue")]
+    //[Authorize(Policy = "Over21Only")]
+    //[Authorize(Policy = "BuildingEntry")]
+    //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/resourcebased?view=aspnetcore-2.1&tabs=aspnetcore2x
+    public class export_queueController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+        //private readonly IDocumentRepository _documentRepository;
+
+        public export_queueController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+            //_documentRepository = documentRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
