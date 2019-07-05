8a91d3b14caf6d000a9cff398a2dbad6b0763068
icuseven <jhaines@thetagrid.com>
2019-07-05 11:53:16 -0400

    MMRDS-1054 implemented version_code_genController

---
+++ source-code/mmria/mmria-server/Controllers/api/version_code_genController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mmria.common.model;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server
+{
+	[Route("api/[controller]")]
+	public class version_code_genController: ControllerBase
+	{ 
+		// GET api/values 
+		//public IEnumerable<master_record> Get() 
+		[AllowAnonymous] 
+		[HttpGet]
+		public async Task<string> Get()
+		{
+			string result = null;
+
+			try
+			{
+				//"2016-06-12T13:49:24.759Z"
+                string request_string = Program.config_couchdb_url + $"/metadata/2016-06-12T13:49:24.759Z/validator.js";
+
+				System.Net.WebRequest request = System.Net.WebRequest.Create(new Uri(request_string));
+				request.Method = "GET";
+				request.PreAuthenticate = false;
+
+				System.Net.WebResponse response = (System.Net.HttpWebResponse) await request.GetResponseAsync();
+				System.IO.Stream dataStream = response.GetResponseStream();
+				System.IO.StreamReader reader = new System.IO.StreamReader (dataStream);
+				result = await reader.ReadToEndAsync ();
+
+			}
+			catch(Exception ex) 
+			{
+				Console.WriteLine (ex);
+			}
+
+			return result;
+		} 
+
+		// POST api/values 
+		[AllowAnonymous] 
+		[HttpPost]
+		[HttpPut]
+		public async System.Threading.Tasks.Task<ContentResult> Post
+		(
+			[FromBody] System.Dynamic.ExpandoObject code_gen_request
+		) 
+		{ 
+			var generatedFile = "";
+
+			//if(!string.IsNullOrWhiteSpace(json))
+			try
+			{
+
+				var byName = (IDictionary<string,object>)code_gen_request;
+				var payload = byName["payload"].ToString(); 
+				string id_val = null;
+
+
+				Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings ();
+				settings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+				var payload_string = Newtonsoft.Json.JsonConvert.SerializeObject(byName["payload"], settings);
+
+
+				generatedFile = await GenerateFileAsync(payload_string);
+
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine (ex);
+			}
+/*
+			this.Response.Clear();
+			this.Response.ClearHeaders();
+			this.Response.AddHeader("Content-Type", "text/plain");
+ */
+			return Content(generatedFile, "text/plain");
+		}
+
+		async Task<string> GenerateFileAsync(string schemaJson)
+		{
+				string result = null;
+
+				var schema = await NJsonSchema.JsonSchema.FromJsonAsync(schemaJson);
+				var settings = new NJsonSchema.CodeGeneration.CSharp.CSharpGeneratorSettings()
+				{
+					Namespace = "AwesomeSauce.v1",
+					//ClassStyle = NJsonSchema.CodeGeneration.CSharp.CSharpClassStyle.Inpc 
+					ClassStyle = NJsonSchema.CodeGeneration.CSharp.CSharpClassStyle.Poco,
+					GenerateJsonMethods = true,
+					GenerateDataAnnotations = true
+				};
+
+				var generator = new NJsonSchema.CodeGeneration.CSharp.CSharpGenerator(schema, settings);
+				result = generator.GenerateFile();
+
+//NJsonSchema.CodeGeneration.CSharp.CSharpClassStyle.
+				return result;
+		}
+ 
+	} 
+}
+
