34d94e0de8fd52fe7904ca0e7defe6aec6659dea
Lee <pla9@cdc.gov>
2019-07-16 17:30:17 -0400

    De-Id layout

--- source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/form.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/form.js
@@ -3,160 +3,154 @@ function form_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
     if(
         p_metadata.cardinality == "+" ||
         p_metadata.cardinality == "*"
-   
-   )
-   {
-       p_result.push("<section id='");
-       p_result.push(p_metadata.name);
-       p_result.push("_id' class='form'><h2 style='grid-column:1/-1;'");
-       if(p_metadata.description && p_metadata.description.length > 0)
-       {
-           p_result.push("rel='tooltip'  data-original-title='");
-           p_result.push(p_metadata.description.replace(/'/g, "\\'"));
-           p_result.push("'>");
-       }
-       else
-       {
-           p_result.push(">");
-       }
+    )
+    {
+        p_result.push("<section id='");
+        p_result.push(p_metadata.name);
+        p_result.push("_id' class='form'><h2 style='grid-column:1/-1;'");
 
-       p_result.push(p_metadata.prompt);
-       p_result.push("</h2>");
+        if(p_metadata.description && p_metadata.description.length > 0)
+        {
+            p_result.push("rel='tooltip'  data-original-title='");
+            p_result.push(p_metadata.description.replace(/'/g, "\\'"));
+            p_result.push("'>");
+        }
+        else
+        {
+            p_result.push(">");
+        }
 
-       if(g_data)
-       {
-           p_result.push("<h3  style='color: #341c54;grid-column:1/-1;'>");
-           p_result.push(g_data.home_record.last_name);
-           p_result.push(", ");
-           p_result.push(g_data.home_record.first_name);
-           if(g_data.home_record.record_id)
-           {
-               p_result.push("  - ");
-               p_result.push(g_data.home_record.record_id);
-           }
-           p_result.push("</h3>");
-       }
+            p_result.push(p_metadata.prompt);
+            p_result.push("</h2>");
 
+            if(g_data)
+            {
+                p_result.push("<h3  style='color: #341c54;grid-column:1/-1;'>");
+                p_result.push(g_data.home_record.last_name);
+                p_result.push(", ");
+                p_result.push(g_data.home_record.first_name);
+                if(g_data.home_record.record_id)
+                {
+                    p_result.push("  - ");
+                    p_result.push(g_data.home_record.record_id);
+                }
+                p_result.push("</h3>");
+            }
+                p_result.push('<input path="" type="button" class="btn btn-primary" value="Add New ');
+                p_result.push(p_metadata.prompt.replace(/"/g, "\\\""));
+                p_result.push(' form" onclick="add_new_form_click(\'' + p_metadata_path + '\',\'' + p_object_path + '\')" />');
+            
+            p_result.push('<div class="search_wrapper">');
 
-        p_result.push('<input path="" type="button" class="btn btn-primary" value="Add New ');
-        p_result.push(p_metadata.prompt.replace(/"/g, "\\\""));
-        p_result.push(' form" onclick="add_new_form_click(\'' + p_metadata_path + '\',\'' + p_object_path + '\')" />');
-       
-       p_result.push('<div class="search_wrapper">');
-       for(var i = 0; i < p_data.length; i++)
-       {
-           var item = p_data[i];
-           if(item)
-           {
-               if(i % 2)
-               {
-                   p_result.push('		  <div class="p_result_wrapper_grey"> <a href="#/');
-               }
-               else
-               {
-                   p_result.push('		  <div class="p_result_wrapper"> <a href="#/');
-               }
-               p_result.push(p_ui.url_state.path_array.join("/"));
-               //p_result.push(p_metadata.name);
-               p_result.push("/");
-               p_result.push(i);
-               p_result.push("\">");
-               p_result.push('View Record ');
-               p_result.push(i + 1);
-   
-               p_result.push('</a>&nbsp;|&nbsp;');
-               
-                p_result.push('<a onclick="g_delete_record_item(\'' + p_object_path + "[" + i + "]" + '\', \'' + p_metadata_path + '\')');
-                p_result.push("\">");
-                p_result.push('Delete Record ');
-                p_result.push(i + 1);
-                p_result.push('</a>');
-               
-               p_result.push('</div>');
-           }
+            for(var i = 0; i < p_data.length; i++)
+            {
+                var item = p_data[i];
+                if(item)
+                {
+                    if(i % 2)
+                    {
+                        p_result.push('<div class="p_result_wrapper_grey"> <a href="#/');
+                    }
+                    else
+                    {
+                        p_result.push('<div class="p_result_wrapper"> <a href="#/');
+                    }
+                    p_result.push(p_ui.url_state.path_array.join("/"));
+                    //p_result.push(p_metadata.name);
+                    p_result.push("/");
+                    p_result.push(i);
+                    p_result.push("\">");
+                    p_result.push('View Record ');
+                    p_result.push(i + 1);
+        
+                    p_result.push('</a>&nbsp;|&nbsp;');
+                    
+                        p_result.push('<a onclick="g_delete_record_item(\'' + p_object_path + "[" + i + "]" + '\', \'' + p_metadata_path + '\')');
+                        p_result.push("\">");
+                        p_result.push('Delete Record ');
+                        p_result.push(i + 1);
+                        p_result.push('</a>');
+                    
+                    p_result.push('</div>');
+                }
 
-       }
-       p_result.push('		</div>');
-       p_result.push("</section>");
+            }
+            p_result.push('</div>');
+        p_result.push("</section>");
 
-       if(p_ui.url_state.path_array.length > 2)
-       {
-           var data_index = parseInt(p_ui.url_state.path_array[2]);
-           var form_item = p_data[data_index];
+        if(p_ui.url_state.path_array.length > 2)
+        {
+            var data_index = parseInt(p_ui.url_state.path_array[2]);
+            var form_item = p_data[data_index];
 
-           p_result.push("<section id='");
-           p_result.push(p_metadata.name);
-           p_result.push("' class='form'><h2 style='grid-column:1/-1;' ");
-           console.log(p_metadata);
-           if(p_metadata.description && p_metadata.description.length > 0)
-           {
-               p_result.push("rel='tooltip'  data-original-title='");
-               p_result.push(p_metadata.description.replace(/'/g, "\\'"));
-               p_result.push("'>");
-           }
-           else
-           {
-               p_result.push(">");
-           }
+            p_result.push("<section id='");
+            p_result.push(p_metadata.name);
+            p_result.push("' class='form'><h2 style='grid-column:1/-1;' ");
+            console.log(p_metadata);
+            if(p_metadata.description && p_metadata.description.length > 0)
+            {
+                p_result.push("rel='tooltip'  data-original-title='");
+                p_result.push(p_metadata.description.replace(/'/g, "\\'"));
+                p_result.push("'>");
+            }
+            else
+            {
+                p_result.push(">");
+            }
 
-           p_result.push(p_metadata.prompt);
-           
+            p_result.push(p_metadata.prompt);
+            
+                p_result.push(" <input type='button'  class='btn btn-primary' value='undo' onclick='undo_click()' />");
+                p_result.push(" <input type='button'  class='btn btn-primary' value='save' onclick='save_form_click()' />");
 
+            p_result.push("</h2><h4 style='grid-column:1/-1;'>");
+            p_result.push(" record: ");
+            p_result.push(data_index + 1);
+            p_result.push("</h4>");
+            
+            if(g_data)
+            {
+                p_result.push("<h3  style='color: #341c54;grid-column:1/-1;'>");
+                p_result.push(g_data.home_record.last_name);
+                p_result.push(", ");
+                p_result.push(g_data.home_record.first_name);
+                if(g_data.home_record.record_id)
+                {
+                    p_result.push("  - ");
+                    p_result.push(g_data.home_record.record_id);
+                }
+                p_result.push("</h3>");
+            }
 
-            p_result.push(" <input type='button'  class='btn btn-primary' value='undo' onclick='undo_click()' />");
-            p_result.push(" <input type='button'  class='btn btn-primary' value='save' onclick='save_form_click()' />");
+            for(var i = 0; form_item && i < p_metadata.children.length; i++)
+            {
+                var child = p_metadata.children[i];
+                //var item = p_data[data_index][child.name];
+                if(form_item[child.name])
+                {
 
+                }
+                else
+                {
+                    form_item[child.name] = create_default_object(child, {})[child.name];
+                }
 
-           p_result.push("</h2><h4 style='grid-column:1/-1;'>");
-           p_result.push(" record: ");
-           p_result.push(data_index + 1);
-           p_result.push("</h4>");
-           
-           
-           if(g_data)
-           {
-               p_result.push("<h3  style='color: #341c54;grid-column:1/-1;'>");
-               p_result.push(g_data.home_record.last_name);
-               p_result.push(", ");
-               p_result.push(g_data.home_record.first_name);
-               if(g_data.home_record.record_id)
-               {
-                   p_result.push("  - ");
-                   p_result.push(g_data.home_record.record_id);
-               }
-               p_result.push("</h3>");
-           }
-
-           for(var i = 0; form_item && i < p_metadata.children.length; i++)
-           {
-               var child = p_metadata.children[i];
-               //var item = p_data[data_index][child.name];
-               if(form_item[child.name])
-               {
-
-               }
-               else
-               {
-                   form_item[child.name] = create_default_object(child, {})[child.name];
-               }
-
-               if(child.type=="group")
-               {
-                   Array.prototype.push.apply(p_result, page_render(child,form_item[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "[" + data_index + "]." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
-               }
-               else
-               {
-                   Array.prototype.push.apply(p_result, page_render(child, form_item[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "[" + data_index + "]." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
-               }
-               
-               //p_result.push("</div>");
-           }
-
+                if(child.type=="group")
+                {
+                    Array.prototype.push.apply(p_result, page_render(child,form_item[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "[" + data_index + "]." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
+                }
+                else
+                {
+                    Array.prototype.push.apply(p_result, page_render(child, form_item[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "[" + data_index + "]." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
+                }
+                
+                //p_result.push("</div>");
+            }
 
             p_result.push(" <input type='button'  class='btn btn-primary' value='undo' onclick='undo_click()' />");
             p_result.push(" <input type='button'  class='btn btn-primary' value='save' onclick='save_form_click()' />");
            
-           p_result.push("</section>");
+            p_result.push("</section>");
 
        }
 
@@ -228,7 +222,6 @@ function form_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
            p_result.push(g_data.birth_fetal_death_certificate_parent.reviewer_note);
            p_result.push("</textarea></label></p>");
 
-           
            //birth_certificate_infant_fetal_section/reviewer_note
            p_result.push("<h3>Birth/Fetal Death Certificate- Infant/Fetal Section Reviewer's Notes</h3>");
            for(var i = 0; i < g_data.birth_certificate_infant_fetal_section.length; i++)
@@ -248,14 +241,11 @@ function form_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
            p_result.push(g_data.autopsy_report.reviewer_note);
            p_result.push("</textarea></label></p>");
 
-           
            //prenatal/reviewer_note
            p_result.push("<p><h3>Prenatal Care Record </h3>");
            p_result.push("<label>Reviewer's Notes<br/><textarea cols=120 rows=7>");
            p_result.push(g_data.prenatal.reviewer_note);
            p_result.push("</textarea></label></p>");
-           
-
            
            //er_visit_and_hospital_medical_records/reviewer_note
            p_result.push("<h3>ER Visits and Hospitalizations Reviewer's Notes</h3>");
@@ -282,10 +272,10 @@ function form_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
                p_result.push("</textarea></label>");
                p_result.push("</p>");
            }
-///medical_transport/transport_narrative_summary
-           p_result.push("<h3>Medical Transport Reviewer's Notes</h3>");
-           for(var i = 0; i < g_data.medical_transport.length; i++)
-           {
+            ///medical_transport/transport_narrative_summary
+            p_result.push("<h3>Medical Transport Reviewer's Notes</h3>");
+            for(var i = 0; i < g_data.medical_transport.length; i++)
+            {
                p_result.push("<p><label>Note: ");
                p_result.push(i+1);
                p_result.push("<br/>");
@@ -293,56 +283,53 @@ function form_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
                p_result.push(g_data.medical_transport[i].reviewer_note);
                p_result.push("</textarea></label>");
                p_result.push("</p>");
-           }
+            }
            
-           //social_and_environmental_profile/reviewer_note
-           p_result.push("<p><h3>Social and Environmental Profile </h3>");
-           p_result.push("<label>Reviewer's Notes<br/><textarea cols=120 rows=7>");
-           p_result.push(g_data.social_and_environmental_profile.reviewer_note);
-           p_result.push("</textarea></label></p>");
+            //social_and_environmental_profile/reviewer_note
+            p_result.push("<p><h3>Social and Environmental Profile </h3>");
+            p_result.push("<label>Reviewer's Notes<br/><textarea cols=120 rows=7>");
+            p_result.push(g_data.social_and_environmental_profile.reviewer_note);
+            p_result.push("</textarea></label></p>");
 
+            p_result.push("<p><h3>Mental Health Profile </h3>");
+            p_result.push("<label>Reviewer's Notes<br/><textarea cols=120 rows=7>");
+            p_result.push(g_data.mental_health_profile.reviewer_note);
+            p_result.push("</textarea></label></p>");
 
-           p_result.push("<p><h3>Mental Health Profile </h3>");
-           p_result.push("<label>Reviewer's Notes<br/><textarea cols=120 rows=7>");
-           p_result.push(g_data.mental_health_profile.reviewer_note);
-           p_result.push("</textarea></label></p>");
+            p_result.push("<h3>Informant Interviews Reviewer's Notes</h3>");
+            for(var i = 0; i < g_data.informant_interviews.length; i++)
+            {
+                p_result.push("<p><label>Note: ");
+                p_result.push(i+1);
+                p_result.push("<br/>");
+                p_result.push("<textarea cols=120 rows=7>");
+                p_result.push(g_data.informant_interviews[i].reviewer_note);
+                p_result.push("</textarea></label>");
+                p_result.push("</p>");
+            }
+        }
 
-           p_result.push("<h3>Informant Interviews Reviewer's Notes</h3>");
-           for(var i = 0; i < g_data.informant_interviews.length; i++)
-           {
-               p_result.push("<p><label>Note: ");
-               p_result.push(i+1);
-               p_result.push("<br/>");
-               p_result.push("<textarea cols=120 rows=7>");
-               p_result.push(g_data.informant_interviews[i].reviewer_note);
-               p_result.push("</textarea></label>");
-               p_result.push("</p>");
-           }
+        for(var i = 0; i < p_metadata.children.length; i++)
+        {
+            var child = p_metadata.children[i];
+            if(p_data[child.name] || p_data[child.name] == 0)
+            {
+                // do nothing 
+            }
+            else
+            {
+                p_data[child.name] = create_default_object(child, {})[child.name];
+            }
+            Array.prototype.push.apply(p_result, page_render(child, p_data[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
+        }
 
-           
-       }
-
-       for(var i = 0; i < p_metadata.children.length; i++)
-       {
-           var child = p_metadata.children[i];
-           if(p_data[child.name] || p_data[child.name] == 0)
-           {
-               // do nothing 
-           }
-           else
-           {
-               p_data[child.name] = create_default_object(child, {})[child.name];
-           }
-           Array.prototype.push.apply(p_result, page_render(child, p_data[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render));
-       }
-
-        p_result.push("</div> <!-- end .construct-output -->");     
-        p_result.push("</div> <!-- end .construct__body -->");     
-        p_result.push("<div class='construct__footer'>");
-            p_result.push(" <input type='button' class='btn btn-secondary' value='Undo' onclick='undo_click()' />");
-            p_result.push(" <input type='button' class='btn btn-primary' value='Save' onclick='save_form_click()' />");
-        p_result.push("</div> <!-- end .construct__footer -->"); 
-       
-       p_result.push("</section>");
+            p_result.push("</div> <!-- end .construct-output -->");     
+            p_result.push("</div> <!-- end .construct__body -->");     
+            p_result.push("<div class='construct__footer'>");
+                p_result.push(" <input type='button' class='btn btn-secondary' value='Undo' onclick='undo_click()' />");
+                p_result.push(" <input type='button' class='btn btn-primary' value='Save' onclick='save_form_click()' />");
+            p_result.push("</div> <!-- end .construct__footer -->"); 
+        
+        p_result.push("</section>");
    }
 }
--- source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/grid.committee_member.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/grid.committee_member.js
@@ -4,65 +4,92 @@ function grid_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
 
     //p_result.push("<table style='grid-column:1/-1'  id='");
     p_result.push("<fieldset id='");
-    p_result.push(p_metadata_path);
-    p_result.push("'  class='grid2' style='overflow: auto;");
-
-    var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
-    if(style_object)
-    {
-        p_result.push(get_only_size_and_position_string(style_object.control.style));
-        is_grid_context = style_object;
-    }
-
-    p_result.push("' >"); // close opening div
-    p_result.push("<legend style='");
-
-    var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
-    if(style_object && style_object.prompt)
-    {
-        p_result.push(get_only_font_style_string(style_object.prompt.style));
-    }
-    p_result.push("'>");
-    p_result.push(p_metadata.prompt);
-    p_result.push(" - ");
-    p_result.push(p_data.length)
-    p_result.push(" item(s) </legend>");
-    p_result.push("<div style=''>");
-    for(var i = 0; i < p_data.length; i++)
-    {
-        p_result.push('<div>');
-        for(var j = 0; j < p_metadata.children.length; j++)
-        {
-            var child = p_metadata.children[j];
-
-            if(p_data[i][child.name] || p_data[child.name] == 0)
-            {
-                // do nothing 
-            }
-            else
-            {
-                p_data[i][child.name] = create_default_object(child, {})[child.name];
-            }
-            Array.prototype.push.apply
-            (
-                p_result,
-                page_render
-                (
-                    child,
-                    p_data[i][child.name], p_ui, p_metadata_path + ".children[" + j + "]",
-                    p_object_path + "[" + i + "]." + child.name,
-                    p_dictionary_path + "/" + child.name,
-                    is_grid_context,
-                    p_post_html_render
-                )
-            );
-
-        }
-        
-        p_result.push('</div>');
-    }
-    p_result.push("<br/>");
     
-    p_result.push("</div>");
+        p_result.push(p_metadata_path);
+        p_result.push("' ");
+        p_result.push(" mpath='" + p_metadata_path + "'");
+        p_result.push(" class='grid2 grid-control' style='");
+        var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
+        if(style_object)
+        {
+            p_result.push(get_only_size_and_position_string(style_object.control.style));
+            is_grid_context = style_object;
+        }
+        p_result.push("' >"); // close opening div
+
+        p_result.push("<legend style='");
+        
+        var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
+        if(style_object && style_object.prompt)
+        {
+            p_result.push(get_only_font_style_string(style_object.prompt.style));
+        }
+        p_result.push("'>");
+        p_result.push(p_metadata.prompt);
+        p_result.push(" - ");
+        p_result.push(p_data.length)
+        p_result.push(" item(s) </legend>");
+        p_result.push("<div class='grid-control-items'>");
+        for(var i = 0; i < p_data.length; i++)
+        {
+            p_result.push("<div class='grid-control-item'>");
+            for(var j = 0; j < p_metadata.children.length; j++)
+            {
+                var child = p_metadata.children[j];
+
+                if(p_data[i][child.name] || p_data[child.name] == 0)
+                {
+                    // do nothing 
+                }
+                else
+                {
+                    p_data[i][child.name] = create_default_object(child, {})[child.name];
+                }
+                Array.prototype.push.apply
+                (
+                    p_result,
+                    page_render
+                    (
+                        child,
+                        p_data[i][child.name],
+                        p_ui, p_metadata_path + ".children[" + j + "]",
+                        p_object_path + "[" + i + "]." + child.name,
+                        p_dictionary_path + "/" + child.name,
+                        is_grid_context,
+                        p_post_html_render
+                    )
+                );
+
+            }
+            // p_result.push("<button type='button' class='grid-control-action-btn' id='delete_");
+            // // p_result.push('<br/><input type="button" style="width:120px" class="btn btn-primary" value="delete" id="delete_');
+            //     p_result.push(p_object_path.replace(/\./g,"_") + "[" + i + "]");
+            //     p_result.push("' onclick='g_delete_grid_item(\"");
+            //     p_result.push(p_object_path + "[" + i + "]");
+            //     p_result.push("\", \"");
+            //     p_result.push(p_metadata_path);
+            //     p_result.push("\", \"");
+            //     p_result.push(p_dictionary_path);
+            //     p_result.push("\")'>");
+            //     p_result.push("<span class='x24 text-secondary cdc-icon-close'></span>");
+            //     p_result.push("<span class='sr-only'>Close</span>");
+            // p_result.push("</button>");
+            p_result.push(" item ");
+            p_result.push(new Number(i + 1));
+            p_result.push(" of ");
+            p_result.push(p_data.length);
+            p_result.push("</div>");
+        }
+        // p_result.push("<br/>");
+        p_result.push("</div>");
+        // p_result.push("<button type='button'class='grid-control-btn btn btn-primary d-flex align-items-center' onclick='g_add_grid_item(\"");
+            // p_result.push("<input type='button' style='width:90px'  class='btn btn-primary' value='Add Item' onclick='g_add_grid_item(\"");
+        //     p_result.push(p_object_path);
+        //     p_result.push("\", \"");
+        //     p_result.push(p_metadata_path);
+        //     p_result.push("\", \"");
+        //     p_result.push(p_dictionary_path);    
+        //     p_result.push("\")'><span class='x24 cdc-icon-plus'></span> Add Item");
+        // p_result.push("</button>");
     p_result.push("</fieldset>");
 }
