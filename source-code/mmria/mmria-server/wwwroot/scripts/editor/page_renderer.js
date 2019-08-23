d228c50b808bf31ee1739cc4ef2eed45fa6c9b7a
icuseven <jhaines@thetagrid.com>
2019-08-23 15:05:39 -0400

    Mirror fields

--- source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer.js
@@ -143,7 +143,7 @@ function page_render(p_metadata, p_data, p_ui, p_metadata_path, p_object_path, p
 			hidden_render(result, p_metadata, p_data, p_ui, p_metadata_path, p_object_path, p_dictionary_path, p_is_grid_context, p_post_html_render, p_search_ctx);
 			break;
 		case 'jurisdiction':
-			let juristdiction_data = p_data;
+			let jurisdiction_data = p_data;
 			if
 			(
 				p_metadata.mirror_reference &&
