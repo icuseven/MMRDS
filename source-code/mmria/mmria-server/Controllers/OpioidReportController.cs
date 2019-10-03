79aca67bb789caa6e58174597529e933edaf3472
Tou Fong Lee <leetoufong@gmail.com>
2019-10-03 10:02:18 -0400

    Add opioid report view

---
+++ source-code/mmria/mmria-server/Controllers/OpioidReportController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers
+{
+    [Authorize(Roles  = "abstractor,committee_member")]
+    [Route("opioid-report")]
+    //[Authorize(Policy = "Over21Only")]
+    //[Authorize(Policy = "BuildingEntry")]
+    //https://docs.microsoft.com/en-us/aspnet/core/security/authorization/resourcebased?view=aspnetcore-2.1&tabs=aspnetcore2x
+    public class OpioidReportController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+        //private readonly IDocumentRepository _documentRepository;
+
+        public OpioidReportController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+            //_documentRepository = documentRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
