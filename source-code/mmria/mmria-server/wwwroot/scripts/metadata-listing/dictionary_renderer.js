2ac5a727c09b739a28f3a2fc78dc775ddb91f52f
Tou Fong Lee <leetoufong@gmail.com>
2019-10-16 17:58:46 -0400

    MMRDS-1212 Work in progress, meta data listing uI updates

--- source-code/mmria/mmria-server/wwwroot/scripts/metadata-listing/dictionary_renderer.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/metadata-listing/dictionary_renderer.js
@@ -4,16 +4,20 @@ function dictionary_render(p_metadata, p_path)
 				
 	if(p_metadata.type.toLowerCase() == 'app')
 	{
-		result.push('<table border=1>');
-		result.push('<tr><th colspan="23">');
-		result.push(p_path);
-		result.push(' ');
-		result.push(p_metadata.name);
-		result.push(' ');
-		result.push(p_metadata.prompt);
-		result.push(' ');
-		result.push(p_metadata.type);
-		result.push('</th></tr>');
+		result.push('<table class="table" border="1">');
+		result.push('<thead class="thead">');
+			result.push('<tr class="bg-primary">');
+				result.push('<th class="th" colspan="23">');
+				result.push(p_path);
+				result.push(' ');
+				result.push(p_metadata.name);
+				result.push(' ');
+				result.push(p_metadata.prompt);
+				result.push(' ');
+				result.push(p_metadata.type);
+				result.push('</th>');
+			result.push('</tr>');
+		result.push('</thead>');
 	}
 	else
 	{
@@ -52,146 +56,141 @@ function dictionary_render_header()
 {
 	var result = [];
 
-	result.push('<tr><th>path</th>');
-	result.push('<th>name</th>');
-	result.push('<th>type</th>');
-	result.push('<th>prompt</th>');
-	result.push('<th>control_style</th>');
-	result.push('<th>description</th>');
-	result.push('<th>is_save_value_display_description</th>');
-	result.push('<th>values</th>');
-	result.push('<th>is_core_summary</th>');
-	result.push('<th>is_required</th>');
-	result.push('<th>is_multiselect</th>');
-	result.push('<th>is_read_only</th>');
-	result.push('<th>default_value</th>');
-	result.push('<th>regex_pattern</th>');
-	result.push('<th>pre_fill</th>');
-	result.push('<th>max_value</th>');
-	result.push('<th>min_value</th>');	
-	result.push('<th>validation</th>');
-	result.push('<th>validation_description</th>');
-	result.push('<th>onfocus</th>');
-	result.push('<th>onchange</th>');
-	result.push('<th>onblur</th>');
-	result.push('<th>onclick</th>');
+	result.push(`
+		<tr class="tr bg-gray-l3">
+			<th class="th">path</th>
+			<th class="th">name</th>
+			<th class="th">type</th>
+			<th class="th">prompt</th>
+			<th class="th">control_style</th>
+			<th class="th">description</th>
+			<th class="th">is_save_value_display_description</th>
+			<th class="th">values</th>
+			<th class="th">is_core_summary</th>
+			<th class="th">is_required</th>
+			<th class="th">is_multiselect</th>
+			<th class="th">is_read_only</th>
+			<th class="th">default_value</th>
+			<th class="th">regex_pattern</th>
+			<th class="th">pre_fill</th>
+			<th class="th">max_value</th>
+			<th class="th">min_value</th>	
+			<th class="th">validation</th>
+			<th class="th">validation_description</th>
+			<th class="th">onfocus</th>
+			<th class="th">onchange</th>
+			<th class="th">onblur</th>
+			<th class="th">onclick</th>
+		</tr>
+	`);
 
-	
-	result.push('</tr>');
 	return result;
-
 }
 
 function dictionary_render_row(p_metadata, p_path)
 {
 	var result = [];
 
-	result.push('<tr><td>' + p_path + '</td>');
-	result.push('<td>' + ((p_metadata['name'])? p_metadata['name']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['type'])? p_metadata['type']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['prompt'])? p_metadata['prompt']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['control_style'])? p_metadata['control_style']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['description'])? p_metadata['description']: "&nbsp;") + '</td>');
-	
-
-	if(p_metadata.type.toLowerCase() == "list")
-	{
-		result.push('<td>')
-		if
-		(
-			p_metadata['is_save_value_display_description'] &&
-			p_metadata['is_save_value_display_description'] == true
-		)
+	result.push('<tr class="tr">');
+		result.push('<td class="td">' + p_path + '</td>');
+		result.push('<td class="td">' + ((p_metadata['name'])? p_metadata['name']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['type'])? p_metadata['type']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['prompt'])? p_metadata['prompt']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['control_style'])? p_metadata['control_style']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['description'])? p_metadata['description']: "&nbsp;") + '</td>');
+		
+		if(p_metadata.type.toLowerCase() == "list")
 		{
-			result.push("saves the value, displays the <strong>description</strong> in list.");
+			result.push('<td class="td">')
+			if
+			(
+				p_metadata['is_save_value_display_description'] &&
+				p_metadata['is_save_value_display_description'] == true
+			)
+			{
+				result.push("saves the value, displays the <strong>description</strong> in list.");
+			}
+			else
+			{
+				result.push("saves value, displays value.");
+			}
+				
+			result.push('</td>');
 		}
 		else
 		{
-			result.push("saves value, displays value.");
+			result.push("<td class='td'>&nbsp;</td>");
 		}
-			
-		result.push('</td>');
-	}
-	else
-	{
-		result.push("<td>&nbsp;</td>");
-	}
 
-
-	if(p_metadata.values)
-	{
-
-
-		result.push('<td valign="center"><table><tr style="background-color:#FF7777;color:#FFFFFF"><th>value</th><th>description</th></tr>');
-
-		let value_list = p_metadata.values;
-
-		if(p_metadata.path_reference && p_metadata.path_reference != "")
+		if(p_metadata.values)
 		{
-			value_list = eval(convert_dictionary_path_to_lookup_object(p_metadata.path_reference));
-	
-			if(value_list == null)	
-			{
-				value_list = p_metadata.values;
-			}
-		}
+			result.push('<td class="td"><table class="table"><tr class="tr bg-secondary"><th class="th">value</th><th class="th">description</th></tr>');
 
-		for(var i = 0; i < value_list.length; i++)
+			let value_list = p_metadata.values;
+
+			if(p_metadata.path_reference && p_metadata.path_reference != "")
+			{
+				value_list = eval(convert_dictionary_path_to_lookup_object(p_metadata.path_reference));
+		
+				if(value_list == null)	
+				{
+					value_list = p_metadata.values;
+				}
+			}
+
+			for(var i = 0; i < value_list.length; i++)
+			{
+				var child = value_list[i];
+				if(i % 2)
+				{
+					result.push("<tr class='tr bg-gray-l3'><td>");
+				}
+				else
+				{
+					result.push("<tr class='tr'><td class='td'>");
+				}
+				if(child.value == "")
+				{
+					result.push("(blank)");
+				}
+				else
+				{
+					result.push(child.value);
+				}
+				
+				result.push("</td><td class='td'>")
+				if(child.display == "")
+				{
+					result.push("(blank)");
+				}
+				else
+				{
+					result.push(child.display);
+				}
+				result.push('</td></tr>');
+			}
+			result.push('</table></td>');
+		}
+		else
 		{
-			var child = value_list[i];
-			if(i % 2)
-			{
-				result.push("<tr style='background-color:#BBBBBB'><td>");
-			}
-			else
-			{
-				result.push("<tr><td>");
-			}
-			if(child.value == "")
-			{
-				result.push("(blank)");
-			}
-			else
-			{
-				result.push(child.value);
-			}
-			
-			result.push("</td><td>")
-			if(child.display == "")
-			{
-				result.push("(blank)");
-			}
-			else
-			{
-				result.push(child.display);
-			}
-			result.push('</td></tr>');
+			result.push('<td class="td">&nbsp;</td>');
 		}
-		result.push('</table></td>');
-	}
-	else
-	{
-		result.push('<td>&nbsp;</td>');
-	}
 
-	result.push('<td>' + ((p_metadata['is_core_summary'])? p_metadata['is_core_summary']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['is_required'])? p_metadata['is_required']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['is_multiselect'])? p_metadata['is_multiselect']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['is_read_only'])? p_metadata['is_read_only']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['default_value'])? p_metadata['default_value']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['regex_pattern'])? p_metadata['regex_pattern']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['pre_fill'])? p_metadata['pre_fill']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['max_value'])? p_metadata['max_value']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['min_value'])? p_metadata['min_value']: "&nbsp;") + '</td>');	
-	result.push('<td>' + ((p_metadata['validation'])? p_metadata['validation']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['validation_description'])? p_metadata['validation_description']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['onfocus'])? p_metadata['onfocus']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['onchange'])? p_metadata['onchange']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['onblur'])? p_metadata['onblur']: "&nbsp;") + '</td>');
-	result.push('<td>' + ((p_metadata['onclick'])? p_metadata['onclick']: "&nbsp;") + '</td>');
-
-	
-
+		result.push('<td class="td">' + ((p_metadata['is_core_summary'])? p_metadata['is_core_summary']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['is_required'])? p_metadata['is_required']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['is_multiselect'])? p_metadata['is_multiselect']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['is_read_only'])? p_metadata['is_read_only']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['default_value'])? p_metadata['default_value']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['regex_pattern'])? p_metadata['regex_pattern']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['pre_fill'])? p_metadata['pre_fill']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['max_value'])? p_metadata['max_value']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['min_value'])? p_metadata['min_value']: "&nbsp;") + '</td>');	
+		result.push('<td class="td">' + ((p_metadata['validation'])? p_metadata['validation']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['validation_description'])? p_metadata['validation_description']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['onfocus'])? p_metadata['onfocus']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['onchange'])? p_metadata['onchange']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['onblur'])? p_metadata['onblur']: "&nbsp;") + '</td>');
+		result.push('<td class="td">' + ((p_metadata['onclick'])? p_metadata['onclick']: "&nbsp;") + '</td>');
 
 	result.push('</tr>');
 
@@ -202,7 +201,6 @@ function dictionary_render_row(p_metadata, p_path)
 
 function convert_dictionary_path_to_lookup_object(p_path)
 {
-
 	//g_data.prenatal.routine_monitoring.systolic_bp
 	var result = null;
 	var temp_result = []
@@ -221,7 +219,6 @@ function convert_dictionary_path_to_lookup_object(p_path)
 			break;
 		}
 	}
-
 
 	return result;
 }
