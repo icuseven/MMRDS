5547c48cb488853365e7be8efd315ec0981bf10f
icuseven <jhaines@thetagrid.com>
2019-10-25 13:58:40 -0400

    Setting up aggregate report

---
+++ source-code/mmria/mmria-server/util/c_convert_to_report_object/distribution_of_underlying_cause_of_pregnancy_related_death_pmss_mm.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mmria.server.util
+{
+	public partial class c_convert_to_report_object
+	{
+
+        private void popluate_distribution_of_underlying_cause_of_pregnancy_related_death_pmss_mm(ref mmria.server.model.c_report_object p_report_object, System.Dynamic.ExpandoObject p_source_object)
+        {
+            
+        }
+    }
+
+
+}
