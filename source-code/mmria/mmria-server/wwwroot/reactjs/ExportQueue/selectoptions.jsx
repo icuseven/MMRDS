6336059adaa6e07d4539d7fb3bc16ac8b50eb47c
Lacey <pro7@cdc.gov>
2020-07-28 16:10:55 -0400

    fixed default selected issue

--- source-code/mmria/mmria-server/wwwroot/reactjs/ExportQueue/selectoptions.jsx
+++ source-code/mmria/mmria-server/wwwroot/reactjs/ExportQueue/selectoptions.jsx
@@ -1,13 +1,32 @@
 class SelectOptions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: getDefaultSelected(props),
+    };
+    function getDefaultSelected({ options }) {
+      const selectedOpt = options.find((opt) => !!opt.selected);
+      return selectedOpt.value || selectedOpt.text;
+    }
+  }
+  handleChange(e) {
+    const value = e.target;
+    this.setState({ selected: value });
+  }
+
   render() {
     const { options, id } = this.props;
-    const OPTS = options.map(({ text, value, selected }) => (
-      <option key={value} value={value || text} selected={!!selected}>
-        {text}
-      </option>
-    ));
+    let selected = '';
+    const OPTS = options.map(({ text, value, selected }) => {
+      if (!!selected) selected = value || text;
+      return (
+        <option key={value || text} value={value || text}>
+          {text}
+        </option>
+      );
+    });
     return (
-      <select key={id} id={id} className="custom-select">
+      <select id={id} className="custom-select" value={this.state.selected}>
         {OPTS}
       </select>
     );
