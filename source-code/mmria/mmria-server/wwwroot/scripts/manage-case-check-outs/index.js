0cc0480500828db015e2e94aa6309d221b3e5d22
Tou Fong Lee <leetoufong@gmail.com>
2020-08-17 12:08:22 -0400

    Add diff minutes to Admin page

--- source-code/mmria/mmria-server/wwwroot/scripts/manage-case-check-outs/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/manage-case-check-outs/index.js
@@ -67,7 +67,7 @@ function loadUser()
 
 function is_case_checked_out(p_case)
 {
-  let is_checked_out = false;
+	let is_checked_out = false;
   // let checked_out_html = '';
   let current_date = new Date();
   
@@ -87,7 +87,7 @@ function is_case_checked_out(p_case)
 		
 		if
 		(
-				diff_minutes(try_date, current_date) <= 120 &&
+				getMinuteDifference(try_date, current_date) <= 120 &&
 				p_case.value.last_checked_out_by.toLowerCase() == g_user_name.toLowerCase()
 		)
 		{
@@ -98,17 +98,26 @@ function is_case_checked_out(p_case)
   return is_checked_out;
 }
 
-function diff_minutes(dt1, dt2) 
+function getMinuteDifference(dt1, dt2) 
 {
   let diff =(dt2.getTime() - dt1.getTime()) / 1000;
 
-  diff /= 60;
+	diff /= 60;
   return Math.abs(Math.round(diff));
+}
+
+function convertToReadableTime(millis) {
+  var minutes = Math.floor(millis / 60000);
+	var seconds = ((millis % 60000) / 1000).toFixed(0);
+	
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
 function get_case_set()
 {
-  var case_view_url = location.protocol + '//' + location.host + '/api/case_view' + case_view_request.get_query_string();
+	var case_view_url = location.protocol + '//' + location.host + '/api/case_view' + case_view_request.get_query_string();
+	var p_time = null;
+
   $.ajax({
 		url: case_view_url,
   }).done(function(case_view_response) {
@@ -119,6 +128,7 @@ function get_case_set()
     for(let i = 0; i < case_view_response.rows.length; i++)
     {
 			let caseView = case_view_response.rows[i];
+			let caseLastUpdated = caseView.value.date_last_checked_out;
 			// let isCheckedOut = caseView.value.last_checked_out_by;
 
 			if (is_case_checked_out(caseView))
@@ -139,7 +149,7 @@ function get_case_set()
 }
 
 
-function render_case_list(p_cases)
+function render_case_list(p_cases, p_time)
 {
 	const result = [];
 
@@ -174,20 +184,34 @@ function render_case_list(p_cases)
 						let jurisdictionID = el.value.jurisdiction_id;
 						let firstName = el.value.first_name;
 						let lastName = el.value.last_name;
+						let recordID = el.value.record_id;
+						let agencyCaseID = el.value.agency_case_id;
+
 						let lastUpdatedDate = el.value.date_last_updated;
-						let timeLocked = el.value.date_last_checked_out;
+						let lastCheckedOutDate = el.value.date_last_updated;
+						lastUpdatedDate = lastUpdatedDate.split('T')[0];
+						lastUpdatedDate = lastUpdatedDate.split('-');
+						//convert ISO format to MM/DD/YYYY
+						lastUpdatedDate = `${lastUpdatedDate[1]}/${lastUpdatedDate[2]}/${lastUpdatedDate[0]}`;
+
+						let timeLocked = Math.abs(new Date(lastCheckedOutDate) - new Date());
+						debugger
+						timeLocked = convertToReadableTime(timeLocked);
+						debugger
+
 						let lockedBy = el.value.last_checked_out_by;
 						let caseStatus = ''; //TODO: Need to wire in Case Status
 
 						return (
 							`<tr class="tr">
 								<td class="td" scope="col">
-									${jurisdictionID && jurisdictionID || ''}
-									${lastName && lastName || ''}
-									${firstName && firstName || ''}
+									${jurisdictionID && `${jurisdictionID}: ` || ''}
+									${lastName && lastName || ''}, ${firstName && firstName || ''}
+									${recordID && ` - (${recordID})`}
+									${agencyCaseID && ` ac_id: ${agencyCaseID}`}
 								</td>
 								<td class="td" scope="col">${lastUpdatedDate && lastUpdatedDate || ''}</td>
-								<td class="td" scope="col">${timeLocked && timeLocked || ''}</td>
+								<td class="td" scope="col">${timeLocked && `${timeLocked} minutes` || ''}</td>
 								<td class="td" scope="col">${lockedBy && lockedBy || ''}</td>
 								<td class="td" scope="col">${caseStatus && caseStatus || ''}</td>
 								<td class="td" scope="col"><button class="anti-btn link" onclick="save_and_finish_click()">Release</button></td>
@@ -198,56 +222,40 @@ function render_case_list(p_cases)
 			</table>`
 		);
 	}
-
-	// result.push("<br/>");
-	// result.push("<table>");
-	// result.push("<tr  bgcolor='silver'>")
-	// result.push("<th scope='col'>user name</th>");
-	// result.push("<th scope='col'>check out date</th>");
-	// result.push("<th scope='col'>action</th>");
-	// result.push("</tr>");
-
-	//result.push("<tr><td colspan=2 align=center><input type='button' value='save list' onclick='server_save()' /></td></tr>")
-
-	
-	// for(let i = 0; i < g_power_bi_user_list.rows.length; i++)
-	// {
-	// 	var item = g_power_bi_user_list.rows[i].doc;
-
-	// 	if(i % 2)
-	// 	{
-	// 		result.push("<tr bgcolor='#CCCCCC'>");
-	// 	}
-	// 	else
-	// 	{
-	// 		result.push("<tr>");
-	// 	}
-	// 	result.push("<td>");
-	// 	result.push(encodeHTML(item.name));
-	// 	result.push("</td>");
-	// 	result.push("<td><label title='");
-	// 	if(item.alternate_email != null)
-	// 	{
-	// 		result.push(encodeHTML(item.alternate_email));
-	// 	}
-	// 	result.push("'><input size='120' type='text' value='");
-	// 	if(item.alternate_email != null)
-	// 	{
-	// 		result.push(escape(item.alternate_email));
-	// 	}
-	// 	result.push("' onchange='update_item("+ i +", this.value)'/></label></td>");
-	// 	result.push("<td colspan=2 align=center><input type='button' value='save' onclick='server_save("+ i +")' /></td>")
-	// 	result.push("</tr>");		
-		
-	// }
-
-	
-	// result.push("</table>");
-	// result.push("<br/>");
 	
 	return result;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function update_item(p_index, p_value)
 // {
@@ -291,4 +299,194 @@ function render_case_list(p_cases)
 // {
 // 	let result = s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 //     return result;
+// }
+
+// function initCaseRease() {
+// 	const currentCase = 
+// }
+
+
+
+
+
+// function get_specific_case(p_id)
+// {
+//   var case_url = location.protocol + '//' + location.host + '/api/case?case_id=' + p_id;
+
+//   $.ajax({
+//     url: case_url,
+//   }).done(function(case_response) 
+//   {
+//     if(case_response)
+//     {
+//       var local_data = get_local_case(p_id);
+
+//       if(local_data)
+//       {
+//           if(local_data._rev && local_data._rev == case_response._rev)
+//           {
+//               g_data = local_data;
+//               g_data_is_checked_out = is_case_checked_out(g_data);
+//           }
+//           else
+//           {
+//             /*
+//             console.log( "get_specific_case potential conflict:",  local_data._id, local_data._rev, case_response._rev);
+//             var date_difference = local_data.date_last_updated.diff(case_response.date_last_updated);
+//             if(date_difference.days > 3)
+//             {*/
+
+//               local_data = case_response;
+//             /*}
+//             else
+//             {
+//               local_data._rev = case_response._rev;
+//             }*/
+            
+//             set_local_case(local_data);
+//             g_data = local_data;
+//             g_data_is_checked_out = is_case_checked_out(g_data);
+//           }
+
+//           if(g_autosave_interval != null && g_data_is_checked_out == false)
+//           {
+//             window.clearInterval(g_autosave_interval);
+//             g_autosave_interval = null;
+//           }
+
+//           g_render();
+//       }
+//       else
+//       {
+//         g_data = case_response;
+//         g_data_is_checked_out = is_case_checked_out(g_data);
+
+//         if(g_autosave_interval != null && g_data_is_checked_out == false)
+//         {
+//           window.clearInterval(g_autosave_interval);
+//           g_autosave_interval = null;
+//         }
+//       }
+//       g_render();
+//     }
+//     else
+//     {
+//       g_render();
+//     }
+//   })
+//   .fail(function(jqXHR, textStatus, errorThrown) {
+//     console.log( "get_specific_case:",  textStatus, errorThrown);
+//     g_data = get_local_case(p_id);
+//     g_data_is_checked_out = is_case_checked_out(g_data);
+//   });
+// }
+
+// function save_case(p_data, p_call_back)
+// {
+
+//   if(p_data.host_state == null || p_data.host_state == "")
+//   {
+//     p_data.host_state = window.location.host.split("-")[0];
+//   }
+
+//   $.ajax({
+//     url: location.protocol + '//' + location.host + '/api/case',
+//     contentType: 'application/json; charset=utf-8',
+//     dataType: 'json',
+//     data: JSON.stringify(p_data),
+//     type: "POST"
+//   }).done(function(case_response) {
+
+//     console.log("save_case: success");
+
+//     g_change_stack = [];
+
+//     if(g_data && g_data._id == case_response.id)
+//     {
+//       g_data._rev = case_response.rev;
+//       g_data_is_checked_out = is_case_checked_out(g_data);
+//       set_local_case(g_data);
+//       //console.log('set_value save finished');
+//     }
+    
+//     if(p_call_back)
+//     {
+//       p_call_back();
+//     }
+//   })
+//   .fail
+//   (
+//     function(xhr, err) 
+//     { 
+//       console.log("server save_case: failed", err); 
+//       if(xhr.status == 401)
+//       {
+//         let redirect_url = location.protocol + '//' + location.host;
+//         window.location = redirect_url;
+//       }
+  
+//     }
+//   );
+// }
+
+// function save_and_finish_click()
+// {
+//   g_data.date_last_updated = new Date();
+//   g_data.date_last_checked_out = null;
+//   g_data.last_checked_out_by = null;
+	
+// 	save_case(g_data, create_save_message);
+	
+//   // g_render()
+//   // window.clearInterval(g_autosave_interval);
+//   // g_autosave_interval = null;
+// }
+
+// function get_metadata_value_node_by_mmria_path(p_metadata, p_search_path, p_path)
+// {
+// 		let result = null;
+		
+//     switch(p_metadata.type.toLowerCase())
+//     {
+//         case "app":
+//         case "form":
+//         case "group":
+//         case "grid":
+//             for(let i = 0; i < p_metadata.children.length; i++)
+//             {
+//                 let child = p_metadata.children[i];
+//                 result = get_metadata_value_node_by_mmria_path(child, p_search_path, p_path + "/" + child.name);
+//                 if(result != null)
+//                 {
+//                     break;
+//                 }
+//             }
+//             break;
+//         default:
+//             if(p_search_path == p_path)
+//             {
+//                 result = p_metadata;
+//             }
+//             break;
+// 		}
+		
+//     return result;
+// }
+
+// if (g_data.home_record.case_status && !isNullOrUndefined(g_data.home_record.case_status.overall_case_status))
+// {
+// 		let current_value = g_data.home_record.case_status.overall_case_status;
+// 		let look_up = get_metadata_value_node_by_mmria_path(g_metadata, "/home_record/case_status/overall_case_status", "");
+// 		let label = current_value;
+// 		for (let i = 0; i < look_up.values.length; i++)
+// 		{
+// 				let item = look_up.values[i];
+// 				if (item.value == current_value)
+// 				{
+// 						label = item.display;
+// 						break;
+// 				}
+// 		}
+
+// 		p_result.push(`<p class='construct__info mb-0'>Case Status: <span>${label}</span></p>`);
 // }
