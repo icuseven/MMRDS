d7712b48f6eff80aabd791ed4dca6b5d8fde1ad6
Tou Fong Lee <leetoufong@gmail.com>
2020-08-17 14:20:34 -0400

    Add better time conversion to admin page. Add release logic.

--- source-code/mmria/mmria-server/wwwroot/scripts/manage-case-check-outs/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/manage-case-check-outs/index.js
@@ -1,3 +1,4 @@
+var g_data = null;
 var g_user_name = null;
 var case_view_list = [];
 var case_view_request = {
@@ -36,7 +37,7 @@ $(function ()
 	//profile.initialize_profile();
 
 	loadUser();
-	get_case_set();
+	getCaseSet();
 
 	// $(document).keydown(function(evt){
 	// 	if (evt.keyCode==83 && (evt.ctrlKey)){
@@ -65,55 +66,7 @@ function loadUser()
 	});
 }
 
-function is_case_checked_out(p_case)
-{
-	let is_checked_out = false;
-  // let checked_out_html = '';
-  let current_date = new Date();
-  
-  if(p_case.value.date_last_checked_out != null && p_case.value.date_last_checked_out != "")
-  {
-		let try_date = null;
-		let is_date = false;
-
-		if(!(p_case.value.date_last_checked_out instanceof Date))
-		{
-				try_date = new Date(p_case.value.date_last_checked_out);
-		}
-		else
-		{
-			try_date = p_case.value.date_last_checked_out;
-		}
-		
-		if
-		(
-				getMinuteDifference(try_date, current_date) <= 120 &&
-				p_case.value.last_checked_out_by.toLowerCase() == g_user_name.toLowerCase()
-		)
-		{
-			is_checked_out = true;
-		}
-	}
-
-  return is_checked_out;
-}
-
-function getMinuteDifference(dt1, dt2) 
-{
-  let diff =(dt2.getTime() - dt1.getTime()) / 1000;
-
-	diff /= 60;
-  return Math.abs(Math.round(diff));
-}
-
-function convertToReadableTime(millis) {
-  var minutes = Math.floor(millis / 60000);
-	var seconds = ((millis % 60000) / 1000).toFixed(0);
-	
-  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
-
-function get_case_set()
+function getCaseSet()
 {
 	var case_view_url = location.protocol + '//' + location.host + '/api/case_view' + case_view_request.get_query_string();
 	var p_time = null;
@@ -131,7 +84,7 @@ function get_case_set()
 			let caseLastUpdated = caseView.value.date_last_checked_out;
 			// let isCheckedOut = caseView.value.last_checked_out_by;
 
-			if (is_case_checked_out(caseView))
+			if (isCaseCheckedOut(caseView))
 			{
 				checkedOutCases.push(caseView);
 			}
@@ -144,12 +97,12 @@ function get_case_set()
 			// }
 		}
     
-    document.getElementById('output').innerHTML = render_case_list(checkedOutCases).join('');
+    document.getElementById('output').innerHTML = renderCheckedOutCases(checkedOutCases).join('');
   });
 }
 
 
-function render_case_list(p_cases, p_time)
+function renderCheckedOutCases(p_cases, p_time)
 {
 	const result = [];
 
@@ -175,12 +128,13 @@ function render_case_list(p_cases, p_time)
 						<th class="th" scope="col">Time Locked</th>
 						<th class="th" scope="col">Locked By</th>
 						<th class="th" scope="col">Case Status</th>
-						<th class="th" scope="col">Actions</th>
+						<th class="th" scope="col"></th>
 					</tr>
 				</thead>
 				<tbody class="tbody">
 					<!-- START loop through checked out cases -->
 					${p_cases.map((el, i) => {
+						let caseID = el.id;
 						let jurisdictionID = el.value.jurisdiction_id;
 						let firstName = el.value.first_name;
 						let lastName = el.value.last_name;
@@ -188,22 +142,21 @@ function render_case_list(p_cases, p_time)
 						let agencyCaseID = el.value.agency_case_id;
 
 						let lastUpdatedDate = el.value.date_last_updated;
-						let lastCheckedOutDate = el.value.date_last_updated;
-						lastUpdatedDate = lastUpdatedDate.split('T')[0];
-						lastUpdatedDate = lastUpdatedDate.split('-');
+						// lastUpdatedDate = lastUpdatedDate.split('T')[0];
+						// lastUpdatedDate = lastUpdatedDate.split('-');
+						lastUpdatedDate = new Date(lastUpdatedDate);
 						//convert ISO format to MM/DD/YYYY
-						lastUpdatedDate = `${lastUpdatedDate[1]}/${lastUpdatedDate[2]}/${lastUpdatedDate[0]}`;
-
+						lastUpdatedDate = lastUpdatedDate.toLocaleDateString('en-US');
+						
+						let lastCheckedOutDate = el.value.date_last_updated;
 						let timeLocked = Math.abs(new Date(lastCheckedOutDate) - new Date());
-						debugger
 						timeLocked = convertToReadableTime(timeLocked);
-						debugger
 
 						let lockedBy = el.value.last_checked_out_by;
 						let caseStatus = ''; //TODO: Need to wire in Case Status
 
 						return (
-							`<tr class="tr">
+							`<tr class="tr" data-id="${caseID}">
 								<td class="td" scope="col">
 									${jurisdictionID && `${jurisdictionID}: ` || ''}
 									${lastName && lastName || ''}, ${firstName && firstName || ''}
@@ -214,7 +167,7 @@ function render_case_list(p_cases, p_time)
 								<td class="td" scope="col">${timeLocked && `${timeLocked} minutes` || ''}</td>
 								<td class="td" scope="col">${lockedBy && lockedBy || ''}</td>
 								<td class="td" scope="col">${caseStatus && caseStatus || ''}</td>
-								<td class="td" scope="col"><button class="anti-btn link" onclick="save_and_finish_click()">Release</button></td>
+								<td class="td text-center" scope="col"><button class="anti-btn link" onclick="handleCaseRelease('${caseID}')">Release</button></td>
 							</tr>`
 						)
 					})}
@@ -224,8 +177,96 @@ function render_case_list(p_cases, p_time)
 	}
 	
 	return result;
+}
+
+function isCaseCheckedOut(p_case)
+{
+	let is_checked_out = false;
+  let current_date = new Date();
+  
+  if(p_case.value.date_last_checked_out != null && p_case.value.date_last_checked_out != "")
+  {
+		let try_date = null;
+		let is_date = false;
+
+		if(!(p_case.value.date_last_checked_out instanceof Date))
+		{
+				try_date = new Date(p_case.value.date_last_checked_out);
+		}
+		else
+		{
+			try_date = p_case.value.date_last_checked_out;
+		}
+		
+		if
+		(
+				getMinuteDifference(try_date, current_date) <= 120
+				// p_case.value.last_checked_out_by.toLowerCase() == g_user_name.toLowerCase()
+		)
+		{
+			is_checked_out = true;
+		}
+	}
+
+  return is_checked_out;
+}
+
+function handleCaseRelease(p_id) {
+	$.ajax({
+		url: location.protocol + '//' + location.host + '/api/case?case_id=' + p_id //call the API and get current case
+  }).done((response) => {
+		g_data = response; //set to local var
+		g_data.date_last_updated = new Date(); //set 'date_last_updated' prop
+		g_data.date_last_checked_out = null; //set 'date_last_checked_out' prop
+		g_data.last_checked_out_by = null; //set 'last_checked_out_by' prop
+
+		//save and release case with a callback to rerender the table
+		saveCaseAndRelease(g_data, getCaseSet);
+	});
 
 }
+
+function saveCaseAndRelease(p_data, p_call_back) {
+	$.ajax({
+    url: location.protocol + '//' + location.host + '/api/case',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    data: JSON.stringify(p_data),
+    type: "POST"
+  }).done(function(response) {
+		console.log("save_case: success");
+
+		if(p_call_back)
+    {
+      p_call_back();
+    }
+	}).fail(function(xhr, err) { 
+		console.log("server save_case: failed", err);
+
+		if(xhr.status == 401)
+		{
+			let redirect_url = location.protocol + '//' + location.host;
+			window.location = redirect_url;
+		}
+	});
+}
+
+function getMinuteDifference(dt1, dt2) 
+{
+  let diff =(dt2.getTime() - dt1.getTime()) / 1000;
+
+	diff /= 60;
+  return Math.abs(Math.round(diff));
+}
+
+function convertToReadableTime(millis) {
+  var minutes = Math.floor(millis / 60000);
+	var seconds = ((millis % 60000) / 1000).toFixed(0);
+	
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+
 
 
 
@@ -300,11 +341,6 @@ function render_case_list(p_cases, p_time)
 // 	let result = s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 //     return result;
 // }
-
-// function initCaseRease() {
-// 	const currentCase = 
-// }
-
 
 
 
