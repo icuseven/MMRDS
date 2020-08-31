b09cd42d27aafe89a57705910bf87d90aa195a99
icuseven <jhaines@thetagrid.com>
2020-08-31 07:12:31 -0400

    SAMS Auth

--- source-code/mmria/mmria-server/Controllers/AccountController.OIDC.cs
+++ source-code/mmria/mmria-server/Controllers/AccountController.OIDC.cs
@@ -60,28 +60,13 @@ namespace mmria.common.Controllers
             _configuration = configuration;
         }
         private IConfiguration _configuration;
-/*
-        public ActionResult Index()
-        {
-            if (TempData["email"] == null)
-            {
-                ViewBag.Message = "Log in to see your account.";
-            }
-            else
-            {
-                ViewBag.Message = $"Welcome back {TempData["email"]}!";
-                ViewBag.Content = $"Your user ID is: {TempData["id"]}";
-            }
-            return View();
-        }
-*/
+
         [AllowAnonymous] 
         public ActionResult SignIn()
         {
 
-            Response.Cookies.Delete("sid");
-            Response.Cookies.Delete("expires_at");
-
+            //Response.Cookies.Delete("sid");
+            //Response.Cookies.Delete("expires_at");
 
             var sams_endpoint_authorization = _configuration["sams:endpoint_authorization"];
             var sams_endpoint_token = _configuration["sams:endpoint_token"];
@@ -354,59 +339,15 @@ namespace mmria.common.Controllers
                 Response.Cookies.Append("expires_at", unix_time.ToString(), new CookieOptions{ HttpOnly = true });
                 //return RedirectToAction("Index", "HOME");
                 //return RedirectToAction("Index", "HOME");
-            }
+                return RedirectToAction("Index", "HOME");
 
+            }
 
 
             System.Console.WriteLine($"http_async_signin_called: {http_async_signin_called}");
             TempData["user_name"] = user.name;
             return View();
-           // return RedirectToAction("Index", "HOME");
 
-            // Generate JWT for token request
-            //var cert = new X509Certificate2(Server.MapPath("~/App_Data/cert.pfx"), "1234");
-            /*
-            var cert = new X509Certificate2();
-            var signingCredentials = new SigningCredentials(new X509SecurityKey(cert), SecurityAlgorithms.RsaSha256);
-            var header = new JwtHeader(signingCredentials);
-             
-            var header = new JwtHeader();
-            var payload = new JwtPayload
-            {
-                {"iss", sams_client_id},
-                {"sub", sams_client_id},
-                {"aud", $"{sams_endpoint_token}"},
-                {"jti", Guid.NewGuid().ToString("N")},
-                {"exp", (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds + 5 * 60}
-            };
-            var securityToken = new JwtSecurityToken(header, payload);
-            var handler = new JwtSecurityTokenHandler();
-            var tokenString = handler.WriteToken(securityToken);
-
-            // Send POST to make token request
-            using (var wb = new WebClient())
-            {
-                var data = new NameValueCollection();
-                data["client_assertion"] = tokenString;
-                data["client_assertion_type"] = HttpUtility.HtmlEncode("urn:ietf:params:oauth:client-assertion-type:jwt-bearer");
-                data["code"] = code;
-                data["grant_type"] = "authorization_code";
-
-                //var response = wb.UploadValues($"{IdpUrl}/api/openid_connect/token", "POST", data);
-                var response = wb.UploadValues($"{sams_endpoint_token}", "POST", data);
-
-                var responseString = Encoding.ASCII.GetString(response);
-                dynamic tokenResponse = JObject.Parse(responseString);
-
-                var token = handler.ReadToken((String)tokenResponse.id_token) as JwtSecurityToken;
-                var userId = token.Claims.First(c => c.Type == "sub").Value;
-                var userEmail = token.Claims.First(c => c.Type == "email").Value;
-
-                TempData["id"] = userId;
-                TempData["email"] = userEmail;
-                //return RedirectToAction("Index", "HOME");
-                return RedirectToAction("Index", "HOME");
-            }*/
         }
 
 
