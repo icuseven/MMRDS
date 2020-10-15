9dba92b60ef658912031ff94b638ffb7054d4ce3
icuseven <jhaines@thetagrid.com>
2020-10-15 16:09:32 -0400

    update.

--- source-code/mmria/data-migration/migration-set/v2.3-Migration.cs
+++ source-code/mmria/data-migration/migration-set/v2.3-Migration.cs
@@ -70,7 +70,9 @@ namespace migrate.set
 			try
 			{
 				//string metadata_url = host_db_url + "/metadata/2016-06-12T13:49:24.759Z";
-				string metadata_url = $"https://testdb-mmria.services-dev.cdc.gov/metadata/version_specification-20.07.13/metadata";
+				//string metadata_url = $"https://testdb-mmria.services-dev.cdc.gov/metadata/version_specification-20.07.13/metadata";
+
+				string metadata_url = $"{host_db_url}/metadata/version_specification-20.07.13/metadata";
 				
 				cURL metadata_curl = new cURL("GET", null, metadata_url, null, config_timer_user_name, config_timer_value);
 				mmria.common.metadata.app metadata = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.metadata.app>(await metadata_curl.executeAsync());
