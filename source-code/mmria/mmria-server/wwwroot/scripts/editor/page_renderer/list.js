179431a01529bb3a1f2449f9d2606ca4cc4ace9f
leetoufong <leetoufong@gmail.com>
2020-11-04 17:16:59 -0500

    More list fixes

--- source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/list.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/list.js
@@ -167,36 +167,35 @@ function list_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_obje
 
     var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
 
-    p_result.push("<div class='list' id='");
-    p_result.push(convert_object_path_to_jquery_id(p_object_path));
-    
-    p_result.push("' ");
-    p_result.push(" mpath='");
-    p_result.push(p_metadata_path);
-    p_result.push("' ");
+    p_result.push(`<div class="list" id="${convert_object_path_to_jquery_id(p_object_path)}" mpath="${p_metadata_path}">`);
+    // p_result.push("<div class='list' id='");
+    // p_result.push(convert_object_path_to_jquery_id(p_object_path));
+    // p_result.push("' ");
+    // p_result.push(" mpath='");
+    // p_result.push(p_metadata_path);
+    // p_result.push("' ");
+    // p_result.push(">");
 
-    p_result.push(">");
     p_result.push("<label for='");
-    p_result.push(p_object_path.replace(/\//g, "--"));
+        p_result.push(p_object_path.replace(/\//g, "--"));
     p_result.push("' style='");
-      if(style_object && style_object.prompt)
-      {
-          p_result.push(get_style_string(style_object.prompt.style));
-      }
-      p_result.push("' ");
-            
-    if(p_metadata.description && p_metadata.description.length > 0)
-    {
-        p_result.push("rel='tooltip'  data-original-title='");
-        p_result.push(p_metadata.description.replace(/'/g, "&#39;"));
-        p_result.push("'>");
-    }
-    else
-    {
-        p_result.push(">");
-    }
-    
-    p_result.push(p_metadata.prompt);
+        if(style_object && style_object.prompt)
+        {
+            p_result.push(get_style_string(style_object.prompt.style));
+        }
+        p_result.push("' ");
+              
+        if(p_metadata.description && p_metadata.description.length > 0)
+        {
+            p_result.push("rel='tooltip' data-original-title='");
+            p_result.push(p_metadata.description.replace(/'/g, "&#39;"));
+            p_result.push("'>");
+        }
+        else
+        {
+            p_result.push(">");
+        }
+        p_result.push(p_metadata.prompt);
     p_result.push("</label> ");
 
     if(p_metadata.list_display_size && p_metadata.list_display_size!="")
