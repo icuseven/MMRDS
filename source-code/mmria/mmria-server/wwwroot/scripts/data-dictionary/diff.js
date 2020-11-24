695340f68e605ac9cee05bcf2c35782c53bbe6ff
icuseven <jhaines@thetagrid.com>
2020-11-24 08:10:16 -0500

    Moved js to .js file

--- source-code/mmria/mmria-server/wwwroot/scripts/data-dictionary/diff.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/data-dictionary/diff.js
@@ -210,3 +210,28 @@ function compare_versions_click()
 
     }
 }
+
+function diffUsingJS(viewType) 
+{
+	"use strict";
+	var byId = function (id) { return document.getElementById(id); },
+		base = difflib.stringAsLines(byId("baseText").value),
+		newtxt = difflib.stringAsLines(byId("newText").value),
+		sm = new difflib.SequenceMatcher(base, newtxt),
+		opcodes = sm.get_opcodes(),
+		diffoutputdiv = byId("diffoutput"),
+		contextSize = byId("contextSize").value;
+
+	diffoutputdiv.innerHTML = "";
+	contextSize = contextSize || null;
+
+	diffoutputdiv.appendChild(diffview.buildView({
+		baseTextLines: base,
+		newTextLines: newtxt,
+		opcodes: opcodes,
+		baseTextName: "Base Text",
+		newTextName: "New Text",
+		contextSize: contextSize,
+		viewType: viewType
+	}));
+}
