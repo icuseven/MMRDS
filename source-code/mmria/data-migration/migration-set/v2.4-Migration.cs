79c1efbf3ff4ba30f3fbddc189fab6e65222427c
icuseven <jhaines@thetagrid.com>
2021-01-26 15:06:29 -0500

    2.4 migration setup

--- source-code/mmria/data-migration/migration-set/v2.4-Migration.cs
+++ source-code/mmria/data-migration/migration-set/v2.4-Migration.cs
@@ -20,7 +20,23 @@ namespace migrate.set
 		public System.Text.StringBuilder output_builder;
         private Dictionary<string,mmria.common.metadata.value_node[]> lookup;
 
+        List<Metadata_Node> all_list_set;
+
+		List<Metadata_Node> single_form_value_set;
+		List<Metadata_Node> single_form_multi_value_set;
+		List<Metadata_Node> single_form_grid_value_set;
+		List<Metadata_Node> single_form_grid_multi_value_list_set;
+		List<Metadata_Node> multiform_value_set;
+		List<Metadata_Node> multiform_multi_value_set;
+		List<Metadata_Node> multiform_grid_value_set;
+
+		List<Metadata_Node> multiform_grid_multi_value_set;
+
 		public Dictionary<string, HashSet<string>> summary_value_dictionary = new Dictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase);
+
+		public bool is_data_correction = false;
+
+
         public v2_4_Migration
         (
             string p_host_db_url, 
@@ -61,7 +77,31 @@ namespace migrate.set
 				cURL metadata_curl = new cURL("GET", null, metadata_url, null, config_timer_user_name, config_timer_value);
 				mmria.common.metadata.app metadata = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.metadata.app>(await metadata_curl.executeAsync());
             
-            
+				this.lookup = get_look_up(metadata);
+
+				all_list_set = get_metadata_node_by_type(metadata, "list");
+
+				single_form_value_set = all_list_set.Where(o=> o.is_multiform == false && o.is_grid == false && o.Node.is_multiselect == null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+				single_form_multi_value_set = all_list_set.Where(o=> o.is_multiform == false && o.is_grid == false && o.Node.is_multiselect != null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+
+				single_form_grid_value_set = all_list_set.Where(o=> o.is_multiform == false && o.is_grid == true && o.Node.is_multiselect == null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+				single_form_grid_multi_value_list_set = all_list_set.Where(o=> o.is_multiform == false && o.is_grid == true && o.Node.is_multiselect != null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+
+				multiform_value_set = all_list_set.Where(o=> o.is_multiform == true && o.is_grid == false && o.Node.is_multiselect == null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+				multiform_multi_value_set = all_list_set.Where(o=> o.is_multiform == true && o.is_grid == false && o.Node.is_multiselect != null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+
+				multiform_grid_value_set = all_list_set.Where(o=> o.is_multiform == true && o.is_grid == true && o.Node.is_multiselect == null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+				multiform_grid_multi_value_set = all_list_set.Where(o=> o.is_multiform == true && o.is_grid == true && o.Node.is_multiselect != null && (o.Node.control_style == null || !o.Node.control_style.Equals("editable",StringComparison.OrdinalIgnoreCase))).ToList();
+
+
+				var total_count = single_form_value_set.Count + single_form_grid_value_set.Count + multiform_value_set.Count + multiform_grid_value_set.Count + single_form_multi_value_set.Count + single_form_grid_multi_value_list_set.Count + multiform_multi_value_set.Count + multiform_grid_multi_value_set.Count;
+				System.Console.WriteLine($"all_list_set.Count: {all_list_set.Count} total_count: {total_count}");
+				System.Console.WriteLine($"is count the same: {all_list_set.Count == single_form_value_set.Count + single_form_grid_value_set.Count + multiform_value_set.Count + multiform_grid_value_set.Count + single_form_multi_value_set.Count + single_form_grid_multi_value_list_set.Count + multiform_multi_value_set.Count + multiform_grid_multi_value_set.Count}");
+
+				string url = $"{host_db_url}/{db_name}/_all_docs?include_docs=true";
+
+
+
             // pmss migration - start
             // 24
 /*
@@ -240,5 +280,195 @@ mhpwtdmhc_rf_treat
                 
             }
         }
+        public class Metadata_Node
+		{
+			public Metadata_Node(){}
+			public bool is_multiform { get; set; }
+			public bool is_grid { get; set; }
+
+			public string path {get;set;}
+			public mmria.common.metadata.node Node { get; set; }
+
+			public Dictionary<string,string> display_to_value { get; set; }
+			public Dictionary<string,string> value_to_display { get; set; }
+		}
+		
+
+		private List<Metadata_Node> get_metadata_node_by_type(mmria.common.metadata.app p_metadata, string p_type)
+		{
+			var result = new List<Metadata_Node>();
+			foreach(var node in p_metadata.children)
+			{
+				var current_type = node.type.ToLowerInvariant();
+				if(current_type == p_type)
+				{
+					result.Add(new Metadata_Node()
+					{
+						is_multiform = false,
+						is_grid = false,
+						path = node.name,
+						Node = node
+					});
+				}
+				else if(current_type == "form")
+				{
+					if
+					(
+						node.cardinality == "+" ||
+						node.cardinality == "*"
+					)
+					{
+						get_metadata_node_by_type(ref result, node, p_type, true, false, node.name);
+					}
+					else
+					{
+						get_metadata_node_by_type(ref result, node, p_type, false, false, node.name);
+					}
+				}
+			}
+			return result;
+		}
+
+		private void get_metadata_node_by_type(ref List<Metadata_Node> p_result, mmria.common.metadata.node p_node, string p_type, bool p_is_multiform, bool p_is_grid, string p_path)
+		{
+			var current_type = p_node.type.ToLowerInvariant();
+			if(current_type == p_type)
+			{
+				var value_to_display = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+				var display_to_value = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+				if
+				(
+					current_type == "list"
+				)
+				{
+
+					if(!string.IsNullOrWhiteSpace(p_node.path_reference))
+					{
+						//var key = "lookup/" + p_node.name;
+						var key = p_node.path_reference;
+						if(this.lookup.ContainsKey(key))
+						{
+							var values = this.lookup[key];
+
+							p_node.values = values;
+						}
+					}
+
+					foreach(var value_item in p_node.values)
+					{
+						var value = value_item.value;
+						var display = value_item.display;
+
+						if(!value_to_display.ContainsKey(value))
+						{
+							value_to_display.Add(value, display);
+						}
+
+						if(!display_to_value.ContainsKey(display))
+						{
+							display_to_value.Add(display, value);
+						}
+					}
+				}
+
+				p_result.Add(new Metadata_Node()
+				{
+					is_multiform = p_is_multiform,
+					is_grid = p_is_grid,
+					path = p_path,
+					Node = p_node,
+					value_to_display = value_to_display,
+					display_to_value = display_to_value
+				});
+			}
+			else if(p_node.children != null)
+			{
+				foreach(var node in p_node.children)
+				{
+					if(current_type == "grid")
+					{
+						get_metadata_node_by_type(ref p_result, node, p_type, p_is_multiform, true, p_path + "/" + node.name);
+					}
+					else
+					{
+						get_metadata_node_by_type(ref p_result, node, p_type, p_is_multiform, p_is_grid, p_path + "/" + node.name);
+					}
+				}
+			}
+		}
+
+        private mmria.common.metadata.node get_metadata_node(mmria.common.metadata.app p_metadata, string p_path)
+		{
+
+/*
+	example usage
+				var pregnancy_relatedness_set_node = get_metadata_node(metadata, "committee_review/pregnancy_relatedness");
+				foreach(var item in pregnancy_relatedness_set_node.values)
+				{
+					pregnancy_relatedness_set.Add(item.value);
+				}
+*/
+
+			mmria.common.metadata.node result = null;
+
+			mmria.common.metadata.node current = null;
+			
+			string[] path = p_path.Split("/");
+
+			for(int i = 0; i < path.Length; i++)
+			{
+				string current_name = path[i];
+				if(i == 0)
+				{
+					foreach(var child in p_metadata.children)
+					{
+						if(child.name.Equals(current_name, StringComparison.OrdinalIgnoreCase))
+						{
+							current = child;
+							break;
+						}
+					}
+				}
+
+				else
+				{
+
+					if(current.children != null)
+					{
+						foreach(var child2 in current.children)
+						{
+							if(child2.name.Equals(current_name, StringComparison.OrdinalIgnoreCase))
+							{
+								current = child2;
+								break;
+							}
+						}	
+					}
+					else
+					{
+						return result;
+					}
+
+					if(i == path.Length -1)
+					{
+						result = current;
+					}
+				}
+
+			}
+
+			return result;
+		}
+        private Dictionary<string,mmria.common.metadata.value_node[]> get_look_up(mmria.common.metadata.app p_metadata)
+        {
+			var result = new Dictionary<string,mmria.common.metadata.value_node[]>(StringComparer.OrdinalIgnoreCase);
+
+			foreach(var node in p_metadata.lookup)
+			{
+				result.Add("lookup/" + node.name, node.values);
+			}
+			return result;
+		}	
     }
 }
