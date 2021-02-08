4a28b72df93e0c4c2f60596482a6dd252072301b
icuseven <jhaines@thetagrid.com>
2021-02-08 08:56:17 -0500

    Updated  CentralPull to use configuration db

--- source-code/mmria/mmria-server/model/actor/quartz/Process_Central_Pull_list.cs
+++ source-code/mmria/mmria-server/model/actor/quartz/Process_Central_Pull_list.cs
@@ -10,6 +10,7 @@ namespace mmria.server.model.actor.quartz
     public class Process_Central_Pull_list : UntypedActor
     {
         private static int run_count = 0;
+        private const int SkipCount = 2;
         //protected override void PreStart() => Console.WriteLine("Rebuild_Export_Queue started");
         //protected override void PostStop() => Console.WriteLine("Rebuild_Export_Queue stopped");
 
@@ -17,22 +18,22 @@ namespace mmria.server.model.actor.quartz
         {
              
 
-             Console.WriteLine($"Rebuild_Export_Queue Baby {System.DateTime.Now}");
+             Console.WriteLine($"Process_Central_Pull_list OnRecieve {System.DateTime.Now}");
 
             
             switch (message)
             {
                 case ScheduleInfoMessage scheduleInfo:
 
-/*  */  
-                    if(run_count < 3)
+
+                    if(run_count < SkipCount)
                     {
                         run_count ++;
                         break;
                     }
-                    else if(run_count == 3)
+                    else if(run_count == SkipCount)
                     {
-                        //run_count ++;
+                        run_count ++;
                     }
                     else
                     {
@@ -45,14 +46,15 @@ namespace mmria.server.model.actor.quartz
                         }
                     }
                 
-
+/*  */  
                     if 
                     (
-                        //!Test_has_run &&
+                        //run_count == 2 &&
                         !string.IsNullOrWhiteSpace(Program.config_cdc_instance_pull_db_url) &&
                         !string.IsNullOrWhiteSpace(Program.config_cdc_instance_pull_list)
                     )
                     {
+                    
                         try
                         {
                             var db_url = $"{Program.config_couchdb_url}/{Program.db_prefix}mmrds";
@@ -102,6 +104,10 @@ namespace mmria.server.model.actor.quartz
 
                         var cdc_instance_pull = config_cdc_instance_pull_list.Split(",");
 
+                        var config_db = Program.configuration_set;
+                                    
+                                    
+
                         for (var i = 0; i < cdc_instance_pull.Length; i++)
                         {
                             var db_name_split = cdc_instance_pull[i].Split("/");
@@ -110,79 +116,83 @@ namespace mmria.server.model.actor.quartz
 
                                 var instance_name = db_name_split[0];
                                 var db_name = db_name_split[1];
-                                var db_server_url = pre_db_server_url.Replace("{prefix}", instance_name);
 
-                                string url = $"{db_server_url}/{db_name}/_all_docs?include_docs=true";
-                                var case_curl = new cURL("GET", null, url, null, Program.config_timer_user_name, Program.config_timer_value);
-                                string responseFromServer = case_curl.execute();
-                                var case_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_response_header<System.Dynamic.ExpandoObject>>(responseFromServer);
-
-                                Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings ();
-                                settings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-
-                                foreach(var case_response_item in case_response.rows)
+                                if(config_db.detail_list.ContainsKey(instance_name))
                                 {
-                                    var case_item = case_response_item.doc as IDictionary<string,object>;
+                                    var db_info = config_db.detail_list[instance_name];
 
-                                    string _id = "";
+                                   
 
-                                    if(case_item == null)
+                                    string url = $"{db_info.url}/{db_info.prefix}mmrds/_all_docs?include_docs=true";
+                                    var case_curl = new cURL("GET", null, url, null, db_info.user_name, db_info.user_value);
+                                    string responseFromServer = case_curl.execute();
+                                    var case_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.get_response_header<System.Dynamic.ExpandoObject>>(responseFromServer);
+
+                                    Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings ();
+                                    settings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+
+                                    foreach(var case_response_item in case_response.rows)
                                     {
-                                        continue;
+                                        var case_item = case_response_item.doc as IDictionary<string,object>;
+
+                                        string _id = "";
+
+                                        if(case_item == null)
+                                        {
+                                            continue;
+                                        }
+                                        else if (case_item.ContainsKey ("_id")) 
+                                        {
+                                            _id = case_item ["_id"].ToString();
+                                        }
+                                        else
+                                        {
+                                            continue;
+                                        }
+
+                                        if (_id.IndexOf ("_design/") > -1)
+                                        {
+                                            continue;
+                                        }
+
+                                        var  target_url = $"{Program.config_couchdb_url}/{Program.db_prefix}mmrds/{_id}";
+
+                                        var document_json = Newtonsoft.Json.JsonConvert.SerializeObject(case_item);
+                                        var de_identified_json = new mmria.server.util.c_cdc_de_identifier(document_json).executeAsync().GetAwaiter().GetResult();
+                                        
+                                        var de_identified_case = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(de_identified_json);
+
+                                        var de_identified_dictionary = de_identified_case as IDictionary<string,object>;
+
+                                        if(de_identified_dictionary == null)
+                                        {
+                                            continue;
+                                        }
+                                        
+                                        var revision = get_revision(target_url).GetAwaiter().GetResult();
+                                        if(!string.IsNullOrWhiteSpace(revision))
+                                        {
+                                            de_identified_dictionary["_rev"] = revision;
+                                        }                                    
+                                        
+                                        var save_json = document_json = Newtonsoft.Json.JsonConvert.SerializeObject(de_identified_dictionary);
+
+                                        var put_result_string = Put_Document(save_json, _id, target_url, Program.config_timer_user_name, Program.config_timer_value).GetAwaiter().GetResult();
+
+                                        var result = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.document_put_response>(put_result_string);
+
+                                        if(result.ok)
+                                        {
+                                            var Sync_Document_Message = new mmria.server.model.actor.Sync_Document_Message
+                                            (
+                                                _id,
+                                                de_identified_json
+                                            );
+
+                                            Context.ActorOf(Props.Create<mmria.server.model.actor.Synchronize_Case>()).Tell(Sync_Document_Message);
+                                        }
+
                                     }
-                                    else if (case_item.ContainsKey ("_id")) 
-                                    {
-                                        _id = case_item ["_id"].ToString();
-                                    }
-                                    else
-                                    {
-                                        continue;
-                                    }
-
-                                    if (_id.IndexOf ("_design/") > -1)
-                                    {
-                                        continue;
-                                    }
-
-
-
-                                    var  target_url = $"{Program.config_couchdb_url}/{Program.db_prefix}mmrds/{_id}";
-
-                                    var document_json = Newtonsoft.Json.JsonConvert.SerializeObject(case_item);
-                                    var de_identified_json = new mmria.server.util.c_cdc_de_identifier(document_json).executeAsync().GetAwaiter().GetResult();
-                                    
-                                    var de_identified_case = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(de_identified_json);
-
-                                    var de_identified_dictionary = de_identified_case as IDictionary<string,object>;
-
-                                    if(de_identified_dictionary == null)
-                                    {
-                                        continue;
-                                    }
-                                    
-                                    var revision = get_revision(target_url).GetAwaiter().GetResult();
-                                    if(!string.IsNullOrWhiteSpace(revision))
-                                    {
-                                        de_identified_dictionary["_rev"] = revision;
-                                    }                                    
-                                    
-                                    var save_json = document_json = Newtonsoft.Json.JsonConvert.SerializeObject(de_identified_dictionary);
-
-                                    var put_result_string = Put_Document(save_json, _id, target_url, Program.config_timer_user_name, Program.config_timer_value).GetAwaiter().GetResult();
-
-                                    var result = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.document_put_response>(put_result_string);
-
-                                    if(result.ok)
-                                    {
-                                        var Sync_Document_Message = new mmria.server.model.actor.Sync_Document_Message
-                                        (
-                                            _id,
-                                            de_identified_json
-                                        );
-
-                                        Context.ActorOf(Props.Create<mmria.server.model.actor.Synchronize_Case>()).Tell(Sync_Document_Message);
-                                    }
-
                                 }
                             }
                         }
