fec64495846ecf09c08864eb238cbbe06813d6db
Harrell, Dustin <oip1@cdc.gov>
2021-03-03 18:04:44 -0500

    Added in Header based authentication to service

---
+++ source-code/mmria/mmria.services/Classes/HeaderAuthenticationHandler.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using mmria.services.vitalsimport;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace mmria.services.Classes
+{
+    public class HeaderAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private string headerName = "vital-service-key";
+        private mmria.common.couchdb.ConfigurationSet _configurationSet;
+        
+        public HeaderAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+            _configurationSet = Program.DbConfigSet;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            // skip authentication if endpoint has [AllowAnonymous] attribute
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return AuthenticateResult.NoResult();
+
+            if (!Request.Headers.ContainsKey(headerName))
+            {
+                Debug.WriteLine($"Missing Authorization Header"); 
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            }
+
+            try
+            {
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers[headerName])?.Scheme?.ToString();
+                var serviceKey = _configurationSet.name_value["vital_service_key"];
+
+                if (authHeader != null && !authHeader.Equals(serviceKey))
+                {
+                    return AuthenticateResult.Fail("Invalid Service Key Header");
+                }
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier,  "apiUser"),
+                new Claim(ClaimTypes.Name, "apiUser"),
+            };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
