38e538afd0713f1acc7a91f817096eef965c1602
icuseven <jhaines@thetagrid.com>
2021-03-15 11:03:51 -0400

    Vital Group Hide/Show

--- source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/group.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/editor/page_renderer/group.js
@@ -1,47 +1,57 @@
 function group_render(p_result, p_metadata, p_data, p_ui, p_metadata_path, p_object_path, p_dictionary_path, p_is_grid_context, p_post_html_render, p_search_ctx, p_ctx)
 {
-    p_result.push("<fieldset id='");
-    p_result.push(p_metadata.name);
-    p_result.push("_id' class='group' style='");
 
-    //var key = p_dictionary_path.substring(1);
+    let vital_group_set = {
+        "/home_record/automated_vitals_group":true,
+        "/death_certificate/vitals_import_group":true,
+        "/birth_certificate_infant_fetal_section/vitals_import_group":true
+    };
 
-    var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
-    if(style_object)
+    if(!vital_group_set[p_dictionary_path])
     {
-        p_result.push(get_only_size_and_position_string(style_object.control.style));
-    }
+        p_result.push("<fieldset id='");
+        p_result.push(p_metadata.name);
+        p_result.push("_id' class='group' style='");
 
-    p_result.push("' >"); // close opening div
-    p_result.push("<legend style='");
-      var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
-      if(style_object && style_object.prompt)
-      {
-          p_result.push(get_only_font_style_string(style_object.prompt.style));
-      }
-    p_result.push("'>");
-    p_result.push(p_metadata.prompt);
-    p_result.push("</legend>");
+        //var key = p_dictionary_path.substring(1);
 
-
-
-    for(var i = 0; i < p_metadata.children.length; i++)
-    {
-        var child = p_metadata.children[i];
-
-        if(p_data[child.name] || p_data[child.name] == 0)
+        var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
+        if(style_object)
         {
-            // do nothing 
-        }
-        else
-        {
-            p_data[child.name] = create_default_object(child, {})[child.name];
+            p_result.push(get_only_size_and_position_string(style_object.control.style));
         }
 
-        Array.prototype.push.apply(p_result, page_render(child, p_data[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render, p_search_ctx));
+        p_result.push("' >"); // close opening div
+        p_result.push("<legend style='");
+        var style_object = g_default_ui_specification.form_design[p_dictionary_path.substring(1)];
+        if(style_object && style_object.prompt)
+        {
+            p_result.push(get_only_font_style_string(style_object.prompt.style));
+        }
+        p_result.push("'>");
+        p_result.push(p_metadata.prompt);
+        p_result.push("</legend>");
 
+
+
+        for(var i = 0; i < p_metadata.children.length; i++)
+        {
+            var child = p_metadata.children[i];
+
+            if(p_data[child.name] || p_data[child.name] == 0)
+            {
+                // do nothing 
+            }
+            else
+            {
+                p_data[child.name] = create_default_object(child, {})[child.name];
+            }
+
+            Array.prototype.push.apply(p_result, page_render(child, p_data[child.name], p_ui, p_metadata_path + '.children[' + i + "]", p_object_path + "." + child.name, p_dictionary_path + "/" + child.name, false, p_post_html_render, p_search_ctx));
+
+        }
+        p_result.push("</fieldset>");
     }
-    p_result.push("</fieldset>");
 
 
 }
