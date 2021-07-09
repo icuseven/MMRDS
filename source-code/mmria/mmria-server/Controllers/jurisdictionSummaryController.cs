a9c5efe8729f5ad97303ba05c8406a921eb0bca9
icuseven <jhaines@thetagrid.com>
2021-07-09 07:41:44 -0400

    Excel generate test

--- source-code/mmria/mmria-server/Controllers/jurisdictionSummaryController.cs
+++ source-code/mmria/mmria-server/Controllers/jurisdictionSummaryController.cs
@@ -29,7 +29,7 @@ namespace mmria.server.Controllers
 
             return View(await result.execute());
         }
-
+/*
         public async Task<IActionResult> GenerateReport()
         {
             System.IO.MemoryStream ms = new ();
@@ -44,6 +44,64 @@ namespace mmria.server.Controllers
 
             return File(ms, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");
         }
+        */
+
+        public async Task<IActionResult> GenerateReport()
+        {
+
+            var Template_xlsx = "database-scripts/Template.xlsx";
+            var Output_xlsx = System.IO.Path.Combine (configuration["mmria_settings:export_directory"], "Output.xlsx");
+/*
+
+            var Template_xlsx = "Template.xlsx";
+            var Output_xlsx = "Output.xlsx";
+*/
+            if(System.IO.File.Exists(Output_xlsx))
+                System.IO.File.Delete(Output_xlsx);
+
+            using (FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(new System.IO.FileInfo(Template_xlsx), new System.IO.FileInfo(Output_xlsx)))
+            {
+                //Create a worksheet with some rows
+                var worksheet = new FastExcel.Worksheet();
+                var rows = new System.Collections.Generic.List<FastExcel.Row>();
+                for (int rowNumber = 1; rowNumber < 100000; rowNumber++)
+                {
+                    var cells = new List<FastExcel.Cell>();
+                    for (int columnNumber = 1; columnNumber < 13; columnNumber++)
+                    {
+                        cells.Add(new FastExcel.Cell(columnNumber, columnNumber * System.DateTime.Now.Millisecond));
+                    }
+                    cells.Add(new FastExcel.Cell(13, "FileFormat" + rowNumber));
+                    cells.Add(new FastExcel.Cell(14, "FileFormat Developer Guide"));
+
+                    rows.Add(new FastExcel.Row(rowNumber, cells));
+                }
+                worksheet.Rows = rows;
+
+                fastExcel.Write(worksheet, "sheet1");
+            }
+
+            byte[] fileBytes = GetFile(Output_xlsx);
+            return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");;
+        }
+        byte[] GetFile(string s)
+		{
+			byte[] data;
+			int br;
+			int fs_length;
+
+			using(System.IO.FileStream fs = new System.IO.FileStream (s, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+			{
+				fs_length = (int) fs.Length;
+				data = new byte[fs.Length];
+				br = fs.Read(data, 0, data.Length);
+			}
+			if (br != (int) fs_length)
+				throw new System.IO.IOException(s);
+			return data;
+		}
+
+
 
     }
 }
