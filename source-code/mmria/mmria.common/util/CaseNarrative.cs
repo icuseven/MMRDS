b5d197b40ff52ad9fc02d68dfc9d0d67d98e89c7
icuseven <jhaines@thetagrid.com>
2021-08-24 08:25:52 -0400

    added util.CaseNarrative

---
+++ source-code/mmria/mmria.common/util/CaseNarrative.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+
+namespace mmria.common.util
+{
+    public static class CaseNarrative
+    {
+        public static string StripHtmlAttributes(string value)
+        {
+            string result = "";
+            
+            return result;
+        }
+    }
+
+}
