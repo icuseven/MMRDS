77b2f4d5648f7f68b4bb5dc46b244e74a47c76d9
icuseven <jhaines@thetagrid.com>
2022-01-13 06:04:53 -0500

    Added report metadata.

--- source-code/mmria/mmria-server/wwwroot/scripts/d3-report/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/d3-report/index.js
@@ -43,61 +43,6 @@ g_nav_map.set(11,"Emotional Stress");
 g_nav_map.set(12,"Living Arrangements");
 
 
-var indicator_map = new Map();
-indicator_map.set("mDeathCause",{
-    title:"Committee Determinations"
-});
-indicator_map.set("mDeathsbyRaceEth",{
-    title:"Race/Ethnicity"
-});
-indicator_map.set("mDeathSubAbuseEvi",{
-    title:""
-});
-indicator_map.set("mEducation",{
-    title:"Education"
-});
-indicator_map.set("mHomeless",{
-    title:"Living Arrangements"
-});
-indicator_map.set("mHxofEmoStress",{
-    title:"Emotional Stress"
-});
-indicator_map.set("mHxofSubAbu",{
-    title:""
-});
-indicator_map.set("mIncarHx",{
-    title:""
-});
-indicator_map.set("mLivingArrange",{
-    title:"Living Arrangements"
-});
-indicator_map.set("mMHTxTiming",{
-    title:""
-});
-indicator_map.set("mPregRelated",{
-    title:"Pregnancy Relatedness"
-});
-indicator_map.set("mSubstAutop",{
-    title:""
-});
-indicator_map.set("mTimingofDeath",{
-    title:"Timing of Death"
-});
-indicator_map.set("mUndCofDeath",{
-    title:"Primary Underlying Cause of Death"
-});
-indicator_map.set("mDeathCause",{
-    title:"Committee Determinations"
-});
-indicator_map.set("mDeathPrevent",{
-    title:"Preventability"
-});
-indicator_map.set("mOMBRaceRcd",{
-    title:"OMB Race Recode"
-});
-indicator_map.set("mDeathbyRace",{
-    title:"Race"
-});
 
 
 var year_options = [
---
+++ source-code/mmria/mmria-server/wwwroot/scripts/d3-report/report-metadata.js
@@ -0,0 +1,56 @@
+var indicator_map = new Map();
+indicator_map.set(1,{
+    indicator_id : "mDeathCause",
+    title:"Committee Determinations"
+});
+indicator_map.set("mDeathsbyRaceEth",{
+    title:"Race/Ethnicity"
+});
+indicator_map.set("mDeathSubAbuseEvi",{
+    title:""
+});
+indicator_map.set("mEducation",{
+    title:"Education"
+});
+indicator_map.set("mHomeless",{
+    title:"Living Arrangements"
+});
+indicator_map.set("mHxofEmoStress",{
+    title:"Emotional Stress"
+});
+indicator_map.set("mHxofSubAbu",{
+    title:""
+});
+indicator_map.set("mIncarHx",{
+    title:""
+});
+indicator_map.set("mLivingArrange",{
+    title:"Living Arrangements"
+});
+indicator_map.set("mMHTxTiming",{
+    title:""
+});
+indicator_map.set("mPregRelated",{
+    title:"Pregnancy Relatedness"
+});
+indicator_map.set("mSubstAutop",{
+    title:""
+});
+indicator_map.set("mTimingofDeath",{
+    title:"Timing of Death"
+});
+indicator_map.set("mUndCofDeath",{
+    title:"Primary Underlying Cause of Death"
+});
+indicator_map.set("mDeathCause",{
+    title:"Committee Determinations"
+});
+indicator_map.set("mDeathPrevent",{
+    title:"Preventability"
+});
+indicator_map.set("mOMBRaceRcd",{
+    title:"OMB Race Recode"
+});
+indicator_map.set("mDeathbyRace",{
+    title:"Race"
+});
