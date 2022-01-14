9c139915a2b4868b27ebd215e2c61f6f8c0571ea
icuseven <jhaines@thetagrid.com>
2022-01-14 07:55:51 -0500

    Export using case_view

--- source-code/mmria/mmria-server/util/exporter/mmrds_exporter.cs
+++ source-code/mmria/mmria-server/util/exporter/mmrds_exporter.cs
@@ -114,10 +114,6 @@ namespace mmria.server.utils
         this.qualitativeStreamWriter[3] = new System.IO.StreamWriter(System.IO.Path.Combine(export_root_directory, "case-narrative-plaintext.txt"), true);
 
 
-        string URL = this.database_url + $"/{Program.db_prefix}mmrds/_all_docs";
-        string urlParameters = "?include_docs=true";
-        cURL document_curl = new cURL("GET", null, URL + urlParameters, null, this.user_name, this.value_string);
-        dynamic all_cases = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(document_curl.execute());
 
         string metadata_url = this.database_url + $"/metadata/version_specification-{this.Configuration.version_number}/metadata";
         cURL metadata_curl = new cURL("GET", null, metadata_url, null, this.user_name, this.value_string);
@@ -228,6 +224,7 @@ namespace mmria.server.utils
 
         if (queue_item.case_filter_type == "custom")
         {
+            /*
           foreach (System.Dynamic.ExpandoObject case_row in all_cases.rows)
           {
             var check_item = ((IDictionary<string, object>)case_row)["doc"] as System.Dynamic.ExpandoObject;
@@ -249,21 +246,50 @@ namespace mmria.server.utils
 
             }
 
-          }
+          }*/
         }
 
         else
         {
+
+            try
+            {
+                string request_string = $"{Program.config_couchdb_url}/{Program.db_prefix}mmrds/_design/sortable/_view/by_date_created?skip=0&take=250000";
+
+                var case_view_curl = new mmria.server.cURL("GET", null, request_string, null, Program.config_timer_user_name, Program.config_timer_value);
+                string case_view_responseFromServer = case_view_curl.execute();
+
+                mmria.common.model.couchdb.case_view_response case_view_response = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.case_view_response>(case_view_responseFromServer);
+
+                foreach (mmria.common.model.couchdb.case_view_item cvi in case_view_response.rows)
+                {
+                    Custom_Case_Id_List.Add(cvi.id);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+/*
           foreach (System.Dynamic.ExpandoObject case_row in all_cases.rows)
           {
             all_cases_rows.Add(((IDictionary<string, object>)case_row)["doc"] as System.Dynamic.ExpandoObject);
           }
+          */
         }
 
 
 
-        foreach (System.Dynamic.ExpandoObject case_row in all_cases_rows)
+        //foreach (System.Dynamic.ExpandoObject case_row in all_cases_rows)
+        foreach(string case_id in Custom_Case_Id_List)
         {
+
+        string URL = $"{this.database_url}/{Program.db_prefix}mmrds/{case_id}";
+        cURL document_curl = new cURL("GET", null, URL, null, this.user_name, this.value_string);
+        System.Dynamic.ExpandoObject case_row = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Dynamic.ExpandoObject>(document_curl.execute());
+
           IDictionary<string, object> case_doc = case_row as IDictionary<string, object>;
 
           if
