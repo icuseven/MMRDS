3dd5d675313e0d335b40d3ba9cfd5dbbf1b42cd8
icuseven <jhaines@thetagrid.com>
2022-01-29 10:05:43 -0500

    Removed console.log

--- source-code/mmria/mmria-server/wwwroot/scripts/d3-report/report-metadata.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/d3-report/report-metadata.js
@@ -472,7 +472,7 @@ async function get_indicator_values(p_indicator_id)
         }
         else
         {
-            console.log("here");
+            //console.log("here");
         }
     }
     
