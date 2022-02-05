36bd27da04bf8b490752c98b717895e96ead591a
icuseven <jhaines@thetagrid.com>
2022-02-05 10:36:06 -0500

    DQR Update.

---
+++ source-code/mmria/mmria-server/model/DQRDetail.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+
+namespace mmria.server.model.dqr;
+
+public class DQRSDetail
+{
+    public DQRSDetail()
+    {
+
+    }
+    public string _id { get; set; }
+    public string _rev { get; set; }
+
+    public string quarter { get; set; }
+    public int? n01 { get; set; }
+	public int? n02  { get; set; }
+	public List<int> n03 { get; set; }
+	public int? n04  { get; set; }
+	public int? n05  { get; set; }
+	public int? n06  { get; set; }
+	public int? n07  { get; set; }
+	public int? n08  { get; set; }
+	public int? n09 { get; set; }
+	public int? n10 { get; set; }
+
+    public int? n11 { get; set; }
+
+    public int? n12 { get; set; }
+
+    public int? n { get; set; }
+
+    public int? n13 { get; set; }
+
+    public int? n14 { get; set; }
+
+    public int? n15 { get; set; }
+
+    public int? n16 { get; set; }
+
+    public int? n17 { get; set; }
+
+    public int? n18 { get; set; }
+
+    public int? n19 { get; set; }
+
+    public int? n20 { get; set; }
+
+    public int? n21 { get; set; }
+
+    public int? n22 { get; set; }
+
+    public int? n23 { get; set; }
+
+    public int? n24 { get; set; }
+    public int? n25 { get; set; }
+    public int? n26 { get; set; }
+    public int? n27 { get; set; }
+    public int? n28 { get; set; }
+    public int? n29 { get; set; }
+    public int? n30 { get; set; }
+    public int? n31 { get; set; }
+    public int? n32 { get; set; }
+    public int? n33 { get; set; }
+    public int? n34 { get; set; }
+    public int? n35 { get; set; }
+    public int? n36 { get; set; }
+    public int? n37 { get; set; }
+    public int? n38 { get; set; }
+    public int? n39 { get; set; }
+    public int? n40 { get; set; }
+    public int? n41 { get; set; }
+    public int? n42 { get; set; }
+    public int? n43 { get; set; }
+    public int? n44 { get; set; }
+    public int? n45 { get; set; }
+    public int? n46 { get; set; }
+    public int? n47 { get; set; }
+    public int? n48 { get; set; }
+    public int? n49 { get; set; }
+
+}
+
