dab026dc3eacdb7a605c6165075d249e6e3b05d1
icuseven <jhaines@thetagrid.com>
2022-02-07 13:51:36 -0500

    update.

--- source-code/mmria/mmria-server/wwwroot/scripts/d3-report/render11.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/d3-report/render11.js
@@ -11,7 +11,6 @@ ${render_navigation_strip(11)}
 <h3>${metadata.title}</h3>
 <p>${metadata.description}</p>
 <div align=center>${await render11_chart(p_post_html, metadata, data_list)}</div>
-//<div align=center>${await render11K_chart(p_post_html, metadata, data_list)}</div>
 <br/>
 <div align=center>${await render11_table(metadata, data_list)}</div>
 </div>
@@ -77,18 +76,27 @@ async function render11_chart(p_post_html, p_metadata, p_data_list)
             //  left: 375
         },
         axis: {
-            rotated: false, 
+            rotated: true, 
             
             x: {
                 label: {
                 text: '${p_metadata.x_axis_title}',
-                position: 'outer-center'  
+                position: 'outer-middle'  
                 },
                 tick: {
                     multiline: false,
                 },
                 type: 'category',
                 categories: [${categories}],
+            },
+            y: {
+                label: {
+                text: '${p_metadata.y_axis_title}',
+                position: 'outer-center'  
+                },
+                tick: {
+                    multiline: false,
+                }
             },
         },
         //size: {
@@ -99,199 +107,13 @@ async function render11_chart(p_post_html, p_metadata, p_data_list)
             duration: null
           },
           bindto: '#chart',
-          onrendered: function()
+         /* onrendered: function()
           {
             d3.select('#chart svg').selectAll('g.c3-axis.c3-axis-x > g.tick > text')
               .attr('transform', 'rotate(325)translate(-25,0)');
-          }
+              
+          }*/
         }); ` 
-    );
-
-    return `
-    <div class="card">
-        <div class="card-header bg-secondary">
-        <h4 class="h5">${p_metadata.chart_title}</h4>
-        </div>
-        <div class="card-body">
-            <div id="chart"></div>
-        </div>
-    </div>
-    
-    `
-}
-
-//
-// based on https://bl.ocks.org/mbostock/7555321
-//
-async function render11K_chart(p_post_html, p_metadata, p_data_list)
-{
-    const totals = new Map();
-
-    const categories = [];
-    for(var i = 0; i < p_metadata.field_id_list.length; i++)
-    {
-        const item = p_metadata.field_id_list[i];
-        if(item.name != p_metadata.blank_field_id)
-        {
-            categories.push(`"${item.title}"`);
-            totals.set(item.name, 0);
-        }
-    }
-
-    for(var i = 0; i <p_data_list.data.length; i++)
-    {
-        const item = p_data_list.data[i];
-        if(totals.has(item.field_id))
-        {
-            let val = totals.get(item.field_id);
-            totals.set(item.field_id, val + 1);
-        }
-    }
-
-    const data = [];
-
-    totals.forEach((value, key) =>
-    {
-        data.push(value);
-    });
-    
-
-    p_post_html.push
-    (
-        
-    `var chart = c3.generate({
-        legend: {
-            show: false
-        },
-        data: {
-            columns: [
-                ["${p_metadata.indicator_id}", ${data.join(",")}
-                 ],
-            ],
-            types: {
-                ${p_metadata.indicator_id}: 'bar',
-        
-            },
-            labels: true 
-        },
-        padding: {
-            //  left: 375
-        },
-        axis: {
-            rotated: false, 
-            
-            x: {
-                label: {
-                text: '${p_metadata.x_axis_title}',
-                position: 'outer-center'  
-                },
-                tick: {
-                    multiline: false,
-                },
-                type: 'category',
-                categories: [${categories}],
-            },
-        },
-        //size: {
-        //    height: 600, 
-        //    width: 600
-        //  },
-          transition: {
-            duration: null
-        },
-        bindto: '#chart',
-        onrendered: function()
-        {
-            var margin = {top: 80, right: 180, bottom: 80, left: 180},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-        
-            var x = d3.scale.ordinal()
-                .rangeRoundBands([0, width], .1, .3);
-            
-            var y = d3.scale.linear()
-                .range([height, 0]);
-            
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom");
-            
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient("left")
-                .ticks(8, "%");
-            
-            var svg = d3.select('#chart svg').append("svg")   //.selectAll('g.c3-axis.c3-axis-x > g.tick > text')
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            d3.tsv(
-                [ ["${p_metadata.indicator_id}", ${data.join(",")} ] ],
-                type,           
-                function (error, data) {
-                    x.domain(data.map(function(d) { return d.name; }));
-                    y.domain([0, d3.max(data, function(d) { return d.value; })]);
-                        
-                    svg.append("text")
-                        .attr("class", "title")
-                        .attr("x", x(data[0].name))
-                        .attr("y", -26)
-                        .text("Why Are We Leaving Facebook?");
-                    
-                    svg.append("g")
-                        .attr("class", "x axis")
-                        .attr("transform", "translate(0," + height + ")")
-                        .call(xAxis)
-                        .selectAll(".tick text")
-                        .call(wrap, x.rangeBand());
-                    
-                    svg.append("g")
-                        .attr("class", "y axis")
-                        .call(yAxis);
-                    
-                    svg.selectAll(".bar")
-                        .data(data)
-                        .enter().append("rect")
-                        .attr("class", "bar")
-                        .attr("x", function(d) { return x(d.name); })
-                        .attr("width", x.rangeBand())
-                        .attr("y", function(d) { return y(d.value); })
-                        .attr("height", function(d) { return height - y(d.value); });
-                }
-            );
-
-            function wrap(text, width) {
-                text.each(function() {
-                    var text = d3.select(this),
-                        words = text.text().split(/\s+/).reverse(),
-                        word,
-                        line = [],
-                        lineNumber = 0,
-                        lineHeight = 1.1, // ems
-                        y = text.attr("y"),
-                        dy = parseFloat(text.attr("dy")),
-                        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-                    while (word = words.pop()) {
-                    line.push(word);
-                    tspan.text(line.join(" "));
-                    if (tspan.node().getComputedTextLength() > width) {
-                        line.pop();
-                        tspan.text(line.join(" "));
-                        line = [word];
-                        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-                    }
-                    }
-                });
-                }
-
-            function type(d) {
-                d.value = +d.value;
-                return d;
-            }
-        }
-      }); `
     );
 
     return `
