e81382897c16cf832ca8813c6a12b79bebd758b1
icuseven <jhaines@thetagrid.com>
2022-02-20 18:44:36 -0500

    Date Min Max added.

--- source-code/mmria/mmria-server/wwwroot/scripts/interactive-aggregate-report/report_renderer.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/interactive-aggregate-report/report_renderer.js
@@ -268,27 +268,28 @@ function show_filter_dialog()
                 <tr>
                     <td>
                         <label for="review_begin_date">Begin</label>
-                        <input id="review_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.begin)}" onchange="review_begin_date_change(this.value)" />
+                        <input id="review_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.begin)}" max="${ControlFormatDate(g_filter.date_of_review.end)}" onchange="review_begin_date_change(this.value)" />
                     </td>
                         
                     <td>
                         <label for="review_end_date">End</label>
-                        <input  id="review_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.end)}" onchange="review_end_date_change(this.value)" />
+                        <input  id="review_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.end)}"  min="${ControlFormatDate(g_filter.date_of_review.begin)}" onchange="review_end_date_change(this.value)" />
                     </td>
                 </tr>
             </table>
         </p>
-        <p><strong>Dates of Death:</strong> 
+        <p>
+            <strong>Dates of Death:</strong> 
             <table>
                 <tr><th>&nbsp;</th><th>&nbsp;</th></tr>
                 <tr>
                     <td>
                         <label for="death_begin_date">Begin</label>
-                        <input id="death_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.begin)}" onchange="death_begin_date_change(this.value)" />
+                        <input id="death_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.begin)}" max="${ControlFormatDate(g_filter.date_of_death.end)}" onchange="death_begin_date_change(this.value)" />
                     </td>
                     <td>
                         <label for="death_end_date">End</label>
-                        <input  id="death_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.end)}" onchange="death_end_date_change(this.value)" />
+                        <input  id="death_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.end)}"  min="${ControlFormatDate(g_filter.date_of_death.begin)}" onchange="death_end_date_change(this.value)" />
                     </td>
                 </tr>
             </table>
--- source-code/mmria/mmria-server/wwwroot/scripts/overdose-data-summary/report_renderer.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/overdose-data-summary/report_renderer.js
@@ -273,12 +273,12 @@ function show_filter_dialog()
                 <tr>
                     <td>
                         <label for="review_begin_date">Begin</label>
-                        <input id="review_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.begin)}" onchange="review_begin_date_change(this.value)" />
+                        <input id="review_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.begin)}" max="${ControlFormatDate(g_filter.date_of_review.end)}" onchange="review_begin_date_change(this.value)" />
                     </td>
                         
                     <td>
                         <label for="review_end_date">End</label>
-                        <input  id="review_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.end)}" onchange="review_end_date_change(this.value)" />
+                        <input  id="review_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_review.end)}"  min="${ControlFormatDate(g_filter.date_of_review.begin)}" onchange="review_end_date_change(this.value)" />
                     </td>
                 </tr>
             </table>
@@ -289,11 +289,11 @@ function show_filter_dialog()
                 <tr>
                     <td>
                         <label for="death_begin_date">Begin</label>
-                        <input id="death_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.begin)}" onchange="death_begin_date_change(this.value)" />
+                        <input id="death_begin_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.begin)}" max="${ControlFormatDate(g_filter.date_of_death.end)}" onchange="death_begin_date_change(this.value)" />
                     </td>
                     <td>
                         <label for="death_end_date">End</label>
-                        <input  id="death_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.end)}" onchange="death_end_date_change(this.value)" />
+                        <input  id="death_end_date" type="date" value="${ControlFormatDate(g_filter.date_of_death.end)}"  min="${ControlFormatDate(g_filter.date_of_death.begin)}" onchange="death_end_date_change(this.value)" />
                     </td>
                 </tr>
             </table>
