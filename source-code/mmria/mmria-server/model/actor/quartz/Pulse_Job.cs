9aac6ccce7aef9db9b47e61ec6ffc7a202cf2c27
icuseven <jhaines@thetagrid.com>
2022-07-04 06:25:00 -0400

    Removed comments in Pulse

--- source-code/mmria/mmria-server/model/actor/quartz/Pulse_Job.cs
+++ source-code/mmria/mmria-server/model/actor/quartz/Pulse_Job.cs
@@ -8,27 +8,12 @@ using Quartz.Logging;
 
 namespace mmria.server.model
 {
-    /// <summary>
-    /// This is just a simple job that says "Hello" to the world.
-    /// </summary>
-    /// <author>Bill Kratzer</author>
-    /// <author>Marko Lahma (.NET)</author>
     public class Pulse_job : IJob
     {
 
-
-        //private static readonly ILog log = LogProvider.GetLogger(typeof (HelloJob));
-
-        //private static readonly ActorSystem actorSystem = LogProvider.GetLogger(typeof (ActorSystem));;
-        /// <summary>
-        /// Called by the <see cref="IScheduler" /> when a
-        /// <see cref="ITrigger" /> fires that is associated with
-        /// the <see cref="IJob" />.
-        /// </summary>
         public virtual Task Execute(IJobExecutionContext context)
         {
-            System.Console.WriteLine($"Quartz_Pulse - {DateTime.Now:r}");
-
+            //System.Console.WriteLine($"Quartz_Pulse - {DateTime.Now:r}");
 
             var quartzSupervisor = Program.actorSystem.ActorSelection("akka://mmria-actor-system/user/QuartzSupervisor");
             quartzSupervisor.Tell("pulse");
