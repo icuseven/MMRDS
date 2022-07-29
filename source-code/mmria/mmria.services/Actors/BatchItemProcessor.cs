d92a3c5e8338475f573891af92b9d76ecfd54f06
icuseven <jhaines@thetagrid.com>
2022-07-29 08:04:52 -0400

    CVS message update.

--- source-code/mmria/mmria.services/Actors/BatchItemProcessor.cs
+++ source-code/mmria/mmria.services/Actors/BatchItemProcessor.cs
@@ -11890,7 +11890,7 @@ CALCULATE_GESTATIONAL_AGE_AT_BIRTH_ON_BC
                 instance: HttpContext.Request.Path
             );*/
 
-            return ($"{response_string}  {ex.Message}", null);
+            return ($"Status: {ex.Status} {ex.Message} {response_string}", null);
         }
 
         return ("success", result);
