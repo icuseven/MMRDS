5a8e358a701e6b4536012e10f7ea1e4e68a06437
icuseven <jhaines@thetagrid.com>
2022-10-25 13:11:13 -0400

    Added missing Attributes.

--- source-code/mmria/mmria-server/Controllers/export_queueController.cs
+++ source-code/mmria/mmria-server/Controllers/export_queueController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
 namespace mmria.server.Controllers;
+
+[Authorize(Roles  = "abstractor, data_analyst")]
+[Route("export-queue")]
+  
 public sealed class export_queueController : Controller
 {
     private readonly IAuthorizationService _authorizationService;
