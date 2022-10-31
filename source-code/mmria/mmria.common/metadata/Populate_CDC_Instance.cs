0bb332bd5868a0ab94d4043aa394cfda36751531
icuseven <jhaines@thetagrid.com>
2022-10-31 06:43:09 -0400

    PopulateCDCInstance

--- source-code/mmria/mmria.common/metadata/Populate_CDC_Instance.cs
+++ source-code/mmria/mmria.common/metadata/Populate_CDC_Instance.cs
@@ -16,6 +16,13 @@ public sealed class Populate_CDC_Instance
     public string _id { get; set; }
     public string _rev { get; set; }
 
+    public DateTime? date_submitted { get; set; } = DateTime.Now;
+
+    public DateTime? date_completed { get; set; }
+
+    public int? duration_in_hours { get; set; } = 0;
+    public int? duration_in_minutes { get; set; } = 0;
+
    public string transfer_result { get; set; }
   public int? transfer_status_number { get; set; }
   
