2132a6de959d4e640a33904ac04fd227701aa913
icuseven <jhaines@thetagrid.com>
2022-12-07 15:57:52 -0500

    Clean up Main config.

--- source-code/mmria/mmria.services/Program.cs
+++ source-code/mmria/mmria.services/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.DI.Core;
 using Akka.DI.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,55 +44,135 @@ public sealed class Program
 
     public static void Main(string[] args)
     {
-        configuration = new ConfigurationBuilder()
-            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", true, true)
-            .AddUserSecrets<Startup>()
+        var builder = WebApplication.CreateBuilder(args);
+
+        configuration = builder.Configuration;
+
+        if (bool.Parse (configuration["mmria_settings:is_environment_based"])) 
+        {
+            Program.config_web_site_url = System.Environment.GetEnvironmentVariable ("web_site_url");
+            //Program.config_export_directory = System.Environment.GetEnvironmentVariable ("export_directory") != null ? System.Environment.GetEnvironmentVariable ("export_directory") : "/workspace/export";
+            Program.couchdb_url = System.Environment.GetEnvironmentVariable ("couchdb_url");
+            Program.db_prefix = System.Environment.GetEnvironmentVariable ("db_prefix");
+            Program.timer_user_name = System.Environment.GetEnvironmentVariable ("timer_user_name");
+            Program.timer_value = System.Environment.GetEnvironmentVariable ("timer_password");
+            Program.central_couchdb_url = System.Environment.GetEnvironmentVariable ("central_couchdb_url");
+            Program.central_timer_user_name = System.Environment.GetEnvironmentVariable ("central_timer_password");
+            Program.central_timer_value = System.Environment.GetEnvironmentVariable ("central_timer_password");
+            Program.vitals_service_key = System.Environment.GetEnvironmentVariable ("vitals_service_key");
+            Program.config_id = System.Environment.GetEnvironmentVariable ("config_id");
+
+            configuration["mmria_settings:web_site_url"] = Program.config_web_site_url;
+            //Program.config_export_directory = configuration["mmria_settings:export_directory"];
+            configuration["mmria_settings:couchdb_url"] = Program.couchdb_url;
+            configuration["mmria_settings:db_prefix"] = Program.db_prefix;
+            configuration["mmria_settings:timer_user_name"] = Program.timer_user_name;
+            configuration["mmria_settings:timer_password"] = Program.timer_value;
+            configuration["mmria_settings:central_couchdb_url"] = Program.central_couchdb_url;
+            configuration["mmria_settings:central_timer_password"] = Program.central_timer_user_name;
+            configuration["mmria_settings:central_timer_password"] = Program.central_timer_value;
+            configuration["mmria_settings:vitals_service_key"] = Program.vitals_service_key;
+            configuration["mmria_settings:config_id"] = Program.config_id;
+        }
+        else 
+        {
+            Program.config_web_site_url = configuration["mmria_settings:web_site_url"];
+            //Program.config_export_directory = configuration["mmria_settings:export_directory"];
+            Program.couchdb_url = configuration["mmria_settings:couchdb_url"];
+            Program.db_prefix = configuration["mmria_settings:db_prefix"];
+            Program.timer_user_name = configuration["mmria_settings:timer_user_name"];
+            Program.timer_value = configuration["mmria_settings:timer_password"];
+
+            Program.central_couchdb_url = configuration["mmria_settings:central_couchdb_url"];
+            Program.central_timer_user_name = configuration["mmria_settings:central_timer_password"];
+            Program.central_timer_value = configuration["mmria_settings:central_timer_password"];
+            Program.vitals_service_key = configuration["mmria_settings:vitals_service_key"];
+            Program.config_id = configuration["mmria_settings:config_id"];
+        }
+
+        DbConfigSet = GetConfiguration();
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddAuthentication("BasicAuthentication")
+            .AddScheme<Microsoft.AspNetCore.Authentication.AuthenticationSchemeOptions, mmria.services.Classes.HeaderAuthenticationHandler>("BasicAuthentication", null);
+
+        
+        var collection = new ServiceCollection();
+
+        collection.AddSingleton<mmria.common.couchdb.ConfigurationSet>(DbConfigSet);
+        collection.AddSingleton<IConfiguration>(configuration);
+        collection.AddLogging();
+
+        var provider = collection.BuildServiceProvider();
+
+        var actorSystem = ActorSystem.Create("mmria-actor-system").UseServiceProvider(provider);
+        actorSystem.ActorOf<RecordsProcessor_Worker.Actors.BatchSupervisor>("batch-supervisor");
+        actorSystem.ActorOf<mmria.services.backup.BackupSupervisor>("backup-supervisor");
+        actorSystem.ActorOf<mmria.services.populate_cdc_instance.PopulateCDCInstanceSupervisor>("populate-cdc-instance-supervisor");
+        
+        builder.Services.AddHostedService<Worker>();
+        builder.Services.AddSingleton(typeof(ActorSystem), (serviceProvider) => actorSystem);
+
+
+        Program.ActorSystem = actorSystem;
+
+        var quartzSupervisor = actorSystem.ActorOf(Props.Create<mmria.server.model.actor.QuartzSupervisor>(), "QuartzSupervisor");
+
+        quartzSupervisor.Tell("init");
+
+        
+        ISchedulerFactory schedFact = new StdSchedulerFactory();
+        Quartz.IScheduler sched = schedFact.GetScheduler().Result;
+
+        // compute a time that is on the next round minute
+        DateTimeOffset runTime = DateBuilder.EvenMinuteDate(DateTimeOffset.UtcNow);
+
+        // define the job and tie it to our HelloJob class
+        IJobDetail job = JobBuilder.Create<mmria.services.vitalsimport.Pulse_job>()
+            .WithIdentity("job1", "group1")
             .Build();
 
-            if (bool.Parse (configuration["mmria_settings:is_environment_based"])) 
-            {
-                config_web_site_url = System.Environment.GetEnvironmentVariable ("web_site_url");
-                //Program.config_export_directory = System.Environment.GetEnvironmentVariable ("export_directory") != null ? System.Environment.GetEnvironmentVariable ("export_directory") : "/workspace/export";
-                couchdb_url = System.Environment.GetEnvironmentVariable ("couchdb_url");
-                db_prefix = System.Environment.GetEnvironmentVariable ("db_prefix");
-                timer_user_name = System.Environment.GetEnvironmentVariable ("timer_user_name");
-                timer_value = System.Environment.GetEnvironmentVariable ("timer_password");
-                central_couchdb_url = System.Environment.GetEnvironmentVariable ("central_couchdb_url");
-                central_timer_user_name = System.Environment.GetEnvironmentVariable ("central_timer_password");
-                central_timer_value = System.Environment.GetEnvironmentVariable ("central_timer_password");
-                vitals_service_key = System.Environment.GetEnvironmentVariable ("vitals_service_key");
-                config_id = System.Environment.GetEnvironmentVariable ("config_id");
+        // Trigger the job to run on the next round minute
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .StartAt(runTime.AddMinutes(3))
+            .WithCronSchedule(DbConfigSet.name_value["cron_schedule"])
+            .Build();
 
-                configuration["mmria_settings:web_site_url"] = config_web_site_url;
-                //Program.config_export_directory = configuration["mmria_settings:export_directory"];
-                configuration["mmria_settings:couchdb_url"] = couchdb_url;
-                configuration["mmria_settings:db_prefix"] = db_prefix;
-                configuration["mmria_settings:timer_user_name"] = timer_user_name;
-                configuration["mmria_settings:timer_password"] = timer_value;
-                configuration["mmria_settings:central_couchdb_url"] = central_couchdb_url;
-                configuration["mmria_settings:central_timer_password"] = central_timer_user_name;
-                configuration["mmria_settings:central_timer_password"] = central_timer_value;
-                configuration["mmria_settings:vitals_service_key"] = vitals_service_key;
-                configuration["mmria_settings:config_id"] = config_id;
-            }
-            else 
-            {
-                config_web_site_url = configuration["mmria_settings:web_site_url"];
-                //Program.config_export_directory = configuration["mmria_settings:export_directory"];
-                couchdb_url = configuration["mmria_settings:couchdb_url"];
-                db_prefix = configuration["mmria_settings:db_prefix"];
-                timer_user_name = configuration["mmria_settings:timer_user_name"];
-                timer_value = configuration["mmria_settings:timer_password"];
+        sched.ScheduleJob(job, trigger);
 
-                central_couchdb_url = configuration["mmria_settings:central_couchdb_url"];
-                central_timer_user_name = configuration["mmria_settings:central_timer_password"];
-                central_timer_value = configuration["mmria_settings:central_timer_password"];
-                vitals_service_key = configuration["mmria_settings:vitals_service_key"];
-                config_id = configuration["mmria_settings:config_id"];
-            }
+        sched.Start();
 
-        CreateHostBuilder(args).Build().Run();
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+        }
+        else
+        {
+            app.UseHttpsRedirection();
+            //app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //app.UseHsts();
+        }
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        //app.MapRazorPages();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.Run(config_web_site_url);
     }
 
 
@@ -122,68 +203,6 @@ public sealed class Program
         return result;
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-            
-
-                webBuilder.UseStartup<Startup>();
-                webBuilder.UseUrls(config_web_site_url);
-            }).ConfigureServices((hostContext, services) =>
-            {
-                DbConfigSet = GetConfiguration();
-
-                var collection = new ServiceCollection();
-
-                collection.AddSingleton<mmria.common.couchdb.ConfigurationSet>(DbConfigSet);
-                collection.AddSingleton<IConfiguration>(configuration);
-                collection.AddLogging();
-                var provider = collection.BuildServiceProvider();
-
-                var actorSystem = ActorSystem.Create("mmria-actor-system").UseServiceProvider(provider);
-                actorSystem.ActorOf<RecordsProcessor_Worker.Actors.BatchSupervisor>("batch-supervisor");
-                actorSystem.ActorOf<mmria.services.backup.BackupSupervisor>("backup-supervisor");
-                actorSystem.ActorOf<mmria.services.populate_cdc_instance.PopulateCDCInstanceSupervisor>("populate-cdc-instance-supervisor");
-                
-                services.AddHostedService<Worker>();
-                services.AddSingleton(typeof(ActorSystem), (serviceProvider) => actorSystem);
-
-
-                Program.ActorSystem = actorSystem;
-
-                var quartzSupervisor = actorSystem.ActorOf(Props.Create<mmria.server.model.actor.QuartzSupervisor>(), "QuartzSupervisor");
-
-                quartzSupervisor.Tell("init");
-
-                
-                ISchedulerFactory schedFact = new StdSchedulerFactory();
-                Quartz.IScheduler sched = schedFact.GetScheduler().Result;
-
-                // compute a time that is on the next round minute
-                DateTimeOffset runTime = DateBuilder.EvenMinuteDate(DateTimeOffset.UtcNow);
-
-                // define the job and tie it to our HelloJob class
-                IJobDetail job = JobBuilder.Create<mmria.services.vitalsimport.Pulse_job>()
-                    .WithIdentity("job1", "group1")
-                    .Build();
-
-                // Trigger the job to run on the next round minute
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity("trigger1", "group1")
-                    .StartAt(runTime.AddMinutes(3))
-                    .WithCronSchedule(DbConfigSet.name_value["cron_schedule"])
-                    .Build();
-
-                sched.ScheduleJob(job, trigger);
-
-
-                sched.Start();
-
-
-
-
-
-            });
+    
 }
 
