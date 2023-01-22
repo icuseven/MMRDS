3c5e76274399b96fe9ca2edc3220ae72004c3bb1
icuseven <jhaines@thetagrid.com>
2023-01-22 16:02:54 -0500

    Dev Config.

--- source-code/mmria/mmria-pmss-client/Program.cs
+++ source-code/mmria/mmria-pmss-client/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
@@ -16,11 +17,36 @@ public class Program
     public static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
         builder.RootComponents.Add<App>("app");
 
+        if(builder.HostEnvironment.IsDevelopment())
+        {
+            string api_server_uri = string.Empty;
+        
+            using(var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            })
+            {
+                var config = await httpClient.GetFromJsonAsync<System.Collections.Generic.Dictionary<string,string>>("config.json");
+                api_server_uri = config["mmria_settings:web_site_url"];
+
+                Console.WriteLine($"api_server_uri: {api_server_uri}");
+            }
+            
+
+            builder.Services.AddHttpClient("base_client", c => c.BaseAddress = new Uri($"{api_server_uri}/"));
+        }
+        else
+        {
+            builder.Services.AddHttpClient("base_client", c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+        }
+
         //builder.Services.AddHttpClient("base_client", c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-        builder.Services.AddHttpClient("base_client", c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-        builder.Services.AddHttpClient("database_client", c => c.BaseAddress = new Uri("http://localhost:5984/"));
+        
+        //builder.Services.AddHttpClient("api_client", c => c.BaseAddress = new Uri());
+        //builder.Services.AddHttpClient("database_client", c => c.BaseAddress = new Uri("http://localhost:5984/"));
         
 /*        
         builder.Services.AddHttpClient<IDatabaseService, DatabaseService>(c => new HttpClient { BaseAddress = new Uri("http://localhost:5984/") });
