59ce20d3b4db703f79f671f3a1b2441362b8ced5
icuseven <jhaines@thetagrid.com>
2023-01-29 10:44:56 -0500

    Steve API Prep

---
+++ source-code/mmria/mmria-server/wwwroot/scripts/steveMMRIA/index.js
@@ -0,0 +1,17 @@
+let BeginDate
+let EndDate
+let Mailbox
+let StartDownloadButton
+
+
+window.onload = main;
+
+function main()
+{
+    BeginDate = document.getElementById("BeginDate");
+    EndDate = document.getElementById("EndDate");
+    Mailbox = document.getElementById("Mailbox");
+    StartDownloadButton = document.getElementById("StartDownloadButton");
+    
+}
+
---
+++ source-code/mmria/mmria-server/wwwroot/scripts/stevePRAMS/index.js
@@ -0,0 +1,16 @@
+let BeginDate
+let EndDate
+let Mailbox
+let StartDownloadButton
+
+
+window.onload = main;
+
+function main()
+{
+    BeginDate = document.getElementById("BeginDate");
+    EndDate = document.getElementById("EndDate");
+    Mailbox = document.getElementById("Mailbox");
+    StartDownloadButton = document.getElementById("StartDownloadButton");
+    
+}
