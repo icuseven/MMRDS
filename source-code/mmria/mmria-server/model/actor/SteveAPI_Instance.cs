d1a8ad6be52acd9d8288ec2f37f8a44eaf4df43b
icuseven <jhaines@thetagrid.com>
2023-02-04 18:02:19 -0500

    STEVE Api - Prep for All download

--- source-code/mmria/mmria-server/model/actor/SteveAPI_Instance.cs
+++ source-code/mmria/mmria-server/model/actor/SteveAPI_Instance.cs
@@ -51,16 +51,15 @@ public sealed class SteveAPI_Instance : ReceiveActor
 
             //System.Console.WriteLine(response);
 
-            var auth_reponse = System.Text.Json.JsonSerializer.Deserialize<AuthResponse>(response);
+            var auth_response = System.Text.Json.JsonSerializer.Deserialize<AuthResponse>(response);
 
             var list_mailboxes_url = $"{base_url}/mailbox";
             var mail_box_curl = new cURL("GET", null, list_mailboxes_url, null, null, null);        
-            mail_box_curl.AddHeader("Authorization","Bearer " + auth_reponse.token); 
+            mail_box_curl.AddHeader("Authorization","Bearer " + auth_response.token); 
             response = mail_box_curl.execute();
             //System.Console.WriteLine(response);
 
-            var GetMailboxListResult = System.Text.Json.JsonSerializer.Deserialize<GetMailboxListResult>(response);
-            
+            var GetMailboxListResult = System.Text.Json.JsonSerializer.Deserialize<GetMailboxListResult>(response);            
 
             var download_directory = System.IO.Path.Combine(message.download_directory,message.file_name);
 
@@ -69,87 +68,16 @@ public sealed class SteveAPI_Instance : ReceiveActor
             var ErrorList = new List<string>();
             var SuccessCount = 0;
 
-            foreach(var mail_box in GetMailboxListResult.mailboxes)
-            {
-                if(mail_box.routingCode != "DRH") continue;
-
-                if(!steve_file_map.ContainsKey(message.Mailbox)) continue;
-                
-                if
-                (
-                    steve_file_map[message.Mailbox] == "PRAMS" &&
-                    mail_box.listName.ToUpper() != "PRAMS"
-                )
-                {
-                    continue;
-                }
-                else
-                {
-                    if
-                    (
-                        mail_box.listName.ToUpper() != "JURISDICTION DATA" ||  
-                        mail_box.fileType != steve_file_map[message.Mailbox]
-                    ) continue;
-
-                }
-
-                var mailbox_unread_url = $"{base_url}/mailbox/{mail_box.mailboxId}/all?count=1000&fromDate={ToRequestString(message.BeginDate)}&toDate={ToRequestString(message.EndDate)}";
-                var mailbox_unread_curl = new cURL("GET", null, mailbox_unread_url, null, null, null);        
-                mailbox_unread_curl.AddHeader("Authorization","Bearer " + auth_reponse.token); 
-                response = mailbox_unread_curl.execute();
-
-                var UnreadMessageResult = System.Text.Json.JsonSerializer.Deserialize<MailBoxMessageResult>(response);
-                if(UnreadMessageResult.messages?.Length > 0)
-                {
-
-                    using (var client = new System.Net.Http.HttpClient())
-                    {
-                        client.DefaultRequestHeaders.Add("Authorization","Bearer " + auth_reponse.token);
-
-                        foreach(var msg in UnreadMessageResult.messages)
-                        {
-                            var message_id = msg.messageId;
-                            var download_message_url = $"{base_url}/file/{message_id}";
-                            var message_path = System.IO.Path.Combine(download_directory, msg.fileName);
-                            try
-                            {
-                                /*
-                                var download_message_curl = new cURL("GET", null, download_message_url, null, null, null);        
-                                download_message_curl.AddHeader("Authorization","Bearer " + auth_reponse.token); 
-                                response = download_message_curl.execute();
-                                */
-
-                                //System.IO.File.WriteAllText(message_path, response);
-
-                               
-
-
-                                using (var client_response = client.GetAsync(download_message_url).Result)
-                                {
-                                    using (var content = client_response.Content)
-                                    {
-                                        using (var fs = new System.IO.FileStream(message_path, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
-                                        {
-                                            client_response.Content.CopyToAsync(fs).GetAwaiter().GetResult();
-                                            //await fs.FlushAsync();
-                                            
-                                        }
-                                    }
-                                }
-
-                                SuccessCount += 1;
-
-                                
-                                //System.Console.WriteLine(response);
-                            }
-                            catch(Exception ex)
-                            {
-                                ErrorList.Add($"{message_path} => {ex.Message}");
-                            }
-                        }
-                    }
-                }
-            }
+            OneMailBox
+            (
+                message,
+                GetMailboxListResult,
+                base_url,
+                auth_response.token,
+                download_directory,
+                ref SuccessCount,
+                ref ErrorList
+            );
 
 
             System.IO.File.WriteAllText
@@ -197,6 +125,95 @@ public sealed class SteveAPI_Instance : ReceiveActor
 
             Context.Stop(this.Self);
         });
+    }
+
+    void OneMailBox
+    (
+        DownloadRequest message,
+        GetMailboxListResult GetMailboxListResult,
+        string base_url,
+        string token,
+        string download_directory,
+        ref int SuccessCount,
+        ref List<string> ErrorList
+    )
+    {
+        
+        foreach(var mail_box in GetMailboxListResult.mailboxes)
+        {
+            if(mail_box.routingCode != "DRH") continue;
+
+            if(!steve_file_map.ContainsKey(message.Mailbox)) continue;
+            
+            if
+            (
+                steve_file_map[message.Mailbox] == "PRAMS" &&
+                mail_box.listName.ToUpper() != "PRAMS"
+            )
+            {
+                continue;
+            }
+            else
+            {
+                if
+                (
+                    mail_box.listName.ToUpper() != "JURISDICTION DATA" ||  
+                    mail_box.fileType != steve_file_map[message.Mailbox]
+                ) continue;
+
+            }
+
+            var mailbox_unread_url = $"{base_url}/mailbox/{mail_box.mailboxId}/all?count=1000&fromDate={ToRequestString(message.BeginDate)}&toDate={ToRequestString(message.EndDate)}";
+            var mailbox_unread_curl = new cURL("GET", null, mailbox_unread_url, null, null, null);        
+            mailbox_unread_curl.AddHeader("Authorization","Bearer " + token); 
+            var response = mailbox_unread_curl.execute();
+
+            var UnreadMessageResult = System.Text.Json.JsonSerializer.Deserialize<MailBoxMessageResult>(response);
+            if(UnreadMessageResult.messages?.Length > 0)
+            {
+
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization","Bearer " + token);
+
+                    foreach(var msg in UnreadMessageResult.messages)
+                    {
+                        var message_id = msg.messageId;
+                        var download_message_url = $"{base_url}/file/{message_id}";
+                        var message_path = System.IO.Path.Combine(download_directory, msg.fileName);
+                        try
+                        {
+                            /*
+                            var download_message_curl = new cURL("GET", null, download_message_url, null, null, null);        
+                            download_message_curl.AddHeader("Authorization","Bearer " + auth_reponse.token); 
+                            response = download_message_curl.execute();
+                            */
+
+                            //System.IO.File.WriteAllText(message_path, response);
+
+                            using (var client_response = client.GetAsync(download_message_url).Result)
+                            {
+                                using (var content = client_response.Content)
+                                {
+                                    using (var fs = new System.IO.FileStream(message_path, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
+                                    {
+                                        client_response.Content.CopyToAsync(fs).GetAwaiter().GetResult();
+                                        //await fs.FlushAsync();
+                                        
+                                    }
+                                }
+                            }
+
+                            SuccessCount += 1;
+                        }
+                        catch(Exception ex)
+                        {
+                            ErrorList.Add($"{message_path} => {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     string ToRequestString(DateTime value)
