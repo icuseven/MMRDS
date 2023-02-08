5a732e76a4f82545bec5c506725f6364082e3f87
icuseven <jhaines@thetagrid.com>
2023-02-08 16:38:42 -0500

    PMSS - Attachement_Item

---
+++ source-code/mmria/mmria-pmss-client/metadata/Attachment.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace mmria.common.metadata;
+
+public class Attachment_Item
+{
+    public string content_type { get; set; }
+    public int revpos { get; set; }
+    public string digest { get; set; }
+    public int length { get; set; }
+    public bool stub { get; set; }
+}
+
+public class Attachment
+{
+/*
+
+  "_attachments": {
+    "validator.js": {
+      "content_type": "text/*",
+      "revpos": 1778,
+      "digest": "md5-tSEsLVhhtv5lesgeIC3VSw==",
+      "length": 503383,
+      "stub": true
+    },
+
+    */
+
+
+
+}
+
+
+
