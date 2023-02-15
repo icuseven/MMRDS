3fc79b0cccdf790e42f88e925c96480998d2a37f
icuseven <jhaines@thetagrid.com>
2023-02-15 13:18:27 -0500

    Conversion Calc - enable/disable fixes.

--- source-code/mmria/mmria-server/wwwroot/scripts/case/conversion-calculator.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/case/conversion-calculator.js
@@ -136,16 +136,16 @@ function cc_render_convert(p_value, p_length)
     } 
     else
     {
-        is_able_to_convert = is_able_to_convert || (cc_cm_input.value != null && cc_cm_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_in_input.value != null && cc_in_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_m_input.value != null && cc_m_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_ft_input.value != null && cc_ft_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_lbs_input.value != null && cc_lbs_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_kg_input.value != null && cc_kg_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_oz_input.value != null && cc_oz_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_g_input.value != null && cc_g_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_f_input.value != null && cc_f_input.value !== '');
-        is_able_to_convert = is_able_to_convert || (cc_c_input.value != null && cc_c_input.value !=='');
+        is_able_to_convert = is_able_to_convert || (cc_cm_input.value != null && cc_cm_input.value !== '' && cc_cm_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_in_input.value != null && cc_in_input.value !== '' && cc_in_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_m_input.value != null && cc_m_input.value !== '' && cc_m_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_ft_input.value != null && cc_ft_input.value !== '' && cc_ft_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_lbs_input.value != null && cc_lbs_input.value !== '' && cc_lbs_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_kg_input.value != null && cc_kg_input.value !== '' && cc_kg_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_oz_input.value != null && cc_oz_input.value !== '' && cc_oz_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_g_input.value != null && cc_g_input.value !== '' && cc_g_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_f_input.value != null && cc_f_input.value !== '' && cc_f_input.value !== '.');
+        is_able_to_convert = is_able_to_convert || (cc_c_input.value != null && cc_c_input.value !=='' && cc_c_input.value !=='.');
     }
 
 
@@ -497,9 +497,10 @@ function cc_only_numeric_input(evt)
     {
 
     }
-    else //if (evt.which == cr_enter)
+    else 
     {
         cc_render_convert(evt.which - zero, evt.srcElement.value.length);
+        if (evt.srcElement.value.readOnly == false)
         switch(evt.srcElement.id)
         {
 
@@ -544,6 +545,8 @@ function cc_onKeyDown(event)
     const BACKSPACE = 8;
     const TAB = 9;
     var key = event.keyCode || event.charCode;
+
+    
 
     if( key == BACKSPACE)
     {
