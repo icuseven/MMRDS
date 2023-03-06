8a14b981d472b0591ecfa87cfb70129ea955ae26
icuseven <jhaines@thetagrid.com>
2023-03-06 15:10:38 -0500

    508 set modal

--- source-code/mmria/mmria-server/wwwroot/scripts/mmria.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/mmria.js
@@ -1149,7 +1149,7 @@ var $mmria = function()
     
                 let html = [];
                 html.push(`
-                    <div class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
+                    <div aria-modal="true" class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
                         <span id="ui-id-1" class="ui-dialog-title" style="font-family: 'Open-Sans';">${p_title.length > 100 ? p_title.slice(0,97) + "..." : p_title}</span>
                         <button type="button" class="ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close" title="×" onclick="$mmria.data_dictionary_dialog_click()"><span class="ui-button-icon ui-icon ui-icon-closethick"></span><span class="ui-button-icon-space"> </span>×</button>
                     </div>
@@ -1233,7 +1233,7 @@ var $mmria = function()
     
                 const html = [];
                 html.push(`
-                    <div class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
+                    <div aria-modal="true" class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
                         <span id="ui-id-1" class="ui-dialog-title">Conversion Calculator</span>
                         <button type="button" class="ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close" title="×" onclick="$mmria.converter_calculater_dialog_click()"><span class="ui-button-icon ui-icon ui-icon-closethick"></span><span class="ui-button-icon-space"> </span>×</button>
                     </div>
@@ -1419,7 +1419,7 @@ var $mmria = function()
     
                 let html = [];
                 html.push(`
-                    <div class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
+                    <div aria-modal="true" class="ui-dialog-titlebar modal-header bg-primary ui-widget-header ui-helper-clearfix">
                         <span id="ui-id-1" class="ui-dialog-title">${Title_Text[0]}</span>
                         <button type="button" class="ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close" title="×" onclick="$mmria.pin_un_pin_dialog_click()"><span class="ui-button-icon ui-icon ui-icon-closethick"></span><span class="ui-button-icon-space"> </span>×</button>
                     </div>
