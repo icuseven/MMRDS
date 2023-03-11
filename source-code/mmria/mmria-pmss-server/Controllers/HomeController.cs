026eddc342766056d77f6c3f2299c393602de26e
icuseven <jhaines@thetagrid.com>
2023-03-11 11:54:08 -0500

    MMRIA PMSS

--- source-code/mmria/mmria-pmss-server/Controllers/HomeController.cs
+++ source-code/mmria/mmria-pmss-server/Controllers/HomeController.cs
@@ -18,21 +18,26 @@ namespace mmria_pmss_server.Controllers;
 
 public sealed class HomeController : Controller
 {
-    IConfiguration _configuration;
+    readonly IConfiguration _configuration;
+    readonly IWebHostEnvironment _webHostEnvironment;
 
     public HomeController
     (
-        IConfiguration configuration
+        IConfiguration configuration,
+        IWebHostEnvironment webHostEnvironment
     )
     {
         _configuration = configuration;
+        _webHostEnvironment= webHostEnvironment;
     }
 
     [HttpGet]
     //[Authorize(AuthenticationSchemes = "BasicAuthentication")]
     public async Task<IActionResult> Index()
     {
-        return View();
+        string webRootPath = _webHostEnvironment.ContentRootPath;
+        string path = Path.Combine(webRootPath , "index.html");
+        return File(await System.IO.File.ReadAllTextAsync(path), "text/html");
     }
 
 }
