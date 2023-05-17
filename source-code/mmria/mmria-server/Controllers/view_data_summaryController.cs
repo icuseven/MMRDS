54ece6e38625626963b3d1760089634b7ee28ce7
icuseven <jhaines@thetagrid.com>
2023-05-17 18:51:12 -0400

    View Data Summary Prep.

---
+++ source-code/mmria/mmria-server/Controllers/view_data_summaryController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace mmria.server.Controllers;
+
+[AllowAnonymous] 
+
+
+public sealed class view_data_summaryController : Controller
+{
+    private readonly IAuthorizationService _authorizationService;
+
+    public view_data_summaryController(IAuthorizationService authorizationService)
+    {
+        _authorizationService = authorizationService;
+;
+    }
+
+    [Route("view-data-summary")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ source-code/mmria/mmria-server/wwwroot/scripts/view-data-summary/index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+var g_metadata = null;
+var g_metadata_set = {};
+var g_release_version_name = null;
+var g_release_version_specification = null;
+var g_selected_version_specification = null;
+var g_selected_version_name = null;
+var g_version_list = null;
+
+var g_filter = 	{
+	date_of_death:
+	{
+		year: [
+			'all'
+		],
+		month: [
+			'all'
+		],
+		day: [
+			'all'
+		],
+	},
+	date_range: [
+		{
+			from: 'all',
+			to:	'all'
+		}
+	],
+	case_status: [
+		'all'
+	],
+	case_jurisdiction: [
+		'/all'
+	],
+	selected_form: "",
+	search_text: ""
+};
+
+(async function() {
+
+	await get_release_version();
+})()
+
+
+async function get_release_version()
+{
+	let response = await $.ajax({
+		url: `${location.protocol}//${location.host}/api/version/release-version`
+	});
+    
+    
+    g_release_version_name = response;
+    g_selected_version_name = g_release_version_name;
+	
+	response = await $.ajax({
+		url: `${location.protocol}//${location.host}/api/metadata/version_specification-${g_release_version_name}`
+	});
+
+    g_release_version_specification = response;
+    g_selected_version_specification = g_release_version_specification;
+	
+    response = await $.ajax
+    ({
+            url: `${location.protocol}//${location.host}/api/version/list`,
+    });
+
+    
+    g_version_list = response;
+
+    for(let i = 0; i < g_version_list.length; i++)
+    {
+        let item = g_version_list[i];
+
+        if(item._id.indexOf("version_specification") == 0 && g_metadata_set[item._id] == null)
+        {
+            g_metadata_set[item._id] = await load_metadata(item.name);
+        }
+    }
+
+
+    g_metadata = g_metadata_set[g_selected_version_specification._id];
+
+    document.getElementById('form_content_id').innerHTML = dictionary_render(g_metadata, "").join("")  + '<br/>';
+
+	$('.spinner-content').removeClass('spinner-active');
+}
+
+async function load_metadata(p_version_id)
+{
+    let result;
+
+    result = await $.ajax
+    (
+        {
+            url: `${location.protocol}//${location.host}/api/version/${p_version_id}/metadata`,
+        }
+    );
+
+    return result;
+}
+
+
+async function metadata_version_filter_change(p_value)
+{
+
+    let idx = g_version_list.findIndex((x)=> {return x._id == p_value;});
+    if(idx ==-1)
+    {
+        g_selected_version_name = g_release_version;
+        g_selected_version_specification = g_release_version_name;
+    }
+    else
+    {
+        g_selected_version_name =  g_version_list[idx].name;
+        g_selected_version_specification = g_version_list[idx];
+    }
+
+    g_metadata = g_metadata_set[g_selected_version_specification._id];
+
+    document.getElementById('form_content_id').innerHTML = dictionary_render(g_metadata, "").join("")  + '<br/>';
+}
+
+function render_metadata_version_filter()
+{
+    let html_result = []
+
+    html_result.push(`<option value="">(Select Version)</option>`)
+    for(let i = 0; i < g_version_list.length; i++)
+    {
+        let item = g_version_list[i];
+        let is_selected = "";
+
+        if(item.name == g_selected_version_name)
+        {
+            is_selected = "selected=true"
+        }
+        
+        if(item._id.indexOf("_design/auth") < 0 && item.name != null)
+        {
+          html_result.push(`<option value="${item._id}" ${is_selected}>${item.name}</option>`)
+        }
+    }
+
+
+    return html_result.join("");
+
+}
+
+function search_text_change(p_value)
+{
+	g_filter.search_text = p_value;
+}
+
+
