a5d76feb703aac26279e30591de2bf15699a9635
icuseven <jhaines@thetagrid.com>
2023-05-21 14:33:33 -0400

    Sync data-summary-view

--- source-code/mmria/mmria-server/util/c_document_sync_all.cs
+++ source-code/mmria/mmria-server/util/c_document_sync_all.cs
@@ -202,6 +202,27 @@ public sealed class Report_PowerBI_Index_Struct
         
         }
 
+        try
+        {
+            string current_directory = AppContext.BaseDirectory;
+            if(!System.IO.Directory.Exists(System.IO.Path.Combine(current_directory, "database-scripts")))
+            {
+                current_directory = System.IO.Directory.GetCurrentDirectory();
+            }
+
+            using (var  sr = new System.IO.StreamReader(System.IO.Path.Combine( current_directory,  "database-scripts/data-summary-view.json")))
+            {
+                string result = await sr.ReadToEndAsync ();
+                var create_de_id_curl = new mmria.getset.cURL ("PUT", null, this.couchdb_url + $"/report/_design/data_summary_view_report", result, this.user_name, this.user_value);
+                await create_de_id_curl.executeAsync ();					
+            }
+
+        }
+        catch (Exception)
+        {
+        
+        }
+
         var curl = new mmria.server.cURL ("GET", null, this.couchdb_url + $"/{Program.db_prefix}mmrds/_all_docs?include_docs=true", null, this.user_name, this.user_value);
         string res = await curl.executeAsync ();
 /*
