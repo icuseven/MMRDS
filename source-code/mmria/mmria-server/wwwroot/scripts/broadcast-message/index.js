8204d379cb0ee1933ca2d459168e1b99ca2317ba
Nick Chapman <ptr8@cdc.gov>
2023-06-06 10:39:51 -0600

    implemented starting UI for broadcast message functionality

--- source-code/mmria/mmria-server/wwwroot/scripts/broadcast-message/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/broadcast-message/index.js
@@ -1,7 +1,41 @@
 'use strict';
-var g_message_one = { tite: "fjd"
+var MESSAGE_ONE_ID = "message-one-";
+var MESSAGE_TWO_ID = "message-two-";
+var g_message_data = {
+    _id: "broadcast-message-list",
+    _rev: null,
+    date_created: "2023-05-12T09:12:33.108Z",
+    created_by: "user1",
+    date_last_updated: "2023-05-12T09:12:33.108Z",
+    last_updated_by: "user1",
+    data_type: "broadcast_message_list",
+    message_one: {
+        draft: {
+            title: "Maintenance scheduled for HH:MM PM on MM/DD/YYYY - HH:MM PM on MM/DD/YYY. Please save case data locally and upload after this maintenance period to prevent data loss.",
+            body: "",
+            type: "warning"
+        },
+        published: {
+            title: "",
+            body: "",
+            type: "",
+        },
+        publish_status: 0
+    },
+    message_two: {
+        draft: {
+            title: "MMRIA version 3.1.1 is now live - see release notes for additional information.",
+            body: "If text has been entered in the content body section of this message, it will appear here.",
+            type: "information",
+        },
+        published: {
+            title: "System maintenance until HH:MM PM on MM/DD/YYY. Please save case data locally and upload after this maintenance period to prevent data loss.",
+            body: "If text has been entered in the content body section of this message, it will appear here.",
+            type: "error",
+        },
+        publish_status: 1
+    },
 };
-var g_message_two = {};
 
 (async function() {
 
@@ -15,7 +49,50 @@ async function main()
 	let response = await $.ajax({
 		url: `${location.protocol}//${location.host}/api/version/release-version`
 	});*/
+    var broadcastMessages = getMessages();
+    document.getElementById('form_content_id').innerHTML = render(broadcastMessages).join("");
+    setBroadcastMessageFormState(broadcastMessages);
+}
 
-    document.getElementById('form_content_id').innerHTML = render().join("");
+function saveDraft(g_message_id){
+    //To-do: save draft
+    var messageContent = createMessageContent(g_message_id);
+    console.log(messageContent);
+}
 
+function publishLatestDraft(g_message_id){
+
+}
+
+function unpublishMessage(g_message_id){
+
+}
+
+function reset(g_message_id){
+
+}
+
+function createMessageContent(g_message_id){
+    var g_form = document.getElementById(g_message_id + "form");
+    console.log(g_form);
+    return {
+        title: g_form[g_message_id + "title"].value,
+        body: g_form[g_message_id + "body"].value,
+        type: g_form[g_message_id + "type"].value,
+    }
+}
+
+function getMessages() {
+    return [Object.assign(g_message_data.message_one, { id: MESSAGE_ONE_ID }), Object.assign(g_message_data.message_two, { id: MESSAGE_TWO_ID })];
+}
+
+function setBroadcastMessageFormState(broadcastMessages){
+    var g_message_one_form = document.getElementById(MESSAGE_ONE_ID + "form");
+    var g_message_two_form = document.getElementById(MESSAGE_TWO_ID + "form");
+    g_message_one_form[MESSAGE_ONE_ID + "title"].value = broadcastMessages[0].draft.title;
+	g_message_one_form[MESSAGE_ONE_ID + "body"].value = broadcastMessages[0].draft.body;
+	g_message_one_form[MESSAGE_ONE_ID + broadcastMessages[0].draft.type].checked = true;
+	g_message_two_form[MESSAGE_TWO_ID + "title"].value = broadcastMessages[1].draft.title;
+	g_message_two_form[MESSAGE_TWO_ID + "body"].value = broadcastMessages[1].draft.body;
+	g_message_two_form[MESSAGE_TWO_ID + broadcastMessages[1].draft.type].checked = true;
 }
--- source-code/mmria/mmria-server/wwwroot/scripts/broadcast-message/renderer.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/broadcast-message/renderer.js
@@ -1,61 +1,128 @@
-function render()
+function render(messages)
 {
-    const result = [];
-
+    var result = [];
+    var message_one = messages[0];
+    var message_two = messages[1];
     result.push
     (
         `
-        <p>Message 1 (Published)</p>
-        <p>Title (Limit 250 characters)</p>
-        <input type=text id=message-one-title />
-
-        <p>Detail Content (Limit 2000 characters)</p>
-        <textarea id="message-one-detail" rows=10 cols=80>
-
-
-        </textarea>
-
-        <p>Type</p>
-
-Error (red)
-
-
-Message 1 (Published)
-
-Published Version
-
-This is a previously published message.
-
-Warning (yellow)
-
-
-Information (purple)
-
-
-Type
-
-Error (red)
-
-
-Draft Preview
-
-
-Draft Preview
-
-
-Title (Limit 250 characters)
-
-Published Version
-
-No message published.
-
-Publish Latest Draft
-
-Unpublish Message
-
-Reset
+        <form id="message-one-form">
+            <h2>Message 1 (${message_one.publish_status == 0 ? 'Unpublished' : 'Published'})</h2>
+            <p>Title (Limit 250 characters)</p>
+            <input type=text id=message-one-title maxlength="250" />
+            <p>Detail Content (Limit 2000 characters)</p>
+            <textarea id="message-one-body" rows=10 cols=80 maxlength="2000"></textarea>
+            <fieldset>
+                <legend>Type</legend>
+                <div>
+                    <label for="message-one-information">
+                        <input type="radio" id="message-one-information" name="message-one-type" value="information" aria-label="Information"> Information
+                    </label>
+                </div>
+                <div>
+                    <label for="message-one-warning">
+                        <input type="radio" id="message-one-warning" name="message-one-type" value="information" aria-label="Information"> Warning
+                    </label>
+                </div>
+                <div>
+                    <label for="message-one-error">
+                        <input type="radio" id="message-one-error" name="message-one-type" value="information" aria-label="Error"> Error
+                    </label>
+                </div>
+            </fieldset>
+            ${createTypePreviewHTML(message_one)}
+            <div>
+                <input class="btn btn-primary" type="button" value="Save Draft" onclick="saveDraft('${message_one.id}')" />
+                <input class="btn btn-primary" type="button" value="Publish Latest Draft" onclick="publishLatestDraft("${message_one.id}")" />
+                <input class="btn btn-primary" type="button" value="Unpublish Message" onclick="unpublishMessage("${message_one.id}")" />
+                <input class="btn btn-cancel" type="button" value="Reset" onclick="reset("${message_one.id}")" />
+            </div>
+        </form>
         `
     );
 
+    result.push( `
+        <form id="message-two-form">
+            <h2>Message 2 (${message_two.publish_status == 0 ? 'Unpublished' : 'Published'})</h2>
+            <p>Title (Limit 250 characters)</p>
+            <input type=text id=message-two-title maxlength="250" />
+            <p>Detail Content (Limit 2000 characters)</p>
+            <textarea id="message-two-body" rows=10 cols=80 maxlength="2000"></textarea>
+            <fieldset>
+                <legend>Type</legend>
+                <div>
+                    <label for="message-two-information">
+                        <input type="radio" id="message-two-information" name="message-two-type" value="information" aria-label="Information"> Information
+                    </label>
+                </div>
+                <div>
+                    <label for="message-two-warning">
+                        <input type="radio" id="message-two-warning" name="message-two-type" value="information" aria-label="Information"> Warning
+                    </label>
+                </div>
+                <div>
+                    <label for="message-two-error">
+                        <input type="radio" id="message-two-error" name="message-two-type" value="information" aria-label="Error"> Error
+                    </label>
+                </div>
+            </fieldset>
+            ${createTypePreviewHTML(message_two)}
+            <div>
+                <input class="btn btn-primary" type="button" value="Save Draft" onclick="saveDraft("${message_two.id}")" />
+                <input class="btn btn-primary" type="button" value="Publish Latest Draft" onclick="publishLatestDraft("${message_two.id}")" />
+                <input class="btn btn-primary" type="button" value="Unpublish Message" onclick="unpublishMessage("${message_two.id}")" />
+                <input class="btn btn-cancel" type="button" value="Reset" onclick="reset("${message_two.id}")" />
+            </div>
+        </form>
+    `
+    );   
+    
     return result;
+}
+
+function createTypePreviewHTML(message){
+    var draftPreviewHTML = ``;
+    var publishedPreviewHTML = ``;
+    draftPreviewHTML = `
+        <p>Draft Preview</p>
+        <div id="${message.id}draft">
+            <div class="alert alert-danger col-md-12" id="alert_unique_16262b641c316a">
+            <div class="row d-flex padding-pagealert align-items-center">
+                <div class="flex-grow-0 col">
+                    <span class="fi cdc-icon-alert_06 " aria-hidden="true"></span>                        
+                </div>
+                <div class="col">
+                    <p id="${message.id}title-draft" class="margin-pagealert">
+                    ${message.draft.title}
+                    </p>		
+                </div>
+                ${message.draft.body.length > 0 ? `<div class="col flex-grow-0"><input class="btn btn-primary" type="button" value="Details" /></div>` : ``}
+            </div>
+        </div>
+    `;
+    if(message.publish_status == 0){
+        publishedPreviewHTML = `
+            <p>Published Version</p>
+            <i>No message published.</i>
+        `;
+    } else {
+        publishedPreviewHTML = `
+            <p>Published Version</p>
+            <div id="${message.id}draft">
+                <div class="alert alert-danger col-md-12" id="alert_unique_16262b641c316a">
+                <div class="row d-flex padding-pagealert align-items-center">
+                    <div class="flex-grow-0 col">
+                        <span class="fi cdc-icon-alert_06 " aria-hidden="true"></span>                        
+                    </div>
+                    <div class="col">
+                        <p id="${message.id}title-draft" class="margin-pagealert">
+                        ${message.published.title}
+                        </p>		
+                    </div>
+                    ${message.published.body.length > 0 ? `<div class="col flex-grow-0"><input class="btn btn-primary" type="button" value="Details" /></div>` : ``}
+                </div>
+            </div>
+        `;
+    }
+    return draftPreviewHTML + publishedPreviewHTML;
 }
