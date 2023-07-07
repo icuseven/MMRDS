289cf734f5a474a18806394db5a7acfbfe40ee44
icuseven <jhaines@thetagrid.com>
2023-07-07 08:59:56 -0400

    IJE Gen update.

--- source-code/mmria/mmria.ije.generator/GenMortality.cs
+++ source-code/mmria/mmria.ije.generator/GenMortality.cs
@@ -28,8 +28,8 @@ public class GenMortality
          {"BLANK3",get_One_BLANK},
          { "DOB_YR", gen_DOB_YR},
          {"DOB_MO", gen_DOB_MO},
-         /*"DOB_DY",
-         "BPLACE_CNT",
+         {"DOB_DY", gen_DOB_DY},
+         /*"BPLACE_CNT",
          "BPLACE_ST",
          "CITYC",
          "COUNTYC",
@@ -348,8 +348,20 @@ public class GenMortality
         return Context.rnd.Next(1,13).ToString().PadLeft(2,'0');
       }
     }
-/*2		DOB_DY	01-31 (based on month), 99
-2		BPLACE_CNT	NCHS Part 8 (from FIPS table 10-4)
+
+    string gen_DOB_DY()
+    {
+         //2		DOB_DY	01-31 (based on month), 99
+         if(Context.rnd.NextDouble() > Context.percentage_threshold)
+         {
+            return "99";
+         }
+         else
+         {
+            return Context.rnd.Next(1,32).ToString().PadLeft(2,'0');
+         }
+    }
+/*2		BPLACE_CNT	NCHS Part 8 (from FIPS table 10-4)
 2		BPLACE_ST	"NCHS Instruction Manual Part 8A
    ZZ = UNKNOWN OR BLANK U.S. STATE OR TERRITORY OR UNKNOWN CANADIAN PROVINCE OR UNKNOWN/ UNCLASSIFIABLE COUNTRY
    XX = UNKNOWN STATE WHERE COUNTRY IS KNOWN, BUT NOT U.S. OR CANADA 
