03025a5fa2959c706405498696b24266ad272196
Nick Chapman <ptr8@cdc.gov>
2023-07-12 17:10:31 -0600

    fixes address three 508 issues

--- source-code/mmria/mmria-server/wwwroot/scripts/Home/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/Home/index.js
@@ -146,7 +146,7 @@ function load_user_role_jurisdiction()
       }
     role_list_html.push("</p>");
     
-    role_list_html.push("<table class='table'>");
+    role_list_html.push("<table aria-label='Role assignment list' class='table'>");
       role_list_html.push("<thead class='thead'>");
       role_list_html.push("<tr class='tr bg-tertiary'>");
       role_list_html.push("<th class='th h4' colspan='7' scope='colgroup'>Role assignment list</th>");
