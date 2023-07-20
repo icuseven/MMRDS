8cbde4c23f469de2a6c0625592dfe23f2309a631
icuseven <jhaines@thetagrid.com>
2023-07-20 09:36:15 -0400

    Added ConfigurationMaster

--- source-code/mmria/mmria.common/couchdb/configuration/configuration.cs
+++ source-code/mmria/mmria.common/couchdb/configuration/configuration.cs
@@ -26,6 +26,25 @@ public sealed class ConfigurationSet
 
     public string data_type { get; } = "configuration-set";
 
+    public static string g_geocode_api_key;
+    public static string geocode_api_url;
+    public static string couchdb_url = "http://localhost:5984";
+
+    public static string db_prefix = "";
+    public static string web_site_url;
+
+    public static string timer_user_name;
+    public static string timer_value;
+    public static string cron_schedule;
+    public static string export_directory;
+
+    public static bool is_schedule_enabled = true;
+    public static int session_idle_timeout_minutes;
+
+    public static bool is_db_check_enabled = false;
+
+    public static string vitals_url;
+
     public Dictionary<string, string> name_value { get;set; }
 
     public Dictionary<string, DBConfigurationDetail> detail_list { get;set; }
@@ -35,4 +54,14 @@ public sealed class ConfigurationSet
     public DateTime date_last_updated { get; set; } 
     public string last_updated_by { get; set; } 
 
+}
+
+public sealed class ConfigurationMaster
+{
+    public ConfigurationMaster()
+    {
+        ConfigurationSet = new Dictionary<string, ConfigurationSet>(StringComparer.OrdinalIgnoreCase);
+    }
+
+    public Dictionary<string, ConfigurationSet> ConfigurationSet { get; set; }
 }
