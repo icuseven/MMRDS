0d42450ccff6d13369b1976fc3abfae5b8e068cb
icuseven <jhaines@thetagrid.com>
2023-08-08 15:12:17 -0400

    Config Update.

--- source-code/mmria/mmria-pmss-server/Controllers/interactive_aggregate_reportController.cs
+++ source-code/mmria/mmria-pmss-server/Controllers/interactive_aggregate_reportController.cs
@@ -4,22 +4,32 @@ using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 
-namespace mmria.pmss.server.Controllers;
+using  mmria.server.extension; 
+
+namespace mmria.server.Controllers;
 
 [Authorize(Roles  = "abstractor,data_analyst")]
 [Route("interactive-aggregate-report")]
 public sealed class interactive_aggregate_reportController : Controller
 {
-    private readonly IAuthorizationService _authorizationService;
-    private IConfiguration configuration;
+    mmria.common.couchdb.OverridableConfiguration configuration;
+    mmria.common.couchdb.DBConfigurationDetail db_config;
+    string host_prefix = null;
 
-    public interactive_aggregate_reportController(IAuthorizationService authorizationService, IConfiguration p_configuration)
+
+    public interactive_aggregate_reportController
+    (
+        IHttpContextAccessor httpContextAccessor, 
+        mmria.common.couchdb.OverridableConfiguration _configuration
+    )
     {
-        _authorizationService = authorizationService;
-        configuration = p_configuration;
+        configuration = _configuration;
+        host_prefix = httpContextAccessor.HttpContext.Request.Host.GetPrefix();
+        db_config = configuration.GetDBConfig(host_prefix);
     }
+
     public async Task<IActionResult> Index()
     {
         ViewBag.is_power_bi_user = false;
@@ -30,9 +40,9 @@ public sealed class interactive_aggregate_reportController : Controller
 
         try
         {
-            string my_user_url = $"{Program.config_couchdb_url}/_users/org.couchdb.user:{userName}";
+            string my_user_url = $"{db_config.url}/_users/org.couchdb.user:{userName}";
 
-            var user_curl = new cURL("GET",null,my_user_url,null, Program.config_timer_user_name, Program.config_timer_value);
+            var user_curl = new cURL("GET",null,my_user_url,null, db_config.user_name, db_config.user_value);
             string responseFromServer = await user_curl.executeAsync();
 
             var user  = Newtonsoft.Json.JsonConvert.DeserializeObject<mmria.common.model.couchdb.user>(responseFromServer);
@@ -40,11 +50,11 @@ public sealed class interactive_aggregate_reportController : Controller
             {
                 ViewBag.is_power_bi_user = true;
 
-                var temp_string = configuration["mmria_settings:power_bi_aggregate"];
+                var temp_string = configuration.GetString("power_bi_aggregate", host_prefix);
                 
                 if(!string.IsNullOrWhiteSpace(temp_string))
                 {
-                    var app_instance_name = configuration["mmria_settings:app_instance_name"];
+                    var app_instance_name = configuration.GetString("mmria_settings:app_instance_name", host_prefix);
                     if(!string.IsNullOrWhiteSpace(app_instance_name))
                     {
                         ViewBag.power_bi_link = temp_string.Replace("{prefix}",app_instance_name);
@@ -64,7 +74,6 @@ public sealed class interactive_aggregate_reportController : Controller
         {
             System.Console.WriteLine ($"{ex}");
         }
-
 
         return View();
     }
