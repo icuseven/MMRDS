1a0b4ee77f9ff993905475392138ee6d603bbdef
icuseven <jhaines@thetagrid.com>
2023-08-22 17:27:55 -0400

    Filter out 409 conflict error.

--- source-code/mmria/mmria-server/wwwroot/scripts/case/index.js
+++ source-code/mmria/mmria-server/wwwroot/scripts/case/index.js
@@ -2075,8 +2075,13 @@ function save_case(p_data, p_call_back, p_note)
                 case_response.rev == null
             ) 
             {
-                if(case_response.indexOf("(409) Conflict") > -1) return;
-                
+                if
+                (
+                    case_response != null &&
+                    case_response.error_description != null &&
+                    case_response.error_description.indexOf("(409) Conflict") > -1
+                ) return;
+
                 const err = {
                     status: 500,
                     responseText : case_response.error_description
