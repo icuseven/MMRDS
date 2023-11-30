b213df84d4ee90c932ac3570e50961320596353d
icuseven <jhaines@thetagrid.com>
2023-11-30 09:28:43 -0500

    PMSS Import - CSV Prep

--- source-code/mmria/mmria-pmss-server/wwwroot/scripts/pmss-import/fileupload.js
+++ source-code/mmria/mmria-pmss-server/wwwroot/scripts/pmss-import/fileupload.js
@@ -109,13 +109,20 @@ function setup_file_list()
         let item = g_file_stat_list[i];
         if (typeof item !== "undefined") 
         {
-            if (item.name.toLowerCase().endsWith(".mor")) 
+            if (item.name.toLowerCase().endsWith(".csv")) 
             {
                 g_cdc_identifier_set = {};
                 
                 is_mor = true;
                 temp[0] = item;
                 temp_contents[0] = g_content_list[i];
+
+                const data = parseCSV(temp_contents[0]);
+
+                // other 150 columns
+
+                // all 555 columns
+
 
                 var patt = new RegExp("20[0-9]{2}_[0-2][0-9]_[0-3][0-9]_[A-Z,a-z]{2}.[mM][oO][rR]");
 
@@ -540,4 +547,42 @@ function validate_AssociatedFET(p_array)
     }
 
     return result;
+}
+
+
+function parseCSV(str) 
+{
+    const arr = [];
+    let quote = false;  // 'true' means we're inside a quoted field
+
+    // Iterate over each character, keep track of current row and column (of the returned array)
+    for (let row = 0, col = 0, c = 0; c < str.length; c++) {
+        let cc = str[c], nc = str[c+1];        // Current character, next character
+        arr[row] = arr[row] || [];             // Create a new row if necessary
+        arr[row][col] = arr[row][col] || '';   // Create a new column (start with empty string) if necessary
+
+        // If the current character is a quotation mark, and we're inside a
+        // quoted field, and the next character is also a quotation mark,
+        // add a quotation mark to the current column and skip the next character
+        if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }
+
+        // If it's just one quotation mark, begin/end quoted field
+        if (cc == '"') { quote = !quote; continue; }
+
+        // If it's a comma and we're not in a quoted field, move on to the next column
+        if (cc == ',' && !quote) { ++col; continue; }
+
+        // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+        // and move on to the next row and move to column 0 of that new row
+        if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+        // If it's a newline (LF or CR) and we're not in a quoted field,
+        // move on to the next row and move to column 0 of that new row
+        if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+        if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+        // Otherwise, append the current character to the current column
+        arr[row][col] += cc;
+    }
+    return arr;
 }
