c27312b3528c6112d1815a47cfdb4da599fc2129
icuseven <jhaines@thetagrid.com>
2024-02-04 13:10:03 -0500

    Strong-Case Test Convert Json

--- source-code/mmria/strongly-typed-case/Program.cs
+++ source-code/mmria/strongly-typed-case/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -8,27 +9,37 @@ namespace strongcase;
 
 internal class Program
 {
+
+    enum RunEnum
+    {
+        Generate_From_Metadata,
+        Test_Output_Load_Json
+    }
+
+    static RunEnum Run_Type = RunEnum.Test_Output_Load_Json;
     static public async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if(Run_Type == RunEnum.Generate_From_Metadata)
+        {
+            Console.WriteLine("Executinng: Generate_From_Metadata");
 
-/*
+            /*
 
-using System.Net.Http.Json;
+            using System.Net.Http.Json;
 
-//Get JSON
-var stock = await httpClient.GetFromJsonAsync<Stock>($"https://localhost:12345/stocks/{symbol}");
+            //Get JSON
+            var stock = await httpClient.GetFromJsonAsync<Stock>($"https://localhost:12345/stocks/{symbol}");
 
-stock.Price = 121.10m;
+            stock.Price = 121.10m;
 
-//Send JSON
-await httpClient.PostAsJsonAsync<Stock>("https://localhost:12345/stocks/", stock);
+            //Send JSON
+            await httpClient.PostAsJsonAsync<Stock>("https://localhost:12345/stocks/", stock);
 
-*/
+            */
 
 
         //string metadata_url = $"http://localhost:5984/metadata/version_specification-20.12.01/metadata";
-;
+
         //string metadata_url = $"{host_db_url}/metadata/version_specification-20.12.01/metadata";
         //var metadata_client = new HttpClient { BaseAddress = new Uri(metadata_url) };
         
@@ -37,7 +48,8 @@ await httpClient.PostAsJsonAsync<Stock>("https://localhost:12345/stocks/", stock
         mmria.common.metadata.app metadata = null;
 
 
-        var metadata_url = $"http://localhost:5984/metadata/version_specification-23.01.03/metadata";
+        //var metadata_url = $"http://localhost:5984/metadata/version_specification-23.01.03/metadata";
+        var metadata_url = $"http://localhost:5984/metadata/version_specification-23.11.08/metadata";
         using(var metadata_client = new HttpClient ())
         {
             metadata = await metadata_client.GetFromJsonAsync<mmria.common.metadata.app>(metadata_url);
@@ -102,8 +114,57 @@ namespace mmria.case_version.v1;");
             print("Grid", metadata_mgr.GridList);
             print("Group", metadata_mgr.GroupList);
         */
+        }
+        else if(Run_Type == RunEnum.Test_Output_Load_Json)
+        {
+            Console.WriteLine("Executinng: Test_Output_Load_Json");
 
+            mmria.case_version.v1.mmria_case test_case = new mmria.case_version.v1.mmria_case();
+            try
+            {
 
+/*
+                System.Text.Json.JsonDocumentOptions options = new()
+                {
+                    WriteIndented = true
+                };
+                */
+
+                using System.IO.FileStream stream = System.IO.File.OpenRead("json-convert-test/23.11.08.json");
+                //test_case = await System.Text.Json.JsonSerializer.DeserializeAsync<mmria.case_version.v1.mmria_case>(stream);
+                //var json = System.IO.File.ReadAllText("json-convert-test/23.11.08.json");
+                var json_element = await System.Text.Json.JsonSerializer.DeserializeAsync<System.Text.Json.JsonDocument>(stream);
+                test_case.Convert(json_element.RootElement);
+                
+
+                if(test_case != null)
+                {
+                    System.Console.WriteLine($"case version: {test_case.version}");
+                }
+                else
+                {
+                    System.Console.WriteLine($"unable to deserialize case version: 23.11.08");
+                }
+                
+
+                //test_case.Convert()
+
+                
+
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+        }
+
+    }
+
+    public static async Task<T> ReadAsync<T>(string filePath)
+    {
+        using System.IO.FileStream stream = System.IO.File.OpenRead(filePath);
+        return await System.Text.Json.JsonSerializer.DeserializeAsync<T>(stream);
     }
 
     static void print(string list_name, List<metadata_mgr.Metadata_Node> list)
