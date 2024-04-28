49dc0a67545849bacbdd36581a82565a5458609a
icuseven <jhaines@thetagrid.com>
2024-04-28 13:53:05 -0400

    Lasso Collider Resize

--- source-code/mmria/form-designer/LayoutSurfacePanel.gd
+++ source-code/mmria/form-designer/LayoutSurfacePanel.gd
@@ -3,11 +3,19 @@ extends Panel
 var lasso_start:Vector2 = Vector2.ZERO
 var lasso_end:Vector2 = Vector2.ZERO
 var lasso_color:Color = Color.YELLOW
-var drag_start:bool = false
+var is_dragging:bool = false
+
+var LassoArea:Area2D
+var LassoCollisionShap:RectangleShape2D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	LassoArea = $LassoArea2D
+	LassoCollisionShap = $LassoArea2D/CollisionShape2D.shape
+	
+	LassoArea.connect("area_entered", _lasso_on_enter)
+	LassoArea.connect("area_exited", _lasso_on_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,19 +24,37 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		lasso_end = get_local_mouse_position()
+		if is_dragging:
+			lasso_end = get_local_mouse_position()
+			
+			
+			#var size_x = abs(lasso_start.x - lasso_end.x) / 2
+			#var size_y = abs(lasso_start.y - lasso_end.y) / 2
+			
+			var size_x = (lasso_start.x - lasso_end.x)
+			var size_y = (lasso_start.y - lasso_end.y)
+	
+			LassoArea.position = lasso_start - Vector2(size_x, size_y) /2
+			
+			LassoCollisionShap.size = Vector2(abs(size_x), abs(size_y))
+			
+
+				 
 		queue_redraw()	
 	elif event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and !event.pressed:
 			print("right mouse button event at %s event_pressed: %s", event.position, event.pressed)
 		elif event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				drag_start = true
+				is_dragging = true
 				lasso_start = get_local_mouse_position()
+				#LassoArea.position = lasso_start
 				#LassoOffset = LassoStart - event.position
 			else:
 				#print("Left mouse button event at %s event_pressed: %s", event.position, event.pressed)
-				drag_start = false
+				#for area in LassoArea.get_overlapping_areas():
+				#	print("OverLapping %s",area)
+				is_dragging = false
 		elif event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
 			print("Wheel up")			
 	if event is InputEventKey and event.pressed:
@@ -40,7 +66,7 @@ func _input(event):
 
 
 func _draw():
-	if drag_start:
+	if is_dragging:
 		var point_array:PackedVector2Array
 		point_array.append(lasso_start)
 		point_array.append(Vector2(lasso_end.x, lasso_start.y))
@@ -52,3 +78,10 @@ func _draw():
 		draw_line(point_array[0], point_array[2], lasso_color)
 		draw_line(point_array[2], point_array[3], lasso_color)
 		draw_line(point_array[3], point_array[1], lasso_color)
+
+func _lasso_on_enter(area: Area2D):
+	print("Lasso Area Enterd")
+	
+	
+func _lasso_on_exited(area: Area2D):
+	print("Lasso Area Exited")
