8e3a261092942e2860a2b6e2dae08e70bbbb7515
Nick Chapman <ptr8@cdc.gov>
2024-08-28 08:58:12 -0600

    JIRA 2026 & 1993 - implemented filtering functionality for data dictionary and finished UI redesign refresh.  Link on homepage now points to new data dictionary page

--- source-code/mmria/mmria.common/metadata/DataDictionary.cs
+++ source-code/mmria/mmria.common/metadata/DataDictionary.cs
@@ -7,4 +7,10 @@ public sealed class DataDictionary
 {
   public string FormName { get; set; }
   public List<FormMetadata> FormDataRows { get; set; }
+  public class FormFilterModel
+  {
+    public string? SearchFilter { get; set; }
+    public string? FormFilter { get; set; }
+    public string? VersionFilter { get; set ; }
+  }
 }
